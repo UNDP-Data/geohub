7cb637a62043c191c2394873ebd9870af29a8ab8
Thuhaa <josephthuha@gmail.com>
2022-02-12 01:29:51 +0300

    Main Content moves together with the drawer opening

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -4,6 +4,17 @@
 
 </script>
 
+<!-- Todo: This styles are set to ensure that the Map Content moves together with the drawer opening-->
+<style global>
+    .mdc-drawer{
+        width:500px;
+    }
+
+    .mdc-drawer.mdc-drawer--open:not(.mdc-drawer--closing)+.mdc-drawer-app-content{
+        margin-left: 500px;
+        margin-right: 0;
+    }
+</style>
 
 <!-- <Button on:click={() => alert('It worked')}>Click me...</Button>
 <Button on:click={() => alert('It worked')} color="secondary">Click me...</Button> -->
