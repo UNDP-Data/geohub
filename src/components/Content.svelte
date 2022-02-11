a7f2f7b492f05c02d328286081cdc5844bb5bb95
janf <ioan.ferencik@solargis.com>
2022-02-11 14:03:44 +0100

    layout improvements

--- src/components/Content.svelte
+++ src/components/Content.svelte
@@ -1,5 +1,6 @@
 
 <script lang="ts">
+	import { mdiWeatherSunny } from '@mdi/js';
   import Drawer, {
     AppContent,
     Content,
@@ -8,36 +9,27 @@
     Subtitle,
   } from '@smui/drawer';
 
-  //import OpenInFullIcon from '@mui/icons-material/OpenInFull';
+  import Paper, { Title, Subtitle, Content } from '@smui/paper';
   import List, { Item, Text } from '@smui/list';
-
   import IconButton from '@smui/icon-button'
   export let expanded = false;
   export let open = false;
 
   import Icon from '@iconify/svelte';
   let active = 'Vector tiles';
-  let toggleClicked;
-  let initialOff;
+  
+  
 
 
   function setActive(value: string) {
     active = value;
   }
-  function toggle() {
-    expanded = !expanded;
-  }
-  function collapse(){
-    panel.expanded = false;
-  }
-  function expand(){
-    panel.expanded = true;
-  }
+  
   let panel;
   
 </script>
 <div class="drawer-container">
-  <Drawer variant="dismissible" bind:open anchor="right">
+  <Drawer variant="dismissible" bind:open>
     <Header>
       <Title>Layers</Title>
       <Subtitle>Operations with Layers</Subtitle>
@@ -86,7 +78,7 @@
   <!-- Todo: Create a component for the following -->
   <AppContent class="app-content">
     <main class="main-content">
-      <slot></slot>
+      <!-- <slot></slot> -->
       <pre class="status">Active: {active}</pre>
       <h1>Map Area Map Area Map Area Map Area Map Area Map Area Map Area
         Map Area Map Area Map Area Map Area Map Area Map Area Map Area Map Area
@@ -97,23 +89,23 @@
         Map Area Map Area Map Area Map Area Map Area Map Area Map Area
       </h1>
 
-      
-      
     </main>
     <div class="bottom-drawer" bind:this={panel} class:expanded>
-      <!-- <div class="bottom-drawer-header">
-        <slot name=heading {expanded}>
-          <IconButton on:click={toggle} toggle bind:pressed={initialOff}>
-            <Icon icon="mdi:expand-all-outline" color="white" />
-          </IconButton>
-        </slot>
-      </div> -->
+      
       {#if expanded}
-        <div class="expandable">
-          Nice
-        </div>
+      <Paper unelevated square variant="outlined" >
+
+        <!-- <Title>Info Panel</Title>
+        <Subtitle>This is an unelevated sheet of paper.</Subtitle> -->
+        <Content>Unelevated paper is used to build a surface.
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum totam modi dolor, quae officiis nobis! Ipsum dicta culpa esse temporibus molestiae? Veniam distinctio beatae, perferendis sapiente libero ad adipisci est voluptas dolores, necessitatibus, doloremque corrupti soluta iure harum! Ipsam, id similique pariatur nobis consequatur earum sed hic explicabo debitis veritatis voluptates temporibus ipsum dolorum alias eaque vitae voluptatem, mollitia modi? Autem voluptas sequi delectus nemo fuga beatae sint ex cumque nihil quisquam neque, cupiditate porro molestiae est mollitia dolor tempora alias itaque. Iusto voluptas voluptate debitis consequuntur excepturi quae quos, consectetur amet aliquid ut facere autem veniam dolor sed distinctio nam nesciunt minus porro harum magnam dignissimos impedit numquam ullam. Enim quaerat odit ipsam, praesentium autem beatae explicabo laboriosam. Possimus excepturi aperiam in aliquam rem cumque sed, ut, vitae sint blanditiis molestias similique voluptatum eius voluptas? Ducimus laborum non veritatis, incidunt porro corporis amet officiis, saepe fugiat modi dolore eius expedita! Aliquid eaque nesciunt minima dignissimos laboriosam quos deleniti cum aut vero necessitatibus rerum, totam mollitia nemo. Accusamus quia cupiditate mollitia labore deserunt consectetur aspernatur harum eius magni, odio corporis minima officia, placeat vitae? Asperiores hic error laboriosam non esse nesciunt dolore cupiditate iste, illum sunt! Possimus iste, culpa repudiandae quis inventore esse totam perspiciatis odit dolore aliquam, adipisci, accusantium eveniet. Voluptates debitis magnam unde beatae quae repudiandae error ipsam, deleniti eveniet accusantium perspiciatis nihil ipsa nesciunt iusto odio non, nam distinctio obcaecati saepe. Suscipit, ullam. In repudiandae similique aut accusantium. Eligendi officiis quisquam libero delectus ipsa veritatis ratione corrupti dolorem modi assumenda atque perferendis quia deserunt, molestiae ab iste est nobis dolore aliquid, nesciunt sit fugiat. Architecto, deleniti? Ab, ratione dolore, minima deserunt beatae eum ea earum omnis porro ad ducimus minus quisquam soluta ex neque reprehenderit. Eius debitis perspiciatis fugit beatae corrupti assumenda dolorem suscipit vitae aperiam enim!
+
+
+        </Content>
+      </Paper>
       {/if}
     </div>
+    
   </AppContent>
   <!-- Todo: Create a component for the following -->
 </div>
@@ -121,19 +113,23 @@
 
 
 <style>
+  
   /* These classes are only needed because the
       drawer is in a container on the page. */
   .drawer-container {
-    position: relative;
+    position: absolute;
     display: flex;
-    height: calc(100vh - 65px);
+    height: calc(100vh - 64px);
+    /* height:100vh; */
     width: 100%;
-    /*border: 1px solid;*/
-    overflow: hidden;
+    border: 0px solid; 
+    /* overflow: hidden; */
     z-index: 0;
+    flex-grow: 1;
+    box-sizing: border-box;
   }
 
-  * :global(.app-content) {
+  .app-content {
     flex: auto;
     overflow: auto;
     position: relative;
@@ -142,7 +138,7 @@
 
   .main-content {
     overflow: hidden;
-    padding: 16px;
+    /* padding: 66px; */
     height: 100%;
     box-sizing: border-box;
   }
@@ -150,37 +146,30 @@
     position: absolute;
     bottom: 0;
     left: 0%;
-    background-color: dodgerblue;
+    /* background-color: dodgerblue; */
     width: 100%;
-
+    /* height: 100%; */
     /* border-radius: 5px 50px 50px 5px; */
+    flex-grow: 1;
+    overflow: auto
   }
-  .bottom-drawer-header{
-    display: inline-flex;
+  
+  .ipanel {
+    border-top: 10px;
   }
 
   .expanded{
-    height: 150px;
+    display :inline-block;
+    max-height: 150px;
+    /* height: 150px; */
     position: absolute;
     bottom: 0;
     left: 0%;
-    background-color: dodgerblue;
+    /* background-color: dodgerblue; */
     width: 100%;
+    
   }
 
-  /* Todo: Declare styles for a non-expanded drawer here*/
 
-  #expand-icon{
-    position: absolute;
-    right: 0;
-  }
-  .example-text{
-    color: white;
-    font-family: "Bodoni MT",serif;
-    padding-left: 50px;
-  }
-
-  .content-for-bottom-drawer{
-    height: 100%;
-  }
+  
 </style>
--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -3,20 +3,28 @@
   import Header from "../components/Header.svelte";
   import Content from "../components/Content.svelte";
   
-  let drawerCollapsed: boolean = false ;
+  let drawerCollapsed: boolean = true ;
   $: updateDrawer = drawerCollapsed;
-  let panelCollapsed: boolean = false
-
+  let panelCollapsed: boolean = false;
+  import {style} from "svelte-body";
   
 </script>
+<svelte:body
+    use:style="{{ height:'100vh', margin:'0px',padding:'0px', border:'0px solid red'}}"
+/>
 
-<Header bind:drawerCollapsed bind:panelCollapsed ></Header>
+  <Header bind:drawerCollapsed bind:panelCollapsed ></Header>
 
-<Content bind:open={drawerCollapsed} bind:expanded={panelCollapsed} >
-  
-</Content>
-
-
+  <Content bind:open={drawerCollapsed} bind:expanded={panelCollapsed} >
+    
+  </Content>
 
 
- <slot />
+
+
+ 
+
+ <style>
+   
+
+ </style>
