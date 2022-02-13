157201ac03bea0a3fa0ecaeb957c6f7cd2662d5a
Thuhaa <josephthuha@gmail.com>
2022-02-14 00:28:42 +0300

    List Blobs on One Container

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,42 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script context="module">
+    export const load = async({fetch}) => {
+        const res = await fetch("list_blobs_in_container.json")
+
+        const tree = await res.json()
+        console.log("Data", tree)
+        return {
+           props: {
+               tree
+           }
+        }
+    }
+</script>
+
+<script lang="ts">
+    import TreeView from './TreeView.svelte'
+    //import tree from './tree'
+    export let tree
+</script>
+
+<div>
+<!--    <select class="select-data">-->
+<!--        {#each data as d}-->
+<!--        <option class="data-option">{d}</option>-->
+<!--        {/each}-->
+<!--    </select>-->
+    <TreeView {tree}/>
+
+    <!--<div>{data.children}</div>-->
+</div>
+<style>
+    .select-data{
+        height: 40px;
+    }
+    .data-option{
+        font-size: larger;
+        font-family: sans-serif;
+        background-color: white;
+        color: cadetblue;
+        font-style: italic;
+    }
+</style>
