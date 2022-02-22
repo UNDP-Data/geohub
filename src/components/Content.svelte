1660ab111e4d6808e7f5224e1c1d186404a8f3cf
janf <ioan.ferencik@solargis.com>
2022-02-22 19:08:18 +0100

    vars and raster

--- src/components/Content.svelte
+++ src/components/Content.svelte
@@ -21,7 +21,7 @@
   
   
   import Icon from '@iconify/svelte';
-  let active = 'Vector tiles';
+  let active = 'Load data';
   let rlabel;
   let expandedt;
  
@@ -32,18 +32,14 @@
 
   import Tab, { Label } from '@smui/tab';
   import TabBar from '@smui/tab-bar';
-  // import { Tabs, TabList, TabPanel, Tab } from '../components/tabs';
-
-  // function setActive(value: string) {
-  //   active = value;
-  // }
+  
   
   let panel;
 
 
   import {wtree}  from '../stores/stores';
   import TreeView from './TreeView.svelte';
-  import LayerList from './LayerList.svelte';
+  
   
   
 </script>
@@ -53,7 +49,7 @@
   <Drawer variant="dismissible" bind:open >
     
     <Header>
-      <TabBar tabs={['Load...', 'Layers', 'Analyze']}   let:tab bind:active>
+      <TabBar tabs={['Load data', 'Layers', 'Analyze']}   let:tab bind:active>
         <!-- Note: the `tab` property is required! -->
         <Tab {tab} class="tab">
           <Label  >{tab}</Label>
@@ -63,52 +59,13 @@
     
     <Content>
       
-      {#if active == 'Load...'}
+      {#if active === 'Load data'}
           <TreeView tree={$wtree.tree}   />
-      {:else if active =='Layers'}
+      {:else if active === 'Layers'}
         <LayerList></LayerList>
-        <!-- <List>
-          <Item
-                  href="javascript:void(0)"
-                  on:click={() => setActive('Vector tiles')}
-                  activated={active === 'Vector tiles'}
-          >
-            <Text>Vector tiles</Text>
-          </Item>
-          <Item
-                  href="javascript:void(0)"
-                  on:click={() => setActive('A Space Rocket')}
-                  activated={active === 'A Space Rocket'}
-          >
-            <Text>A Space Rocket</Text>
-          </Item>
-          <Item
-                  href="javascript:void(0)"
-                  on:click={() => setActive('100 Pounds of Gravel')}
-                  activated={active === '100 Pounds of Gravel'}
-          >
-            <Text>100 Pounds of Gravel</Text>
-          </Item>
-          <Item
-                  href="javascript:void(0)"
-                  on:click={() => setActive('All of the Shrimp')}
-                  activated={active === 'All of the Shrimp'}
-          >
-            <Text>All of the Shrimp</Text>
-          </Item>
-          <Item
-                  href="javascript:void(0)"
-                  on:click={() => setActive('A Planet with a Mall')}
-                  activated={active === 'A Planet with a Mall'}
-          >
-            <Text>A Planet with a Mall</Text>
-          </Item>
-      </List> -->
-      {:else if active=='Analyze'}
+      {:else if active === 'Analyze'}
         Analyze
       {/if}
-      
-       
       
       
     </Content>
