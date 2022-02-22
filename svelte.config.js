3ae0138d3672056d56a0a6169ba9aebc6bf4a4ae
janf <ioan.ferencik@solargis.com>
2022-02-22 21:54:59 +0100

    clean up and fix error

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from '@sveltejs/adapter-static';
+import adapter from '@sveltejs/adapter-auto';
 import preprocess from 'svelte-preprocess';
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -9,7 +9,7 @@ const config = {
 
 	kit: {
 		adapter: adapter({
-			fallback: 'index.html'
+			//fallback: 'index.html'
 		})
 	}
 };
