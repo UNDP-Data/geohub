f7d3a209857db64f15088afd3340cbaeba5f1163
Chris Foo <foochris@gmail.com>
2022-03-10 13:37:47 -0500

    refactor: cleanup for post icons branch merge (#31)

--- src/components/LayerList.svelte
+++ src/components/LayerList.svelte
@@ -1,48 +1,34 @@
 <script lang="ts">
-
-
-	import RasterLayer from './RasterLayer.svelte';
-    import VectorLayer from './VectorLayer.svelte';
-	import { layerList } from '../stores/stores';
-    import Button, { Label } from '@smui/button';
-    import DynamicLayer from './DynamicLayer.svelte'
-    let disabled:boolean = true;
-    let open:boolean = false
-
-
-
+  import RasterLayer from './RasterLayer.svelte'
+  import VectorLayer from './VectorLayer.svelte'
+  import { layerList } from '../stores/stores'
+  import Button, { Label } from '@smui/button'
+  import DynamicLayer from './DynamicLayer.svelte'
+  let disabled = true
+  let open = false
 </script>
+
 {#if !disabled}
-    <div style="display:flex; justify-content:center; flex-direction:row">
-        <Button on:click={() => open = true} bind:disabled>
-            <Label>Combined layer from selection..</Label>
-        </Button>
+  <div style="display:flex; justify-content:center; flex-direction:row">
+    <Button on:click={() => (open = true)} bind:disabled>
+      <Label>Combined layer from selection..</Label>
+    </Button>
+  </div>
 
-    </div>
-
-    <DynamicLayer bind:open  ></DynamicLayer>
+  <DynamicLayer bind:open />
 {/if}
 
-
-
-
-
-{#each $layerList as layerCfg(layerCfg.lDef.id)}
-    <!-- <Layer layerCfg={layerCfg}></Layer> -->
-    {#if layerCfg.lType === 'raster'}
-        <RasterLayer bind:layerCfg bind:disabled></RasterLayer>
-    {:else}
-        <VectorLayer></VectorLayer>
-    {/if}
+{#each $layerList as layerCfg (layerCfg.lDef.id)}
+  <!-- <Layer layerCfg={layerCfg}></Layer> -->
+  {#if layerCfg.lType === 'raster'}
+    <RasterLayer bind:layerCfg bind:disabled />
+  {:else}
+    <VectorLayer />
+  {/if}
 {/each}
 
-
-
 <style>
-
-    :global(.smui-paper__content){
-        padding: 0px!important;
-    }
-
-
+  :global(.smui-paper__content) {
+    padding: 0px !important;
+  }
 </style>
--- src/components/raster/Opacity.svelte
+++ src/components/raster/Opacity.svelte
@@ -1,10 +1,9 @@
 <script lang="ts">
-    import Slider from '@smui/slider';
-    export let layerOpacity = 1;
+  import Slider from '@smui/slider'
+  export let layerOpacity = 1
 </script>
 
-<div >
-    <Slider  bind:value={layerOpacity} min={0} max={1} step={0.01} input$aria-label="Layer opacity"/>
-    <span> {layerOpacity}</span>
+<div>
+  <Slider bind:value={layerOpacity} min={0} max={1} step={0.01} input$aria-label="Layer opacity" />
+  <span> {layerOpacity}</span>
 </div>
-
