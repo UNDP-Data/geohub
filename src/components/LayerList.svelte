cbfb18e04bea86ee44410f69f41367ffa75315d9
foochris <foochris@gmail.com>
2022-03-21 12:27:42 -0400

    feat: add no layer info message

--- src/components/LayerList.svelte
+++ src/components/LayerList.svelte
@@ -1,12 +1,29 @@
 <script lang="ts">
+  import Button, { Label } from '@smui/button'
+  import { Icon } from '@smui/icon-button'
+
   import RasterLayer from './RasterLayer.svelte'
   import VectorLayer from './VectorLayer.svelte'
   import { layerList } from '../stores'
-  import Button, { Label } from '@smui/button'
   import DynamicLayer from './DynamicLayer.svelte'
+  import { TabNames } from '../lib/constants'
+
   let disabled = true
   let open = false
 </script>
+
+{#if $layerList?.length === 0}
+  <ul class="no-data-layers">
+    <li class="message">
+      <div class="icon" style="color: hsl(204, 86%, 53%); transform: scale(1);">
+        <Icon color="primary" class="material-icons">info</Icon>
+      </div>
+      <div class="text">
+        No layers have been selected. Please select a layer from the <strong>{TabNames.LoadData}</strong> tab.
+      </div>
+    </li>
+  </ul>
+{/if}
 
 {#if !disabled && $layerList.length > 1}
   <div style="display:flex; justify-content:center; flex-direction:row">
@@ -29,5 +46,24 @@
 <style type="scss">
   :global(.smui-paper__content) {
     padding: 0px !important;
+  }
+
+  ul {
+    margin: 0;
+    list-style: none;
+    padding-left: 1.2rem;
+    user-select: none;
+    padding-top: 5px;
+
+    li {
+      display: flex;
+      justify-content: left;
+      align-items: center;
+      padding: 10px;
+
+      .text {
+        padding-left: 15px;
+      }
+    }
   }
 </style>
