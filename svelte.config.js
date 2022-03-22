249ddd28d49003d9f350869d847751d2dac1aff9
foochris <foochris@gmail.com>
2022-03-22 01:36:38 -0400

    build: update svelte-kit / node

--- svelte.config.js
+++ svelte.config.js
@@ -9,7 +9,7 @@ const config = {
     adapter: adapter({
       out: 'build',
       precompress: false,
-      env: {
+      envPrefix: {
         host: 'MY_HOST_VARIABLE',
         port: 'MY_PORT_VARIABLE',
         origin: 'MY_ORIGINURL',
