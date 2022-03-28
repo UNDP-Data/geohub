683df7a84d72a98690c4df517f442b0d939bb270
Jin Igarashi <igarashi.jin@water-gis.com>
2022-03-28 18:40:37 +0100

    created LayerName component to share between raster and vector

---
+++ src/components/LayerName.svelte
@@ -0,0 +1,75 @@
+<script lang="ts">
+  import Tag from 'svelma/src/components/Tag/Tag.svelte'
+
+  import { map } from '../stores'
+  import type { Layer, LayerDefinition } from '../lib/types'
+  import { LayerInitialValues } from '../lib/constants'
+  export let layerConfig: Layer = LayerInitialValues
+
+  let name: string, definition: LayerDefinition
+  ;({ name, definition } = layerConfig)
+  const layerId = definition.id
+  const mapLayers = $map.getStyle().layers
+  const mapLayerByLayerId = mapLayers.filter((item: LayerDefinition) => item.id == layerId).pop()
+
+  let mapLayerIndex = mapLayers.indexOf(mapLayerByLayerId)
+
+  export const hierachyDown = (layerID: string) => {
+    const newIndex = mapLayerIndex - 1
+
+    if (newIndex >= 0) {
+      $map.moveLayer(layerID, mapLayers[newIndex].id)
+      mapLayerIndex = newIndex
+      $map.triggerRepaint()
+    }
+  }
+
+  export const hierachyUp = (layerID: string) => {
+    const newIndex = mapLayerIndex + 1
+
+    if (newIndex <= mapLayers.length - 1) {
+      $map.moveLayer(layerID, mapLayers[newIndex].id)
+      mapLayerIndex = newIndex
+      $map.triggerRepaint()
+    }
+  }
+</script>
+
+<div class="layer-header">
+  <div>
+    <div class="layer-header-name">
+      <div class="layer-name">
+        {name}
+      </div>
+      <div class="unread-count">
+        <div style="float: right;">
+          <Tag type="is-info" size="is-small">{mapLayerIndex} / {mapLayers.length}</Tag>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+
+<style lang="scss">
+  .layer-header {
+    .layer-header-name {
+      display: flex;
+      justify-content: left;
+      align-items: center;
+      font-family: ProximaNova, sans-serif;
+      height: 20px;
+
+      .layer-name {
+        white-space: nowrap;
+        overflow: hidden;
+        text-overflow: ellipsis;
+        width: 100%;
+        font-size: 14px;
+      }
+
+      .unread-count {
+        padding-left: 7.5px;
+      }
+    }
+  }
+</style>
