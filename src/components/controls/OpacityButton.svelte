0d4b96b3b86ba54a039f39e11e4d9afc56b01f3d
Jin Igarashi <igarashi.jin@water-gis.com>
2022-03-29 17:43:57 +0100

    feat: create new opacity component and controls (#80)

---
+++ src/components/controls/OpacityButton.svelte
@@ -0,0 +1,35 @@
+<script lang="ts">
+  import Fa from 'svelte-fa'
+  import { faDroplet } from '@fortawesome/free-solid-svg-icons/faDroplet'
+
+  export let isOpacityPanelVisible = false
+</script>
+
+<div
+  class={isOpacityPanelVisible ? 'icon-selected' : 'icon'}
+  style="margin-right: 6px;"
+  on:click={() => {
+    isOpacityPanelVisible = !isOpacityPanelVisible
+  }}>
+  <Fa icon={faDroplet} size="1x" />
+</div>
+
+<style lang="scss">
+  .icon {
+    opacity: 0.5;
+    display: inline;
+    cursor: pointer;
+    margin-right: 10px;
+
+    &:hover {
+      opacity: 1;
+    }
+  }
+
+  .icon-selected {
+    opacity: 1;
+    display: inline;
+    cursor: pointer;
+    margin-right: 10px;
+  }
+</style>
