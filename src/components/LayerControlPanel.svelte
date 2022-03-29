e326e04efda708c41ad443f4547dbf8b0efa3eb5
Jin Igarashi <igarashi.jin@water-gis.com>
2022-03-29 14:35:50 +0100

    feat: add vector layer component

---
+++ src/components/LayerControlPanel.svelte
@@ -0,0 +1,156 @@
+<script lang="ts" context="module">
+  const dynamicLayerIds = {}
+  const layerState = {}
+  const sectionState = {}
+</script>
+
+<script lang="ts">
+  import Button, { Label as LabelButton } from '@smui/button'
+  import Dialog, { Title, Content as ContentDialog, Actions as ActionsDialog } from '@smui/dialog'
+  import Fa from 'svelte-fa'
+  import { faChevronDown } from '@fortawesome/free-solid-svg-icons/faChevronDown'
+  import { faChevronUp } from '@fortawesome/free-solid-svg-icons/faChevronUp'
+  import { faEyeSlash } from '@fortawesome/free-solid-svg-icons/faEyeSlash'
+  import { faEye } from '@fortawesome/free-solid-svg-icons/faEye'
+  import { faToggleOn } from '@fortawesome/free-solid-svg-icons/faToggleOn'
+  import { faToggleOff } from '@fortawesome/free-solid-svg-icons/faToggleOff'
+  import { faTrash } from '@fortawesome/free-solid-svg-icons/faTrash'
+  import { cloneDeep } from 'lodash'
+
+  import { layerList, map } from '../stores'
+  import type { Layer, LayerDefinition } from '../lib/types'
+  import { LayerInitialValues } from '../lib/constants'
+
+  export let layer: Layer = LayerInitialValues
+
+  const name = layer.name
+  const layerId = layer.definition.id
+  const mapLayers = $map.getStyle().layers
+  const mapLayerByLayerId = mapLayers.filter((item: LayerDefinition) => item.id == layerId).pop()
+
+  export let mapLayerIndex = mapLayers.indexOf(mapLayerByLayerId)
+
+  let queryInfoEnabled = true
+  let confirmDeleteLayerDialogVisible = false
+  let isLayerVisible = false
+
+  $: visibility = isLayerVisible ? 'visible' : 'none'
+
+  const hierachyDown = (layerID: string) => {
+    const newIndex = mapLayerIndex - 1
+
+    if (newIndex >= 0) {
+      $map.moveLayer(layerID, mapLayers[newIndex].id)
+      mapLayerIndex = newIndex
+      $map.triggerRepaint()
+    }
+  }
+
+  const hierachyUp = (layerID: string) => {
+    const newIndex = mapLayerIndex + 1
+
+    if (newIndex <= mapLayers.length - 1) {
+      $map.moveLayer(layerID, mapLayers[newIndex].id)
+      mapLayerIndex = newIndex
+      $map.triggerRepaint()
+    }
+  }
+
+  const toggleVisibility = () => {
+    isLayerVisible = !isLayerVisible
+    if (!$map.getLayer(layerId)) {
+      $map.addLayer(layer.definition)
+    }
+    $map.setLayoutProperty(layerId, 'visibility', visibility)
+  }
+
+  const removeLayer = () => {
+    hideLayerControlPanel()
+
+    setTimeout(() => {
+      $map.removeLayer(layerId)
+      $layerList = $layerList.filter((item) => item.definition.id !== layerId)
+      delete layerState[layerId]
+      delete sectionState[layerId]
+      delete dynamicLayerIds[layerId]
+    }, 200)
+  }
+
+  const hideLayerControlPanel = () => {
+    confirmDeleteLayerDialogVisible = false
+  }
+
+  const setQueryInfoEnabled = () => {
+    const layerClone = cloneDeep(layer)
+    layerClone.queryInfoEnabled = !queryInfoEnabled
+    const layerIndex = $layerList.findIndex((layer) => layer.definition.id === layerId)
+    $layerList[layerIndex] = layerClone
+    queryInfoEnabled = !queryInfoEnabled
+  }
+</script>
+
+<div class="layer-header-icons">
+  <div class="group" style="padding-right: 5px;">
+    <div title="Query Map Info" class="icon-selected" on:click={() => setQueryInfoEnabled()}>
+      <Fa icon={queryInfoEnabled ? faToggleOn : faToggleOff} size="1x" />
+    </div>
+
+    <div class="icon-selected" title="Move layer up (in map)" on:click={() => hierachyUp(layerId)}>
+      <Fa icon={faChevronUp} size="1x" />
+    </div>
+
+    <div class="icon-selected" title="Move layer down (in map)" on:click={() => hierachyDown(layerId)}>
+      <Fa icon={faChevronDown} size="1x" />
+    </div>
+
+    <div class="icon-selected" title="Show/hide layer" on:click={() => toggleVisibility()}>
+      <Fa icon={visibility === 'none' ? faEyeSlash : faEye} size="1x" />
+    </div>
+    <div
+      class="icon-selected"
+      style="margin-right: 0;"
+      title="Delete layer"
+      on:click={() => (confirmDeleteLayerDialogVisible = true)}>
+      <Fa icon={faTrash} size="1x" />
+    </div>
+  </div>
+</div>
+
+<Dialog bind:open={confirmDeleteLayerDialogVisible}>
+  <Title>Delete Layer</Title>
+  <ContentDialog>
+    Are you sure you want to delete this layer?<br /><br />
+    {name}
+  </ContentDialog>
+  <ActionsDialog>
+    <Button>
+      <LabelButton>No</LabelButton>
+    </Button>
+    <Button on:click={() => removeLayer()}>
+      <LabelButton>Yes</LabelButton>
+    </Button>
+  </ActionsDialog>
+</Dialog>
+
+<style lang="scss">
+  .layer-header-icons {
+    display: flex;
+    justify-content: left;
+    align-items: center;
+    gap: 15px;
+
+    .group {
+      background: #f0f0f0;
+      border-radius: 7.5px;
+      padding: 5px;
+      padding-right: 0;
+
+      .icon-selected {
+        opacity: 1;
+        display: inline;
+        cursor: pointer;
+        margin-right: 10px;
+      }
+    }
+  }
+</style>
---
+++ src/components/LayerName.svelte
@@ -0,0 +1,56 @@
+<script lang="ts">
+  import Tag from 'svelma/src/components/Tag/Tag.svelte'
+
+  import { map } from '../stores'
+  import type { Layer, LayerDefinition } from '../lib/types'
+  import { LayerInitialValues } from '../lib/constants'
+  export let layer: Layer = LayerInitialValues
+
+  const name = layer.name
+  const layerId = layer.definition.id
+  const mapLayers = $map.getStyle().layers
+  const mapLayerByLayerId = mapLayers.filter((item: LayerDefinition) => item.id == layerId).pop()
+
+  export let mapLayerIndex = mapLayers.indexOf(mapLayerByLayerId)
+  let mapLayerLength = mapLayers.length - 1
+</script>
+
+<div class="layer-header">
+  <div>
+    <div class="layer-header-name">
+      <div class="layer-name">
+        {name}
+      </div>
+      <div class="unread-count">
+        <div style="float: right;">
+          <Tag type="is-info" size="is-small">{mapLayerIndex} / {mapLayerLength}</Tag>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+<slot />
+
+<style lang="scss">
+  .layer-header {
+    .layer-header-name {
+      display: flex;
+      justify-content: left;
+      align-items: center;
+      font-family: ProximaNova, sans-serif;
+      height: 20px;
+
+      .layer-name {
+        white-space: nowrap;
+        overflow: hidden;
+        text-overflow: ellipsis;
+        width: 100%;
+        font-size: 14px;
+      }
+
+      .unread-count {
+        padding-left: 7.5px;
+      }
+    }
+  }
+</style>
