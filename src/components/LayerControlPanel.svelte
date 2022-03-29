ad15ee0eaa466c9aeadb0c79ebf9b415d2132bef
Jin Igarashi <igarashi.jin@water-gis.com>
2022-03-29 19:59:59 +0100

    created QueryMapInfo component

--- src/components/LayerControlPanel.svelte
+++ src/components/LayerControlPanel.svelte
@@ -1,38 +1,18 @@
 <script lang="ts">
-  import Fa from 'svelte-fa'
-  import { faToggleOn } from '@fortawesome/free-solid-svg-icons/faToggleOn'
-  import { faToggleOff } from '@fortawesome/free-solid-svg-icons/faToggleOff'
-  import { cloneDeep } from 'lodash'
-
-  import { layerList } from '../stores'
   import type { Layer } from '../lib/types'
   import { LayerInitialValues } from '../lib/constants'
   import DeleteButton from './controls/DeleteButton.svelte'
   import VisibilityButton from './controls/VisibilityButton.svelte'
   import LayerOrderButtons from './controls/LayerOrderButtons.svelte'
+  import QueryMapInfoButton from './controls/QueryMapInfoButton.svelte'
 
   export let layer: Layer = LayerInitialValues
-
-  const layerId = layer.definition.id
-
   export let mapLayerIndex
-
-  let queryInfoEnabled = true
-
-  const setQueryInfoEnabled = () => {
-    const layerClone = cloneDeep(layer)
-    layerClone.queryInfoEnabled = !queryInfoEnabled
-    const layerIndex = $layerList.findIndex((layer) => layer.definition.id === layerId)
-    $layerList[layerIndex] = layerClone
-    queryInfoEnabled = !queryInfoEnabled
-  }
 </script>
 
 <div class="layer-header-icons">
   <div class="group" style="padding-right: 5px;">
-    <div title="Query Map Info" class="icon-selected" on:click={() => setQueryInfoEnabled()}>
-      <Fa icon={queryInfoEnabled ? faToggleOn : faToggleOff} size="1x" />
-    </div>
+    <QueryMapInfoButton {layer} />
     <LayerOrderButtons {layer} bind:mapLayerIndex />
     <VisibilityButton {layer} />
     <DeleteButton {layer} />
@@ -51,13 +31,6 @@
       border-radius: 7.5px;
       padding: 5px;
       padding-right: 0;
-
-      .icon-selected {
-        opacity: 1;
-        display: inline;
-        cursor: pointer;
-        margin-right: 10px;
-      }
     }
   }
 </style>
