61ec87d7ab53e7190416ca4256f35efd60c3b75c
Chris Foo <foochris@gmail.com>
2022-04-04 16:45:09 -0400

    feat: add vector features display to query info panel (#118)

--- src/components/MapQueryInfoPanel.svelte
+++ src/components/MapQueryInfoPanel.svelte
@@ -1,5 +1,8 @@
 <script lang="ts">
   import { onMount } from 'svelte'
+  import { slide } from 'svelte/transition'
+  import { faChevronDown } from '@fortawesome/free-solid-svg-icons/faChevronDown'
+  import { faChevronUp } from '@fortawesome/free-solid-svg-icons/faChevronUp'
   import { faDownload } from '@fortawesome/free-solid-svg-icons/faDownload'
   import { faSquareCheck } from '@fortawesome/free-solid-svg-icons/faSquareCheck'
   import { faSquare } from '@fortawesome/free-regular-svg-icons/faSquare'
@@ -8,12 +11,13 @@
   import GeoJSON from 'geojson'
   import maplibregl, { Map, MapMouseEvent, Marker } from 'maplibre-gl'
   import Moveable from 'moveable'
+  import { draggable } from '@neodrag/svelte'
   import PapaParse from 'papaparse'
   import Fa from 'svelte-fa'
 
   import { layerList, map } from '../stores'
   import type { Layer } from '../lib/types'
-  import { LayerTypes } from '$lib/constants'
+  import { LayerIconTypes, LayerTypes } from '$lib/constants'
 
   export let mapMouseEvent: MapMouseEvent
 
@@ -24,15 +28,17 @@
   let marker: Marker
 
   const iconSize = 'lg'
+  const noDataLabel = 'N/A'
   const frame = { translate: [0, 0] }
 
   // layer change
   $: {
     const layersVisible = $layerList.filter((layer) => layer.visible === true)
+    layerValuesExpanded = []
 
     if ($map !== null) {
       if (layersVisible.length > 0) {
-        if ($map.hasControl(mapQueryInfoControl) === false) {
+        if (mapQueryInfoControl !== null && $map.hasControl(mapQueryInfoControl) === false) {
           $map.addControl(mapQueryInfoControl, 'top-right')
         }
       } else {
@@ -82,12 +88,12 @@
 
     for (const layer of layersVisible) {
       let values = []
+
       if (layer.type === LayerTypes.RASTER) {
         const layerData = await fetch(`${titilerApiUrl}/point/${lng},${lat}?url=${layer.url}`).then((res) => {
           return res.json()
         })
 
-        // check for no data value
         let layerHasNoDataValue = false
 
         if (Object.prototype.hasOwnProperty.call(layerData, 'detail')) layerHasNoDataValue = true
@@ -98,10 +104,19 @@
           }
         }
 
-        values = layerHasNoDataValue ? null : layerData.values
+        values = layerHasNoDataValue ? [] : layerData.values
+      } else if (layer.type === LayerTypes.VECTOR) {
+        const features = $map
+          .queryRenderedFeatures([mapMouseEvent.lngLat.lng, mapMouseEvent.lngLat.lat])
+          .filter((feature) => feature.layer.id === layer.definition.id)
+
+        if (features.length > 0) {
+          const feature = features[0]
+          values = feature.properties
+        }
       }
 
-      layerValuesDataTmp = [...[{ name: layer.name, lat, lng, values }], ...layerValuesDataTmp]
+      layerValuesDataTmp = [...[{ name: layer.name, lat, lng, type: layer.type, values }], ...layerValuesDataTmp]
     }
 
     layerValuesData = layerValuesDataTmp
@@ -116,11 +131,26 @@
       ['Latitude', 'Longitude'],
       [mapMouseEvent.lngLat?.lat, mapMouseEvent.lngLat?.lng],
       [],
-      ['Layer Name', 'Values'],
+      ['Layer Name', 'Layer Type', 'Property', 'Value'],
     ]
 
     layerValuesData.forEach((layerValue) => {
-      data.push([layerValue.name, layerValue.values])
+      if (layerValue.type === LayerTypes.RASTER) {
+        data.push([
+          layerValue.name,
+          layerValue.type,
+          '',
+          layerValue.values.length > 0 ? layerValue.values : noDataLabel,
+        ])
+      } else if (layerValue.type === LayerTypes.VECTOR) {
+        if (layerValue.values.length === 0) {
+          data.push([layerValue.name, layerValue.type, 'N/A', 'N/A'])
+        } else {
+          Object.keys(layerValue.values).forEach((key) => {
+            data.push([layerValue.name, layerValue.type, key, layerValue.values[key]])
+          })
+        }
+      }
     })
 
     const filename = `undp-geohub-layers-data-${new Date().toISOString().split('T')[0]}.csv`
@@ -165,7 +195,7 @@
     this.container.appendChild(this.queryInfoContainer)
 
     this.button = document.createElement('button')
-    this.button.classList.add('mapboxgl-ctrl-icon', 'mapboxgl-query-info-control')
+    this.button.classList.add('mapboxgl-query-info-control')
     this.button.type = 'button'
     this.button.addEventListener('click', () => {
       if (isDataContainerVisible === false) {
@@ -193,7 +223,7 @@
 
     new Moveable(document.body, {
       className: 'moveable-control',
-      draggable: true,
+      draggable: false,
       dragTarget: document.getElementById('header'),
       edgeDraggable: true,
       hideDefaultLines: true,
@@ -224,9 +254,15 @@
         e.target.style.transform = `translate(${e.drag.beforeTranslate[0]}px, ${e.drag.beforeTranslate[1]}px)`
       })
   })
+
+  let layerValuesExpanded = []
 </script>
 
-<div id="data-container" class="data-container target" hidden={!isDataContainerVisible}>
+<div
+  id="data-container"
+  class="data-container target"
+  hidden={!isDataContainerVisible}
+  use:draggable={{ bounds: document.getElementById('map'), handle: '.header' }}>
   <div id="header" class="header">
     <div class="name">Query Information</div>
 
@@ -258,13 +294,13 @@
         </thead>
         <tbody>
           <tr>
-            <td class="first-column">{mapMouseEvent?.lngLat?.lat ? mapMouseEvent?.lngLat?.lat : 'N/A'}</td>
-            <td class="second-column">{mapMouseEvent?.lngLat?.lng ? mapMouseEvent?.lngLat?.lng : 'N/A'}</td>
+            <td class="first-column">{mapMouseEvent?.lngLat?.lat ? mapMouseEvent?.lngLat?.lat : noDataLabel}</td>
+            <td class="second-column">{mapMouseEvent?.lngLat?.lng ? mapMouseEvent?.lngLat?.lng : noDataLabel}</td>
           </tr>
         </tbody>
       </table>
 
-      <table class="table is-fullwidth is-striped data-values">
+      <table class="table is-fullwidth is-striped">
         <thead>
           <tr>
             <th>Layer Name</th>
@@ -272,14 +308,22 @@
           </tr>
         </thead>
         <tbody>
-          {#if layerValuesData.length > 0}
-            {#each layerValuesData as layerValue}
-              <tr>
-                <td class="first-column">{layerValue.name}</td>
-
-                {#if layerValue.values === null}
-                  <td class="second-column"> N/A </td>
-                {:else if isValuesRounded === true}
+          {#each layerValuesData as layerValue, i (layerValue)}
+            {@const layerIconType = LayerIconTypes.find((iconType) => iconType.id === layerValue.type)}
+            {@const layerIconTypeLabel = `${layerIconType.label} Layer`}
+            <tr style={layerValuesExpanded[i] === true ? 'border-top: 0;' : ''}>
+              <td class="first-column">
+                <div class="icon" alt={layerIconTypeLabel} title={layerIconTypeLabel}>
+                  <Fa icon={layerIconType.icon} size="sm" primaryColor={layerIconType.color} />&nbsp;
+                </div>
+                <div class="name">
+                  {layerValue.name}
+                </div>
+              </td>
+              {#if layerValue.values && layerValue.values.length === 0 && (layerValue.type === LayerTypes.RASTER || layerValue.type === LayerTypes.VECTOR)}
+                <td class="second-column"> N/A </td>
+              {:else if layerValue.type === LayerTypes.RASTER}
+                {#if isValuesRounded === true}
                   <td class="second-column">
                     {layerValue.values
                       .map((val) => (Math.round((val + Number.EPSILON) * 100) / 100).toFixed(2))
@@ -290,14 +334,38 @@
                     {layerValue.values.join(', ')}
                   </td>
                 {/if}
-              </tr>
-            {/each}
-          {:else}
-            <tr>
-              <td class="first-column">N/A</td>
-              <td class="second-column">N/A</td>
+              {:else if layerValue.type === LayerTypes.VECTOR}
+                <td class="second-column">
+                  <div class="expand-collapse" on:click={() => (layerValuesExpanded[i] = !layerValuesExpanded[i])}>
+                    {#if layerValuesExpanded[i] === undefined || layerValuesExpanded[i] === false}
+                      <Fa icon={faChevronDown} size="sm" />
+                    {:else}
+                      <Fa icon={faChevronUp} size="sm" />
+                    {/if}
+                  </div>
+                </td>
+              {/if}
             </tr>
-          {/if}
+
+            {#if layerValuesExpanded[i] === true && layerValue.type !== LayerTypes.RASTER}
+              <tr style={layerValuesExpanded[i] === true ? 'border-top: 0;' : ''}>
+                <td colspan="2">
+                  <div class="expanded-container" transition:slide={{ duration: 750 }}>
+                    <table class="table is-bordered is-striped is-narrow is-hoverable is-fullwidth">
+                      <tbody>
+                        {#each Object.keys(layerValue.values) as layerValueRow}
+                          <tr>
+                            <td class="name">{layerValueRow}</td>
+                            <td class="value">{layerValue.values[layerValueRow]}</td>
+                          </tr>
+                        {/each}
+                      </tbody>
+                    </table>
+                  </div>
+                </td>
+              </tr>
+            {/if}
+          {/each}
         </tbody>
       </table>
     </div>
@@ -343,20 +411,20 @@
 
 <style lang="scss">
   :global(.moveable-control) {
-    position: absolute;
-    width: 34px;
-    height: 34px;
+    background: none !important ;
     border-radius: 50%;
     border: 0 !important;
     box-sizing: none !important;
-    background: none !important ;
-    margin-top: -7px;
+    height: 34px;
     margin-left: -7px;
+    margin-top: -7px;
+    position: absolute;
+    width: 34px;
     z-index: 10;
   }
 
   .data-container {
-    background-color: #f9f9f9;
+    background-color: #fff;
     border-radius: 10px;
     border: 1px solid #ccc;
     bottom: 50px;
@@ -365,7 +433,7 @@
     font-size: 11px;
     left: 10px;
     min-height: 250px;
-    min-width: 300px;
+    min-width: 325px;
     padding: 10px;
     position: absolute;
     width: 325px;
@@ -404,22 +472,26 @@
       margin-top: 15px;
       margin-bottom: 15px;
 
-      table {
-        background-color: #f9f9f9;
-      }
-
       .coordinates {
         margin: 0;
       }
 
-      .data-values {
-        margin-bottom: 10px;
-      }
       .first-column {
-        overflow: hidden;
-        text-overflow: ellipsis;
-        white-space: nowrap;
-        width: 200px;
+        align-items: center;
+        display: flex;
+        justify-content: left;
+
+        .icon {
+          margin-right: 5px;
+          width: 20px;
+        }
+
+        .name {
+          overflow: hidden;
+          text-overflow: ellipsis;
+          white-space: nowrap;
+          width: 200px;
+        }
       }
       .second-column {
         overflow: hidden;
@@ -427,6 +499,30 @@
         text-overflow: ellipsis;
         white-space: nowrap;
         width: 125px;
+
+        .expand-collapse {
+          cursor: pointer;
+          margin-right: 2px;
+          opacity: 0.5;
+
+          &:hover {
+            opacity: 1;
+          }
+        }
+      }
+      .expanded-container {
+        padding-left: 25px;
+        max-height: 150px;
+        overflow-y: auto;
+
+        td.name,
+        td.value {
+          width: 50%;
+        }
+
+        td.value {
+          text-align: right;
+        }
       }
     }
 
@@ -462,10 +558,14 @@
     }
   }
 
+  .table.is-striped tbody tr:not(.is-selected):nth-child(even) {
+    background-color: #fafafa;
+  }
+
   :global(.mapboxgl-query-info-control) {
+    background-position: center !important;
+    background-repeat: no-repeat !important;
+    background-size: 75% !important;
     background: url('data:image/svg+xml;charset=UTF-8,<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><!-- Font Awesome Pro 5.15.4 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) --><path d="M256 8C119.043 8 8 119.083 8 256c0 136.997 111.043 248 248 248s248-111.003 248-248C504 119.083 392.957 8 256 8zm0 110c23.196 0 42 18.804 42 42s-18.804 42-42 42-42-18.804-42-42 18.804-42 42-42zm56 254c0 6.627-5.373 12-12 12h-88c-6.627 0-12-5.373-12-12v-24c0-6.627 5.373-12 12-12h12v-64h-12c-6.627 0-12-5.373-12-12v-24c0-6.627 5.373-12 12-12h64c6.627 0 12 5.373 12 12v100h12c6.627 0 12 5.373 12 12v24z"/></svg>');
-    background-position: center;
-    background-repeat: no-repeat;
-    background-size: 70%;
   }
 </style>
