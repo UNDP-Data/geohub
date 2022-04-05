20f65d2afa64d743f2c4d64a5f5f185022bfe917
Jin Igarashi <igarashi.jin@water-gis.com>
2022-04-05 17:02:21 +0100

    created a component for vector legend symbol

---
+++ src/components/controls/VectorLegendSymbol.svelte
@@ -0,0 +1,119 @@
+<script lang="ts">
+  import { onMount } from 'svelte'
+  import { map } from '../../stores'
+  import type { Layer } from '../../lib/types'
+  import type { LayerSpecification } from '@maplibre/maplibre-gl-style-spec/types'
+  import { LayerInitialValues } from '../../lib/constants'
+  import LegendSymbol from '@watergis/legend-symbol'
+
+  export let layer: Layer = LayerInitialValues
+
+  const layerId = layer.definition.id
+  const zoom = $map.getZoom()
+  let sprite = {
+    image: HTMLImageElement,
+    json: JSON,
+  }
+
+  let legendSymbolContainer: HTMLElement
+
+  const setSprite = (image: any, json: JSON) => {
+    sprite = {
+      image,
+      json,
+    }
+  }
+
+  const loadImage = (url: string) => {
+    let cancelled = false
+    const promise = new Promise((resolve, reject) => {
+      const img = new Image()
+      img.crossOrigin = 'Anonymous'
+      img.onload = () => {
+        if (!cancelled) resolve(img)
+      }
+      img.onerror = (e) => {
+        if (!cancelled) reject(e)
+      }
+      img.src = url
+    })
+    return promise
+  }
+
+  const loadJson = async (url: string) => {
+    return fetch(url).then((data) => data.json())
+  }
+
+  const styleUrl = $map.getStyle().sprite
+  const promise = Promise.all([loadImage(`${styleUrl}.png`), loadJson(`${styleUrl}.json`)])
+
+  onMount(async () => {
+    await promise.then(([image, json]) => {
+      setSprite(image, json)
+    })
+    updateLegend()
+  })
+
+  export const updateLegend = () => {
+    const mapLayers = $map.getStyle().layers
+    const mapLayerByLayerId = mapLayers.find((item: LayerSpecification) => item.id === layerId)
+
+    let symbol = LegendSymbol({ sprite: sprite, zoom: zoom, layer: mapLayerByLayerId })
+    legendSymbolContainer.innerHTML = ''
+    if (symbol) {
+      switch (symbol.element) {
+        case 'div': {
+          const div = document.createElement('div')
+          if (
+            symbol.attributes.style.backgroundImage &&
+            !['url(undefined)', 'url(null)'].includes(symbol.attributes.style.backgroundImage)
+          ) {
+            const img = document.createElement('img')
+            img.src = symbol.attributes.style.backgroundImage.replace('url(', '').replace(')', '')
+            img.alt = layerId
+            img.style.cssText = `height: 24px;`
+            div.appendChild(img)
+          }
+          const divBackground = document.createElement('div')
+          divBackground.style.height = '24px'
+          divBackground.style.width = '50px'
+          divBackground.style.backgroundColor = symbol.attributes.style.backgroundColor
+          divBackground.style.backgroundPosition = symbol.attributes.style.backgroundPosition
+          divBackground.style.backgroundSize = symbol.attributes.style.backgroundSize
+          divBackground.style.backgroundRepeat = symbol.attributes.style.backgroundRepeat
+          divBackground.style.opacity = symbol.attributes.style.opacity
+          div.appendChild(divBackground)
+          legendSymbolContainer.appendChild(div)
+          break
+        }
+        case 'svg': {
+          const svg = document.createElementNS('http://www.w3.org/2000/svg', 'svg')
+          svg.style.cssText = 'height: 24px;'
+          svg.setAttributeNS(null, 'version', '1.1')
+          Object.keys(symbol.attributes).forEach((k) => {
+            svg.setAttribute(k, symbol.attributes[k])
+            let group = document.createElementNS('http://www.w3.org/2000/svg', 'g')
+            symbol.children.forEach((child) => {
+              var c = document.createElementNS('http://www.w3.org/2000/svg', child.element)
+              Object.keys(child.attributes).forEach((k2) => {
+                c.setAttributeNS(null, k2, child.attributes[k2])
+              })
+              group.appendChild(c)
+            })
+            svg.appendChild(group)
+          })
+          legendSymbolContainer.appendChild(svg)
+          break
+        }
+        default: {
+          break
+        }
+      }
+    }
+  }
+</script>
+
+<div bind:this={legendSymbolContainer} />
+
+<style lang="scss">
+</style>
