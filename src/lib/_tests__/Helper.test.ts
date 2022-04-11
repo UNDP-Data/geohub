3ab17ba15ca5429cf7faa602ce37a2cf414979e3
foochris <foochris@gmail.com>
2022-04-11 14:43:59 -0400

    test: reimplement test for fetchUrl

--- src/lib/_tests__/Helper.test.ts
+++ src/lib/_tests__/Helper.test.ts
@@ -18,45 +18,45 @@ describe('stringifyStyleJSON', () => {
   })
 })
 
-// describe('fetchUrl', () => {
-//   beforeEach(() => {
-//     jest.resetAllMocks()
-//   })
+describe('fetchUrl', () => {
+  beforeEach(() => {
+    jest.resetAllMocks()
+  })
 
-//   it('should return a json object upon no timeout ', async () => {
-//     const { Response, Headers } = jest.requireActual('node-fetch')
-//     const meta = {
-//       'Content-Type': 'application/json',
-//       Accept: '*/*',
-//       'Breaking-Bad': '<3',
-//     }
+  it('should return a json object upon no timeout ', async () => {
+    const { Response, Headers } = jest.requireActual('node-fetch')
+    const meta = {
+      'Content-Type': 'application/json',
+      Accept: '*/*',
+      'Breaking-Bad': '<3',
+    }
 
-//     const headers = new Headers(meta)
-//     const ResponseInit = {
-//       status: 200,
-//       statusText: 'fail',
-//       headers,
-//     }
+    const headers = new Headers(meta)
+    const ResponseInit = {
+      status: 200,
+      statusText: 'fail',
+      headers,
+    }
 
-//     const response = new Response(JSON.stringify({ data: {} }), ResponseInit)
-//     const fetchWithNoTimeout = jest.spyOn(helper, 'fetchWithTimeout').mockReturnValue(Promise.resolve(response))
-//     const json = await helper.fetchUrl('http://www.google.com')
-//     expect(json).toEqual({ data: {} })
-//     expect(fetchWithNoTimeout).toHaveBeenCalledTimes(1)
-//   })
+    const response = new Response(JSON.stringify({ data: {} }), ResponseInit)
+    const fetchWithNoTimeout = jest.spyOn(helper, 'fetchWithTimeout').mockReturnValue(Promise.resolve(response))
+    const json = await helper.fetchUrl('http://www.google.com')
+    expect(json).toEqual({ data: {} })
+    expect(fetchWithNoTimeout).toHaveBeenCalledTimes(1)
+  })
 
-//   it('should return no json object and a banner message store should be updated upon timeout', async () => {
-//     const fetchWithTimeout = jest.spyOn(helper, 'fetchWithTimeout').mockRejectedValue(new Error('TIME_OUT'))
-//     const json = await helper.fetchUrl('http://www.google.com')
-//     expect(json).toBeNull()
-//     expect(fetchWithTimeout).toHaveBeenCalledTimes(1)
+  it('should return no json object and a banner message store should be updated upon timeout', async () => {
+    const fetchWithTimeout = jest.spyOn(helper, 'fetchWithTimeout').mockRejectedValue(new Error('TIME_OUT'))
+    const json = await helper.fetchUrl('http://www.google.com')
+    expect(json).toBeNull()
+    expect(fetchWithTimeout).toHaveBeenCalledTimes(1)
 
-//     bannerMessages.subscribe((messages) => {
-//       expect(messages).toHaveLength(1)
-//       const message: BannerMessage = messages[0]
-//       expect(message.type).toEqual('danger')
-//       expect(message.title).toEqual('Whoops! Something went wrong.')
-//       expect(message.message).toEqual('The request took longer than expected. Please try again later.')
-//     })
-//   })
-// })
+    bannerMessages.subscribe((messages) => {
+      expect(messages).toHaveLength(1)
+      const message: BannerMessage = messages[0]
+      expect(message.type).toEqual('danger')
+      expect(message.title).toEqual('Whoops! Something went wrong.')
+      expect(message.message).toEqual('The request took longer than expected. Please try again later.')
+    })
+  })
+})
