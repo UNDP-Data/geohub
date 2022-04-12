611c4c7742100d291391fddee3e684428dbc15dc
Chris Foo <foochris@gmail.com>
2022-04-11 23:09:01 -0400

    test: switch to vitest instead of jest (#157)

--- src/components/__tests__/TestExample.test.ts
+++ src/components/__tests__/TestExample.test.ts
@@ -1,23 +1,20 @@
-// NOTE: jest-dom adds handy assertions to Jest and it is recommended, but not required.
-import '@testing-library/jest-dom'
+import { beforeEach, expect, it } from 'vitest'
+import { cleanup, render, fireEvent } from '@testing-library/svelte'
 
-import { render, fireEvent } from '@testing-library/svelte'
+import TestExample from '../TestExample.svelte'
 
-import Comp from '../TestExample.svelte'
+beforeEach(cleanup)
 
-test('shows proper heading when rendered', () => {
-  const { getByText } = render(Comp, { name: 'World' })
-  expect(getByText('Hello World!')).toBeInTheDocument()
+it('shows proper heading when rendered', () => {
+  const { getByText } = render(TestExample, { name: 'World' })
+  expect(getByText('Hello World!')).toBeDefined()
 })
 
-// Note: This is as an async test as we are using `fireEvent`
-test('changes button text on click', async () => {
-  const { getByText } = render(Comp, { name: 'World' })
+it('changes button text on click', async () => {
+  const { getByText } = render(TestExample, { name: 'World' })
   const button = getByText('Button')
 
-  // Using await when firing events is unique to the svelte testing library because
-  // we have to wait for the next `tick` so that Svelte flushes all pending state changes.
   await fireEvent.click(button)
 
-  expect(button).toHaveTextContent('Button Clicked')
+  expect(button.innerHTML).toContain('Button Clicked')
 })
