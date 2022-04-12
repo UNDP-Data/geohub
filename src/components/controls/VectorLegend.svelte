d34c66220bc36a5c1b21582a791bd10155c74ac4
Jin Igarashi <jin.igarashi@undp.org>
2022-04-12 20:04:23 +0100

    added control changing icon-color

--- src/components/controls/VectorLegend.svelte
+++ src/components/controls/VectorLegend.svelte
@@ -23,6 +23,7 @@
   import IconKeepUpright from './vector-styles/IconKeepUpright.svelte'
   import IconIgnorePlacement from './vector-styles/IconIgnorePlacement.svelte'
   import IconOffset from './vector-styles/IconOffset.svelte'
+  import IconColor from './vector-styles/IconColor.svelte'
 
   export let layer: Layer = LayerInitialValues
   let updateLegend = () => undefined
@@ -72,6 +73,7 @@
   <FillColor on:change={onStyleChange} {layer} />
   <FillOutlineColor on:change={onStyleChange} {layer} />
   <IconImage on:change={onStyleChange} {layer} />
+  <IconColor on:change={onStyleChange} {layer} />
   <IconSize on:change={onStyleChange} {layer} />
   <IconOverlap on:change={onStyleChange} {layer} />
   <IconKeepUpright on:change={onStyleChange} {layer} />
