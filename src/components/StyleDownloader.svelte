a1fee09b931e9da22d10f454cf9cca375ba67dea
Jin Igarashi <igarashi.jin@water-gis.com>
2022-04-19 18:42:32 +0100

    added options for download dialog

--- src/components/StyleDownloader.svelte
+++ src/components/StyleDownloader.svelte
@@ -1,25 +1,121 @@
 <script lang="ts">
+  import Button, { Label as LabelButton } from '@smui/button'
+  import Dialog, { Title, Content as ContentDialog, Actions as ActionsDialog, InitialFocus } from '@smui/dialog'
+  import List, { Item, Graphic, Text } from '@smui/list'
+  import Radio from '@smui/radio'
+  import Textfield from '@smui/textfield'
   import Fa from 'svelte-fa'
   import { faDownload } from '@fortawesome/free-solid-svg-icons/faDownload'
 
-  import { map } from '../stores'
+  import { map, layerList } from '../stores'
   import { downloadFile } from '$lib/helper'
+  import type { StyleSpecification } from '@maplibre/maplibre-gl-style-spec/types'
+
+  let dialogOpen = false
+  let selectedOption = 'all'
+  let radioDisabled = false
+  let fileName = 'style.json'
+  let styleName = 'UNDP GeoHub style'
+
+  const open = () => {
+    selectedOption = 'all'
+    radioDisabled = false
+    if ($layerList.length === 0) {
+      radioDisabled = true
+    }
+    dialogOpen = !dialogOpen
+  }
 
   export const download = () => {
-    const style = $map.getStyle()
+    let style: StyleSpecification = $map.getStyle()
+    if (selectedOption === 'geohub') {
+      if ($layerList.length === 0) {
+        return
+      }
+      const newSources = {}
+      Object.keys(style.sources).forEach((key) => {
+        $layerList.forEach((l) => {
+          if (l.definition && l.definition.source === key) {
+            newSources[key] = style.sources[key]
+          }
+        })
+      })
+      style.sources = newSources
+      const newLayers = []
+      style.layers.forEach((layer) => {
+        $layerList.forEach((l) => {
+          if (l.definition.id === layer.id) {
+            newLayers.push(layer)
+          }
+        })
+      })
+      style.layers = newLayers
+    }
+    style.name = styleName
+    const center = $map.getCenter()
+    style.center = [center.lng, center.lat]
+    style.bearing = $map.getBearing()
+    style.pitch = $map.getPitch()
+
     const json = JSON.stringify(style, null, 2)
-    const filename = `undp-geohub-map-layers-specification-${new Date().toISOString().split('T')[0]}.json`
-    downloadFile(filename, json)
+    downloadFile(fileName, json)
+    dialogOpen = false
+  }
+
+  function closeHandler(e: CustomEvent<{ action: string }>) {
+    if (e.detail.action === 'accept') {
+      download()
+    }
   }
 </script>
 
-<div class="icon" on:click={() => download()}>
+<div class="icon" on:click={() => open()}>
   <Fa icon={faDownload} size="lg" />
 </div>
+
+<Dialog bind:open={dialogOpen} selection on:SMUIDialog:closed={closeHandler}>
+  <Title>Download</Title>
+  <ContentDialog>
+    <div class="textfield">
+      <Textfield bind:value={styleName} label="Style name" />
+    </div>
+    <div class="textfield">
+      <Textfield bind:value={fileName} label="Output file name" />
+    </div>
+    {#if radioDisabled === false}
+      <List radioList>
+        <Item use={[InitialFocus]}>
+          <Graphic>
+            <Radio bind:group={selectedOption} value="all" />
+          </Graphic>
+          <Text>All layers</Text>
+        </Item>
+
+        <Item use={[InitialFocus]}>
+          <Graphic>
+            <Radio bind:group={selectedOption} value="geohub" disabled={radioDisabled} />
+          </Graphic>
+          <Text>Only Geohub layers</Text>
+        </Item>
+      </List>
+    {/if}
+  </ContentDialog>
+  <ActionsDialog>
+    <Button>
+      <LabelButton>Cancel</LabelButton>
+    </Button>
+    <Button action="accept">
+      <LabelButton>Download</LabelButton>
+    </Button>
+  </ActionsDialog>
+</Dialog>
 
 <style lang="scss">
   .icon {
     cursor: pointer;
     margin-right: 20px;
+  }
+  .textfield {
+    padding-left: 30px;
   }
 </style>
