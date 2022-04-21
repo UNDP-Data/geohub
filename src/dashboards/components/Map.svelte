a3dc4e80a0840a76209d5860af03873a6e86120a
Jin Igarashi <igarashi.jin@water-gis.com>
2022-04-21 17:44:56 +0100

    added basemap plugin and bing maps

--- src/dashboards/components/Map.svelte
+++ src/dashboards/components/Map.svelte
@@ -2,6 +2,7 @@
   import { onMount } from 'svelte'
   import { map } from '../stores'
   import { Map, NavigationControl, GeolocateControl, ScaleControl, AttributionControl } from 'maplibre-gl'
+  import BasemapsControl from 'maplibre-gl-basemaps'
 
   let newMap: Map
   let mapContainer: HTMLDivElement
@@ -25,6 +26,41 @@
     newMap.addControl(new ScaleControl({ maxWidth: 80, unit: 'metric' }), 'bottom-left')
     newMap.addControl(new AttributionControl({ compact: true }), 'bottom-right')
 
+    const BingMapsKey = import.meta.env.VITE_BINGMAP_KEY
+    newMap.addControl(
+      new BasemapsControl({
+        basemaps: [
+          {
+            id: 'basemap_World_Topo_Map',
+            tiles: ['//server.arcgisonline.com/ArcGIS/rest/services/World_Topo_Map/MapServer/tile/{z}/{y}/{x}'],
+          },
+          {
+            id: 'esri_satellite',
+            tiles: ['https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}'],
+          },
+          {
+            id: 'Bing CanvasLight',
+            tiles: [`https://dev.virtualearth.net/REST/v1/Imagery/Metadata/CanvasLight?key=${BingMapsKey}`],
+          },
+          {
+            id: 'Bing CanvasDark',
+            tiles: [`https://dev.virtualearth.net/REST/v1/Imagery/Metadata/CanvasDark?key=${BingMapsKey}`],
+          },
+          {
+            id: 'Bing Aerial',
+            tiles: [`https://dev.virtualearth.net/REST/v1/Imagery/Metadata/Aerial?key=${BingMapsKey}`],
+          },
+          {
+            id: 'Bing AerialWithLabels',
+            tiles: [`https://dev.virtualearth.net/REST/v1/Imagery/Metadata/AerialWithLabels?key=${BingMapsKey}`],
+          },
+        ],
+        initialBasemap: 'basemap_World_Topo_Map',
+        expandDirection: 'top',
+      }),
+      'bottom-left',
+    )
+
     map.update(() => newMap)
   })
 </script>
@@ -33,6 +69,7 @@
 
 <style>
   @import 'maplibre-gl/dist/maplibre-gl.css';
+  @import 'maplibre-gl-basemaps/lib/basemaps.css';
   .map {
     height: 100%;
     width: 100%;
