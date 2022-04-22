100fee9553d7b3216021502e62438cabbd7a6114
janf <ioan.ferencik@solargis.com>
2022-04-22 21:32:28 +0200

    feat: Bing aerial layer

--- src/dashboards/components/Content.svelte
+++ src/dashboards/components/Content.svelte
@@ -2,12 +2,19 @@
   import { onMount } from 'svelte'
   import Drawer, { AppContent, Content, Header } from '@smui/drawer'
   import { map } from '../stores'
-  import { TabNames } from '$lib/constants'
+  import Ripple from '@smui/ripple'
+  import { fetchUrl } from '$lib/helper'
+  import type { RasterLayerSpecification } from '@maplibre/maplibre-gl-style-spec/types'
+
+  const BingMapsKey = import.meta.env.VITE_BINGMAP_KEY
+  const aerialBingUrl = 'http://ecn.t3.tiles.virtualearth.net/tiles/a{quadkey}.jpeg?g=1'
 
   export let drawerOpen = false
 
   let drawerWidth = 355
   let isResizingDrawer = false
+  let bingAerialLayerMeta = undefined
+  let aerialBingTiles = []
 
   $: {
     if (drawerOpen) {
@@ -41,6 +48,46 @@
 
   const handleMousedown = () => (isResizingDrawer = true)
   const handleMouseup = () => (isResizingDrawer = false)
+
+  const addBingAerialLayer = async () => {
+    if (aerialBingTiles.length == 0) {
+      bingAerialLayerMeta = await fetchUrl(
+        `https://dev.virtualearth.net/REST/v1/Imagery/Metadata/Aerial?key=${BingMapsKey}`,
+      )
+      const { estimatedTotal, resources } = bingAerialLayerMeta.resourceSets[0]
+      const imageUrlSubdomains = resources[0].imageUrlSubdomains
+      aerialBingTiles = imageUrlSubdomains.map((el) => {
+        return aerialBingUrl.replace('{subdomain}', el)
+      })
+    }
+    const layerSource = {
+      type: 'raster',
+      tiles: aerialBingTiles,
+      tileSize: 256,
+      attribution: 'Layer powered by Microsoft',
+    }
+    if (!('BING' in $map.getStyle().sources)) {
+      console.log('adding Bing aerial')
+      $map.addSource('BING', layerSource)
+
+      const layerDefinition: RasterLayerSpecification = {
+        id: 'bing',
+        type: 'raster',
+        source: 'BING',
+        minzoom: 0,
+        maxzoom: 22,
+        layout: {
+          visibility: 'visible',
+        },
+      }
+      $map.addLayer(layerDefinition)
+    } else {
+      const vis = $map.getLayoutProperty('bing', 'visibility')
+      const visibility = vis === 'visible' ? 'none' : 'visible'
+      console.log(vis, visibility)
+      $map.setLayoutProperty('bing', 'visibility', visibility)
+    }
+  }
 </script>
 
 <div class="content-container">
@@ -51,7 +98,7 @@
     <div class="drawer-container">
       <div class="drawer-content" style="width: {drawerWidth - 10}px; max-width: {drawerWidth - 10}px;">
         <Header>
-          <div />
+          <p use:Ripple={{ surface: true, color: 'primary' }} tabindex="0" on:click={addBingAerialLayer}>Whatever</p>
         </Header>
         <Content style="padding-right: 15px; overflow: visible;">
           <div />
@@ -80,6 +127,15 @@
 <style lang="scss">
   @import '../../styles/bulma.css';
   @import 'https://use.fontawesome.com/releases/v6.1.1/css/all.css';
+
+  p {
+    padding: 10px;
+    border-radius: 5px;
+  }
+
+  [tabindex='0'] {
+    cursor: pointer;
+  }
 
   :global(.app-content) {
     flex: auto;
--- src/dashboards/components/Map.svelte
+++ src/dashboards/components/Map.svelte
@@ -3,10 +3,42 @@
   import { map } from '../stores'
   import { Map, NavigationControl, GeolocateControl, ScaleControl, AttributionControl } from 'maplibre-gl'
   import BasemapsControl from 'maplibre-gl-basemaps'
+  import { fetchUrl } from '$lib/helper'
 
+  const BingMapsKey = import.meta.env.VITE_BINGMAP_KEY
   let newMap: Map
   let mapContainer: HTMLDivElement
+
+  const getQuadkey = (z: number, x: number, y: number): string => {
+    let quadkey = '',
+      mask
+    for (let i = z; i > 0; i--) {
+      mask = 1 << (i - 1)
+      quadkey += (x & mask ? 1 : 0) + (y & mask ? 2 : 0)
+    }
+    return quadkey
+  }
+
+  let aerialBingTiles = []
+
   onMount(async () => {
+    const bingAerialLayerMeta = await fetchUrl(
+      `https://dev.virtualearth.net/REST/v1/Imagery/Metadata/Aerial?key=${BingMapsKey}`,
+    )
+
+    const { estimatedTotal, resources } = bingAerialLayerMeta.resourceSets[0]
+
+    // should probably warn user if estimatedTotal == 0
+    //const aerialBingUrl = resources[0].imageUrl
+    const aerialBingUrl = 'http://ecn.t3.tiles.virtualearth.net/tiles/a{quadkey}.jpeg?g=1'
+    const signedAerialBingUrl = `${aerialBingUrl}?key=${BingMapsKey}`
+    const imageUrlSubdomains = resources[0].imageUrlSubdomains
+    aerialBingTiles = imageUrlSubdomains.map((el) => {
+      return aerialBingUrl.replace('{subdomain}', el)
+    })
+    console.log(aerialBingUrl.replace('{quadkey}', getQuadkey(1, 0, 0)))
+    console.log(aerialBingTiles)
+
     newMap = new Map({
       container: mapContainer,
       style: 'https://undp-data.github.io/style/style.json',
@@ -15,6 +47,7 @@
       hash: true,
       attributionControl: false,
     })
+
     newMap.addControl(new NavigationControl({}), 'top-right')
     newMap.addControl(
       new GeolocateControl({
@@ -26,40 +59,36 @@
     newMap.addControl(new ScaleControl({ maxWidth: 80, unit: 'metric' }), 'bottom-left')
     newMap.addControl(new AttributionControl({ compact: true }), 'bottom-right')
 
-    const BingMapsKey = import.meta.env.VITE_BINGMAP_KEY
-    newMap.addControl(
-      new BasemapsControl({
-        basemaps: [
-          {
-            id: 'basemap_World_Topo_Map',
-            tiles: ['//server.arcgisonline.com/ArcGIS/rest/services/World_Topo_Map/MapServer/tile/{z}/{y}/{x}'],
-          },
-          {
-            id: 'esri_satellite',
-            tiles: ['https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}'],
-          },
-          {
-            id: 'Bing CanvasLight',
-            tiles: [`https://dev.virtualearth.net/REST/v1/Imagery/Metadata/CanvasLight?key=${BingMapsKey}`],
-          },
-          {
-            id: 'Bing CanvasDark',
-            tiles: [`https://dev.virtualearth.net/REST/v1/Imagery/Metadata/CanvasDark?key=${BingMapsKey}`],
-          },
-          {
-            id: 'Bing Aerial',
-            tiles: [`https://dev.virtualearth.net/REST/v1/Imagery/Metadata/Aerial?key=${BingMapsKey}`],
-          },
-          {
-            id: 'Bing AerialWithLabels',
-            tiles: [`https://dev.virtualearth.net/REST/v1/Imagery/Metadata/AerialWithLabels?key=${BingMapsKey}`],
-          },
-        ],
-        initialBasemap: 'basemap_World_Topo_Map',
-        expandDirection: 'top',
-      }),
-      'bottom-left',
-    )
+    // newMap.addControl(
+    //   new BasemapsControl({
+    //     basemaps: [
+    //       {
+    //         id: 'basemap_World_Topo_Map',
+    //         tiles: ['//server.arcgisonline.com/ArcGIS/rest/services/World_Topo_Map/MapServer/tile/{z}/{y}/{x}'],
+    //       },
+    //       {
+    //         id: 'esri_satellite',
+    //         tiles: ['https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}'],
+    //       },
+    //       // {
+    //       //   id: 'Bing CanvasLight',
+    //       //   tiles: [`https://dev.virtualearth.net/REST/v1/Imagery/Metadata/CanvasLight?key=${BingMapsKey}`],
+    //       // },
+    //       // {
+    //       //   id: 'Bing CanvasDark',
+    //       //   tiles: [`https://dev.virtualearth.net/REST/v1/Imagery/Metadata/CanvasDark?key=${BingMapsKey}`],
+    //       // },
+
+    //       {
+    //         id: 'Bing Aerial',
+    //         tiles: ['http://ecn.t3.tiles.virtualearth.net/tiles/a{q}.jpeg?g=1'],
+    //       },
+    //     ],
+    //     initialBasemap: 'basemap_World_Topo_Map',
+    //     expandDirection: 'top',
+    //   }),
+    //   'bottom-left',
+    // )
 
     map.update(() => newMap)
   })
