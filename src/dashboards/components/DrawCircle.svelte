534d40f34febdc7a07558324f86d1068496a3385
Jin Igarashi <jin.igarashi@undp.org>
2022-04-26 15:57:24 +0100

    implemented drag radius

--- src/dashboards/components/DrawCircle.svelte
+++ src/dashboards/components/DrawCircle.svelte
@@ -11,12 +11,15 @@
   const LAYER_LINE_SYMBOL = 'draw-circle-controls-layer-line-symbol'
   const LAYER_SYMBOL = 'draw-circle-controls-layer-symbol'
   const SOURCE_SYMBOL = 'draw-circle-controls-source-symbol'
+  const LAYER_SYMBOL_RADIUS = 'draw-circle-controls-layer-symbol-radius'
+  const SOURCE_SYMBOL_RADIUS = 'draw-circle-controls-source-symbol-radius'
   const LAYER_CIRCLE = 'draw-circle-controls-layer-circle'
   const SOURCE_CIRCLE = 'draw-circle-controls-source-circle'
   const LAYER_CIRCLE_OUTLINE = 'draw-circle-controls-layer-circle-outline'
 
   let isDrawing = false
-  let coordinates = []
+  let coordinatesCenter: number[][] = []
+  let coordinatesRadius: number[][] = []
   let units = 'meters'
 
   const drawStart = () => {
@@ -37,33 +40,37 @@
     }
     const lnglat: number[] = [event.lngLat.lng, event.lngLat.lat]
     if ($map) {
-      if (coordinates.length < 3) {
-        if (coordinates.length === 0) {
-          coordinates.push(lnglat)
-        } else {
-          coordinates[coordinates.length - 1] = lnglat
-          $map.off('click', mapClickListener)
-          $map.off('mousemove', mapMoveListener)
-          $map.getCanvas().style.cursor = ''
-        }
+      if (coordinatesCenter.length === 0) {
+        coordinatesCenter.push(lnglat)
+      } else if (coordinatesRadius.length === 0) {
+        coordinatesRadius.push(lnglat)
+      } else {
+        coordinatesRadius[coordinatesRadius.length - 1] = lnglat
+        $map.off('click', mapClickListener)
+        $map.off('mousemove', mapMoveListener)
+        $map.getCanvas().style.cursor = ''
       }
     }
   }
 
   const mapMoveListener = (event: MapMouseEvent) => {
     if (!$map) return
+    if (coordinatesCenter.length === 0) return
     const lnglat: number[] = [event.lngLat.lng, event.lngLat.lat]
-    if (coordinates.length === 1) {
-      coordinates.push(lnglat)
-    } else if (coordinates.length === 2) {
-      coordinates[coordinates.length - 1] = lnglat
+    if (coordinatesRadius.length === 0) {
+      coordinatesRadius.push(lnglat)
+    } else if (coordinatesRadius.length > 0) {
+      coordinatesRadius[coordinatesRadius.length - 1] = lnglat
     }
     // eslint-disable-next-line @typescript-eslint/ban-ts-comment
     // @ts-ignore
-    $map.getSource(SOURCE_LINE).setData(geoLineString(coordinates))
+    $map.getSource(SOURCE_LINE).setData(geoLineString([coordinatesCenter[0], coordinatesRadius[0]]))
     // eslint-disable-next-line @typescript-eslint/ban-ts-comment
     // @ts-ignore
-    $map.getSource(SOURCE_SYMBOL).setData(geoPoint(coordinates))
+    $map.getSource(SOURCE_SYMBOL).setData(geoPoint(coordinatesCenter))
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    // @ts-ignore
+    $map.getSource(SOURCE_SYMBOL_RADIUS).setData(geoPoint(coordinatesRadius))
 
     updateCircleFeature()
   }
@@ -73,24 +80,31 @@
       if ($map.getLayer(LAYER_LINE)) $map.removeLayer(LAYER_LINE)
       if ($map.getLayer(LAYER_LINE_SYMBOL)) $map.removeLayer(LAYER_LINE_SYMBOL)
       if ($map.getLayer(LAYER_SYMBOL)) $map.removeLayer(LAYER_SYMBOL)
+      if ($map.getLayer(LAYER_SYMBOL_RADIUS)) $map.removeLayer(LAYER_SYMBOL_RADIUS)
       if ($map.getLayer(LAYER_CIRCLE)) $map.removeLayer(LAYER_CIRCLE)
       if ($map.getLayer(LAYER_CIRCLE_OUTLINE)) $map.removeLayer(LAYER_CIRCLE_OUTLINE)
       if ($map.getSource(SOURCE_LINE)) $map.removeSource(SOURCE_LINE)
       if ($map.getSource(SOURCE_SYMBOL)) $map.removeSource(SOURCE_SYMBOL)
+      if ($map.getSource(SOURCE_SYMBOL_RADIUS)) $map.removeSource(SOURCE_SYMBOL_RADIUS)
       if ($map.getSource(SOURCE_CIRCLE)) $map.removeSource(SOURCE_CIRCLE)
     }
   }
 
   const initFeatures = () => {
-    coordinates = []
+    coordinatesCenter = []
+    coordinatesRadius = []
     if ($map) {
       $map.addSource(SOURCE_LINE, {
         type: 'geojson',
-        data: geoLineString(coordinates),
+        data: geoLineString([]),
       })
       $map.addSource(SOURCE_SYMBOL, {
         type: 'geojson',
-        data: geoPoint(coordinates),
+        data: geoPoint(coordinatesCenter),
+      })
+      $map.addSource(SOURCE_SYMBOL_RADIUS, {
+        type: 'geojson',
+        data: geoPoint(coordinatesRadius),
       })
 
       $map.addLayer({
@@ -136,11 +150,35 @@
           'icon-color': 'rgb(245,169,71)',
         },
       })
+      $map.addLayer({
+        id: LAYER_SYMBOL_RADIUS,
+        type: 'symbol',
+        source: SOURCE_SYMBOL_RADIUS,
+        layout: {
+          'icon-image': 'square',
+          'icon-size': 1,
+        },
+        paint: {
+          'icon-color': 'rgb(245,169,71)',
+        },
+      })
+      $map.on('mousedown', LAYER_SYMBOL_RADIUS, (e) => {
+        // Prevent the default map drag behavior.
+        e.preventDefault()
+        isDragging = true
+
+        // Set a cursor indicator
+        $map.getCanvasContainer().style.cursor = 'grab'
+
+        // Mouse events
+        $map.on('mousemove', mouseMoveOnPoint)
+        $map.once('mouseup', mouseUpOnPoint)
+      })
     }
   }
 
   const updateCircleFeature = () => {
-    const circleFeature = getGeoCircle(coordinates)
+    const circleFeature = getGeoCircle([coordinatesCenter[0], coordinatesRadius[0]])
     if (circleFeature) {
       if (!$map?.getSource(SOURCE_CIRCLE)) {
         $map.addSource(SOURCE_CIRCLE, {
@@ -262,6 +300,30 @@
     // radius used by mapbox-gl, see https://github.com/mapbox/mapbox-gl-js/blob/main/src/geo/lng_lat.js#L11
     const DEFAULT_RADIUS = 6371.0088
     return geodesyDestinationPoint(start, distance, bearing, DEFAULT_RADIUS)
+  }
+
+  let isDragging = false
+
+  const mouseMoveOnPoint = (e: MapMouseEvent) => {
+    if (!isDragging) return
+    const coords = e.lngLat
+    isDragging = true
+    $map.getCanvasContainer().style.cursor = 'grabbing'
+    coordinatesRadius = [[coords.lng, coords.lat]]
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    // @ts-ignore
+    $map.getSource(SOURCE_LINE).setData(geoLineString([coordinatesCenter[0], coordinatesRadius[0]]))
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    // @ts-ignore
+    $map.getSource(SOURCE_SYMBOL_RADIUS).setData(geoPoint(coordinatesRadius))
+    updateCircleFeature()
+  }
+
+  const mouseUpOnPoint = (e: MapMouseEvent) => {
+    if (!isDragging) return
+    isDragging = false
+    $map.getCanvasContainer().style.cursor = ''
+    $map.off('mousemove', mouseMoveOnPoint)
   }
 </script>
 
