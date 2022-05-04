da7d40a9e06c47bc4ca66e90568402232b5ab9fd
Thuhaa <josephthuha@gmail.com>
2022-05-04 17:27:14 +0300

    style: minor style changes

--- src/dashboards/components/DashboardCard.svelte
+++ src/dashboards/components/DashboardCard.svelte
@@ -7,7 +7,7 @@
 
 <div class="card one-page">
   <header class="card-header">
-    <p class="card-header-title">
+    <p style='color:dodgerblue;' class="card-header-title">
       {title}
       dashboard
     </p>
@@ -18,7 +18,7 @@
         <img class="dashboard-image" src="electricity-snapshot.jpg" alt="Placeholder image" />
       </figure>
     </div>
-    <div class="content">The description should go here and some the information about the specific dashboard</div>
+    <div style='color:dodgerblue;' class="content">The description should go here and some the information about the specific dashboard</div>
   </div>
   <footer class="card-footer" use:Ripple={{ surface: true }}>
     <a href={link} class="card-footer-item">Open {title} dashboard</a>
@@ -29,10 +29,12 @@
   @import 'https://cdn.jsdelivr.net/npm/bulma@0.9.3/css/bulma.min.css';
 
   .one-page {
-    max-width: 20%;
+    width: 400px;
     margin-right: 10px;
-    height: max-content;
+    height: 460px;
     cursor: pointer;
+    border-radius: 10px;
+
   }
   .dashboard-image:hover {
     webkit-filter: blur(4px); /* Chrome, Safari, Opera */
--- src/routes/dashboards/index.svelte
+++ src/routes/dashboards/index.svelte
@@ -14,9 +14,14 @@
 
 <script lang="ts">
   import DashboardCard from '../../dashboards/components/DashboardCard.svelte'
+  import Header from '../../components/Header.svelte'
 </script>
 
-<!--<Header>Dashboards</Header>-->
+<Header>Dashboards</Header>
+<div style='width: 100%; height: max-content; z-index: -1;'>
+  <h3 style='color:dodgerblue; margin: auto; width: 30%;' class="title is-3">GeoHub Dashboard Gallery</h3>
+
+</div>
 <div class="body-div">
   {#each pages as page}
     <DashboardCard title={page.title} link={page.link} />
@@ -25,10 +30,11 @@
 
 <style>
   .body-div {
-    padding: 20px;
-    height: 100vh;
-    width: 100%;
-    display: flex;
+      padding: 20px;
+      height: 100vh;
+      width: 100%;
+      overflow-y: auto;
+      display: flex;
   }
   @media (max-width: 600px) {
     .body-div {
