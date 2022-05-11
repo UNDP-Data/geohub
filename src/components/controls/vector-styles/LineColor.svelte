c6aefb1770b7c7f36a0d9be268ecf3fb8bb78541
Joseph Thuha <josephthuha@gmail.com>
2022-05-11 23:46:31 +0300

    configured colors to use MaplibreColorpicker component (#347)

--- src/components/controls/vector-styles/LineColor.svelte
+++ src/components/controls/vector-styles/LineColor.svelte
@@ -1,78 +1,33 @@
-<script lang="ts" context="module">
-  let lineState = {}
-</script>
-
 <script lang="ts">
   import { createEventDispatcher } from 'svelte'
   import type { LayerSpecification } from '@maplibre/maplibre-gl-style-spec/types'
-  import Ripple from '@smui/ripple'
-  import chroma from 'chroma-js'
 
-  import DefaultColorPicker from '$components/DefaultColorPicker.svelte'
   import StyleControlGroup from '$components/control-groups/StyleControlGroup.svelte'
   import { LayerInitialValues, LayerTypes } from '$lib/constants'
-  import type { Color, Layer } from '$lib/types'
+  import type { Layer } from '$lib/types'
   import { map } from '$stores'
+  import MaplibreColorPicker from './MaplibreColorPicker.svelte'
 
   export let layer: Layer = LayerInitialValues
 
   const dispatch = createEventDispatcher()
   const layerId = layer.definition.id
   const propertyName = 'line-color'
+  const defaultColor = `rgb(53, 175, 109, 1)`
   const style = $map.getStyle().layers.filter((layer: LayerSpecification) => layer.id === layerId)[0]
 
-  // Default value for the color object
-  const r = 53
-  const g = 175
-  const b = 109
-  const a = 1
+  let rgba = style.paint && style.paint[propertyName] ? style.paint[propertyName] : defaultColor
 
-  let color: Color
-  let rgbaString = [style.paint && style.paint[propertyName] ? style.paint[propertyName] : `rgba(${r}, ${g}, ${b})`][0]
-  let showToolTip = false
-
-  if (!Object.keys(lineState).length) {
-    color = {
-      r,
-      g,
-      b,
-      a,
-      hex: chroma([r, g, b]).hex('rgb'),
-      h: chroma([r, g, b]).hsv()[0],
-      s: chroma([r, g, b]).hsv()[1],
-      v: chroma([r, g, b]).hsv()[2],
-    }
-  } else {
-    color = lineState[layerId]
-  }
-
-  const setLineColor = () => {
-    rgbaString = `rgba(${color.r},${color.g},${color.b},${color.a})`
-    lineState[layerId] = color
+  const handleSetColor = (e: CustomEvent) => {
     if (style.type !== LayerTypes.LINE) return
-    const newStyle = JSON.parse(JSON.stringify(style))
-    if (!newStyle.paint) {
-      newStyle.paint = {}
-    }
-    newStyle.paint[propertyName] = rgbaString
-    $map.setPaintProperty(layerId, propertyName, rgbaString)
+    rgba = e.detail.color
+    $map.setPaintProperty(layerId, propertyName, rgba)
     dispatch('change')
   }
 </script>
 
 {#if style.type === LayerTypes.LINE}
   <StyleControlGroup title="Fill Outline Color">
-    {#if showToolTip}
-      <div class={showToolTip ? 'tooltipshown' : 'tooltiphidden'}>
-        <DefaultColorPicker
-          bind:color
-          on:closeColorPicker={() => (showToolTip = false)}
-          on:changeColor={setLineColor} />
-      </div>
-    {/if}
-    <div
-      use:Ripple={{ surface: true }}
-      on:click={() => (showToolTip = !showToolTip)}
-      style="width: 32px; height: 32px; cursor:pointer; border:1px solid grey; background: {rgbaString}" />
+    <MaplibreColorPicker {rgba} on:change={handleSetColor} />
   </StyleControlGroup>
 {/if}
