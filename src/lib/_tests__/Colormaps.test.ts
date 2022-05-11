53c070f91fcf475988048da2371c2e0823180154
foochris <foochris@gmail.com>
2022-05-10 23:46:54 -0400

    fix: disable fetch when click on selected color map

--- src/lib/_tests__/Colormaps.test.ts
+++ src/lib/_tests__/Colormaps.test.ts
@@ -8,7 +8,7 @@ describe('colorMapStyle', () => {
     const style = colormaps.colorMapStyle(ColorMapTypes.SEQUENTIAL, 'viridis', 100, 103, 5, true)
 
     expect(style).toEqual(
-      'height: calc(1px * 30); width: calc(2px * 30); background: linear-gradient(90deg, #3f4a8a,#2c768f,#1f9d8a,#96d647,#fee825); cursor: pointer;',
+      'height: calc(1px * 30); width: calc(2px * 30); background: linear-gradient(90deg, #3f4a8a,#2c768f,#1f9d8a,#96d647,#fee825);',
     )
   })
 
