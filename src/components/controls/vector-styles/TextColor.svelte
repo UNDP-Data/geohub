a1b393e7e86cddc983a9e2ea385f096f8aa6ad61
Joseph Thuha <josephthuha@gmail.com>
2022-05-11 18:55:02 +0300

    feat: text halo color (#343)

--- src/components/controls/vector-styles/TextColor.svelte
+++ src/components/controls/vector-styles/TextColor.svelte
@@ -1,79 +1,34 @@
-<script lang="ts" context="module">
-  let textState = {}
-</script>
-
 <script lang="ts">
   import { createEventDispatcher } from 'svelte'
   import type { LayerSpecification } from '@maplibre/maplibre-gl-style-spec/types'
-  import Ripple from '@smui/ripple'
-  import chroma from 'chroma-js'
 
-  import DefaultColorPicker from '$components/DefaultColorPicker.svelte'
   import StyleControlGroup from '$components/control-groups/StyleControlGroup.svelte'
   import { LayerInitialValues, LayerTypes } from '$lib/constants'
-  import type { Color, Layer } from '$lib/types'
+  import type { Layer } from '$lib/types'
   import { map } from '$stores'
+  import MaplibreColorPicker from './MaplibreColorPicker.svelte'
 
   export let layer: Layer = LayerInitialValues
 
   const dispatch = createEventDispatcher()
   const layerId = layer.definition.id
   const propertyName = 'text-color'
+  const defaultColor = `rgba(0,0,0,1)`
   const style = $map.getStyle().layers.filter((layer: LayerSpecification) => layer.id === layerId)[0]
 
-  const r = 0
-  const g = 0
-  const b = 0
-  const a = 1
-  let color: Color
+  let rgba = style.paint && style.paint[propertyName] ? style.paint[propertyName] : defaultColor
 
-  let rgbaString = style.paint && style.paint[propertyName] ? style.paint[propertyName] : `rgb(${r}, ${g}, ${b})`
-  let showToolTip = false
-
-  if (!Object.keys(textState).length) {
-    color = {
-      r,
-      g,
-      b,
-      a,
-      hex: chroma([r, g, b]).hex('rgb'),
-      h: chroma([r, g, b]).hsv()[0],
-      s: chroma([r, g, b]).hsv()[1],
-      v: chroma([r, g, b]).hsv()[2],
-    }
-  } else {
-    color = textState[layerId]
-  }
-
-  const setTextColor = () => {
-    rgbaString = `rgba(${color.r},${color.g},${color.b})`
-    textState[layerId] = color
+  const handleSetColor = (e: CustomEvent) => {
     if (style.type !== LayerTypes.SYMBOL) return
-    const newStyle = JSON.parse(JSON.stringify(style))
-    if (!newStyle.paint) {
-      newStyle.paint = {}
-    }
-    newStyle.paint[propertyName] = rgbaString
-    $map.setPaintProperty(layerId, propertyName, rgbaString)
-
+    rgba = e.detail.color
+    $map.setPaintProperty(layerId, propertyName, rgba)
     dispatch('change')
   }
 </script>
 
 {#if style.type === LayerTypes.SYMBOL}
   <StyleControlGroup title="Text Color">
-    {#if showToolTip}
-      <div class={showToolTip ? 'tooltipshown' : 'tooltiphidden'}>
-        <DefaultColorPicker
-          bind:color
-          on:closeColorPicker={() => (showToolTip = false)}
-          on:changeColor={setTextColor} />
-      </div>
-    {/if}
-    <div
-      use:Ripple={{ surface: true }}
-      on:click={() => (showToolTip = !showToolTip)}
-      style="width: 32px; height: 32px; cursor:pointer; border:1px solid grey; background: {rgbaString}" />
+    <MaplibreColorPicker {rgba} on:change={handleSetColor} />
   </StyleControlGroup>
 {/if}
 
--- src/components/controls/vector-styles/TextHaloCalor.svelte
+++ src/components/controls/vector-styles/TextHaloCalor.svelte
@@ -1,40 +1,34 @@
 <script lang="ts">
   import { createEventDispatcher } from 'svelte'
   import type { LayerSpecification } from '@maplibre/maplibre-gl-style-spec/types'
-
-  import ColorPicker from '$components/controls/ColorPicker.svelte'
-  import StyleControlGroup from '$components/control-groups/StyleControlGroup.svelte'
-  import { LayerInitialValues, LayerTypes } from '$lib/constants'
-  import type { Layer } from '$lib/types'
   import { map } from '$stores'
+  import type { Layer } from '$lib/types'
+  import { LayerInitialValues, LayerTypes } from '$lib/constants'
+
+  import StyleControlGroup from '$components/control-groups/StyleControlGroup.svelte'
+  import MaplibreColorPicker from './MaplibreColorPicker.svelte'
 
   export let layer: Layer = LayerInitialValues
 
   const dispatch = createEventDispatcher()
   const layerId = layer.definition.id
   const propertyName = 'text-halo-color'
+  const defaultColor = `rgba(255,255,255,1)`
   const style = $map.getStyle().layers.filter((layer: LayerSpecification) => layer.id === layerId)[0]
 
-  let TextHaloColor = style.paint && style.paint[propertyName] ? style.paint[propertyName] : 'rgb(255, 255, 255)'
+  let rgba = style.paint && style.paint[propertyName] ? style.paint[propertyName] : defaultColor
 
-  $: TextHaloColor, setTextHaloColor()
-
-  const setTextHaloColor = () => {
+  const handleSetColor = (e: CustomEvent) => {
     if (style.type !== LayerTypes.SYMBOL) return
-    const newStyle = JSON.parse(JSON.stringify(style))
-    if (!newStyle.paint) {
-      newStyle.paint = {}
-    }
-    newStyle.paint[propertyName] = TextHaloColor
-    $map.setPaintProperty(layerId, propertyName, TextHaloColor)
-
+    rgba = e.detail.color
+    $map.setPaintProperty(layerId, propertyName, rgba)
     dispatch('change')
   }
 </script>
 
 {#if style.type === LayerTypes.SYMBOL}
   <StyleControlGroup title="Text Halo Color">
-    <ColorPicker bind:RgbColor={TextHaloColor} />
+    <MaplibreColorPicker {rgba} on:change={handleSetColor} />
   </StyleControlGroup>
 {/if}
 
