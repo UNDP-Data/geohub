4a0e4c081a1cb6003469e63eb94c3e9025c8140d
foochris <foochris@gmail.com>
2022-05-12 10:42:45 -0400

    lint cleanup

--- src/components/controls/__tests__/NumberInput.test.ts
+++ src/components/controls/__tests__/NumberInput.test.ts
@@ -1,5 +1,5 @@
 import { describe, beforeEach, expect, it, vi } from 'vitest'
-import { cleanup, fireEvent, render, within, type RenderResult } from '@testing-library/svelte'
+import { cleanup, fireEvent, render, type RenderResult } from '@testing-library/svelte'
 
 import NumberInput from '$components/controls/NumberInput.svelte'
 
