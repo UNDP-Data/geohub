096d31cf6974755d395e61beb0156483685ceeb7
foochris <foochris@gmail.com>
2022-05-18 11:34:41 -0400

    style: fix z-index layering for icon color picker

--- src/components/controls/vector-styles/IconColor.svelte
+++ src/components/controls/vector-styles/IconColor.svelte
@@ -1,5 +1,4 @@
 <script lang="ts">
-  import { createEventDispatcher } from 'svelte'
   import type { LayerSpecification } from '@maplibre/maplibre-gl-style-spec/types'
   import { LayerInitialValues, LayerTypes } from '$lib/constants'
   import type { Layer } from '$lib/types'
@@ -8,7 +7,6 @@
 
   export let layer: Layer = LayerInitialValues
 
-  const dispatch = createEventDispatcher()
   const layerId = layer.definition.id
   const propertyName = 'icon-color'
   const style = $map.getStyle().layers.filter((layer: LayerSpecification) => layer.id === layerId)[0]
@@ -17,17 +15,12 @@
   let rgba = style.paint && style.paint[propertyName] ? style.paint[propertyName] : defaultColor
 
   const handleSetColor = (e: CustomEvent) => {
-    if (style.type !== LayerTypes.SYMBOL) return
-    rgba = e.detail.color
-    $map.setPaintProperty(layerId, propertyName, rgba)
-    dispatch('change')
+    if (e?.detail?.color) {
+      $map.setPaintProperty(layerId, propertyName, e.detail.color)
+    }
   }
 </script>
 
 {#if style.type === LayerTypes.SYMBOL}
   <MaplibreColorPicker {rgba} on:change={handleSetColor} />
 {/if}
-
-<style lang="scss">
-  @use '@material/image-list/index' as image-list;
-</style>
