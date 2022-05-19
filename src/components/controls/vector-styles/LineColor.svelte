44fff3e1c8e60d1136e1278a960dda2e0db3d67e
Chris Foo <foochris@gmail.com>
2022-05-18 23:57:06 -0400

    refactor: vector line (#376)

--- src/components/controls/vector-styles/LineColor.svelte
+++ src/components/controls/vector-styles/LineColor.svelte
@@ -1,8 +1,6 @@
 <script lang="ts">
-  import { createEventDispatcher } from 'svelte'
   import type { LayerSpecification } from '@maplibre/maplibre-gl-style-spec/types'
 
-  import StyleControlGroup from '$components/control-groups/StyleControlGroup.svelte'
   import { LayerInitialValues, LayerTypes } from '$lib/constants'
   import type { Layer } from '$lib/types'
   import { map } from '$stores'
@@ -10,7 +8,6 @@
 
   export let layer: Layer = LayerInitialValues
 
-  const dispatch = createEventDispatcher()
   const layerId = layer.definition.id
   const propertyName = 'line-color'
   const defaultColor = `rgb(53, 175, 109, 1)`
@@ -22,12 +19,9 @@
     if (style.type !== LayerTypes.LINE) return
     rgba = e.detail.color
     $map.setPaintProperty(layerId, propertyName, rgba)
-    dispatch('change')
   }
 </script>
 
 {#if style.type === LayerTypes.LINE}
-  <StyleControlGroup title="Fill Outline Color">
-    <MaplibreColorPicker {rgba} on:change={handleSetColor} />
-  </StyleControlGroup>
+  <MaplibreColorPicker {rgba} on:change={handleSetColor} />
 {/if}
--- src/components/controls/vector-styles/LineDasharray.svelte
+++ src/components/controls/vector-styles/LineDasharray.svelte
@@ -1,22 +1,18 @@
 <script lang="ts">
-  import { createEventDispatcher } from 'svelte'
-  import Select, { Option } from '@smui/select'
   import type { LayerSpecification } from '@maplibre/maplibre-gl-style-spec/types'
 
-  import StyleControlGroup from '$components/control-groups/StyleControlGroup.svelte'
   import { LayerInitialValues, LayerTypes } from '$lib/constants'
   import type { Layer } from '$lib/types'
   import { map } from '$stores'
 
   export let layer: Layer = LayerInitialValues
 
-  const dispatch = createEventDispatcher()
   const layerId = layer.definition.id
   const lineTypes = [
-    { title: 'solid line', value: [1] },
-    { title: 'dashed line', value: [10, 4] },
-    { title: 'dashed-dotted line', value: [10, 3, 2, 3] },
-    { title: 'dotted line', value: [1, 5, 1] },
+    { title: 'solid', value: [1] },
+    { title: 'dash', value: [10, 4] },
+    { title: 'dash-dot', value: [10, 3, 2, 3] },
+    { title: 'dot', value: [1, 5, 1] },
   ]
 
   const propertyName = 'line-dasharray'
@@ -34,17 +30,25 @@
     }
     newStyle.paint[propertyName] = LineDasharrayValue
     $map.setPaintProperty(layerId, propertyName, LineDasharrayValue)
-
-    dispatch('change')
   }
 </script>
 
 {#if style.type === LayerTypes.LINE}
-  <StyleControlGroup title="Line Dasharray">
-    <Select bind:value={LineDasharrayValue}>
-      {#each lineTypes as type}
-        <Option value={type.value}>{type.title}</Option>
-      {/each}
-    </Select>
-  </StyleControlGroup>
+  <div style="width: 60%;">
+    {#each lineTypes as type}
+      <div class="columns is-gapless" style="margin-bottom: 5px;">
+        <div class="column is-4">
+          <input
+            type="radio"
+            bind:group={LineDasharrayValue}
+            value={type.value}
+            alt={`${type.title} Option`}
+            title={`${type.title} Option`} />
+        </div>
+        <div class="column" style="position: relative; top: -2px">
+          {type.title}
+        </div>
+      </div>
+    {/each}
+  </div>
 {/if}
