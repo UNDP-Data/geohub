aeeefbedb4d11e0d7152103b2788d4bd2ca378a4
Jin Igarashi <igarashi.jin@water-gis.com>
2022-05-24 23:33:46 +0100

    Refactor/dashboard (#389)

--- src/dashboards/components/Content.svelte
+++ src/dashboards/components/Content.svelte
@@ -1,46 +1,33 @@
 <script lang="ts">
-  import { LayerTypes } from '$lib/constants'
   import { onMount } from 'svelte'
   import Drawer, { AppContent, Content } from '@smui/drawer'
   import { map, year } from '../stores'
   import SegmentedButton, { Segment, Label } from '@smui/segmented-button'
-  import Fa from 'svelte-fa'
-  import { faPlugCircleBolt } from '@fortawesome/free-solid-svg-icons/faPlugCircleBolt'
-  import { faLaptopCode } from '@fortawesome/free-solid-svg-icons/faLaptopCode'
-  import { faBan } from '@fortawesome/free-solid-svg-icons/faBan'
-  import Button from '@smui/button'
-  import Paper from '@smui/paper'
-  import FormField from '@smui/form-field'
-  import Checkbox from '@smui/checkbox'
-  import type { HeatmapLayerSpecification, VectorSourceSpecification } from '@maplibre/maplibre-gl-style-spec/types'
-  import RangeSlider from 'svelte-range-slider-pips'
   import StyleControlGroup from '$components/control-groups/StyleControlGroup.svelte'
-  import TimeSlider from './TimeSlider.svelte'
   import vegaEmbed from 'vega-embed'
   import AdminLayer from '$lib/adminLayer'
+  import IntroductionPanel from './IntroductionPanel.svelte'
+  import PovertyControl from './PovertyControl.svelte'
+  import ElectricityControl from './ElectricityControl.svelte'
 
-  const TOKEN = import.meta.env.VITE_AZURE_BLOB_TOKEN
   const API_URL = import.meta.env.VITE_TITILER_ENDPOINT
   const AZURE_URL = import.meta.env.VITE_AZURE_URL
-
-  const POVERTY_URL = [`${AZURE_URL}/admin/poverty_points/{z}/{x}/{y}.pbf`]
 
   let POVERTY_ID = 'poverty'
   const HREA_ID = 'HREA'
   const HREA_NODATA = -3.3999999521443642e38
   const ML_ID = 'ML'
   const ML_NODATA = 0
-  const NONE_ID = 'NONE'
   const PRIMARY = '#1f77b4'
   const SECONDARY = '#ff7f0e'
   const GREY = '#808080'
   let adminLayer: AdminLayer = null
 
-  export const getHreaUrl = (y) => {
-    return `${AZURE_URL}/electricity/High_Resolution_Electricity_Access/Electricity_Access/Electricity_access_estimate_${y}.tif?${TOKEN}`
+  let getHreaUrl = (y: number): string => {
+    return
   }
-  export const getMlUrl = (y) => {
-    return `${AZURE_URL}/electricity/Machine_Learning_Electricity_Access/Electricity_access_${y}.tif?${TOKEN}`
+  let getMlUrl = (y: number): string => {
+    return
   }
 
   export let drawerOpen = false
@@ -53,36 +40,22 @@
   let adminHistogramStep = 1
 
   let showIntro = true
-  let heatmapChecked = false
-  $: heatmapChecked, loadHeatmap()
-  let electricityChoices = [
-    { name: HREA_ID, icon: faPlugCircleBolt },
-    { name: ML_ID, icon: faLaptopCode },
-    { name: NONE_ID, icon: faBan },
-  ]
-  let electricitySelected = electricityChoices[0]
+  $: showIntro, showIntroChanged()
+
+  const showIntroChanged = () => {
+    if (showIntro === true) return
+    adminInteraction()
+    loadHeatmap()
+  }
+
+  let electricitySelected
   let interactChoices = ['Admin', 'Point']
   let interactSelected = interactChoices[0]
   let drawerWidth = 355
   let isResizingDrawer = false
 
-  let layerOpacity = 1
-  let rangeSliderValues = [layerOpacity * 100]
   let loadRasterLayer = () => {
     return
-  }
-  $: layerOpacity = rangeSliderValues[0] / 100
-  $: layerOpacity, setLayerOpacity()
-
-  const setLayerOpacity = () => {
-    if ($map && $map.getLayer(POVERTY_ID)) {
-      $map.setPaintProperty(POVERTY_ID, 'heatmap-opacity', layerOpacity)
-    }
-  }
-
-  const hideIntro = () => {
-    showIntro = false
-    adminInteraction()
   }
 
   $: {
@@ -412,51 +385,8 @@
     else pointInteraction()
   }
 
-  const initHeatmap = () => {
-    if (!$map.getSource(POVERTY_ID)) {
-      const layerSource: VectorSourceSpecification = {
-        type: 'vector',
-        tiles: POVERTY_URL,
-        maxzoom: 10,
-      }
-      $map.addSource(POVERTY_ID, layerSource)
-    }
-
-    if (!$map.getLayer(POVERTY_ID)) {
-      const layerDefinition: HeatmapLayerSpecification = {
-        id: POVERTY_ID,
-        type: LayerTypes.HEATMAP,
-        source: POVERTY_ID,
-        'source-layer': POVERTY_ID + '_points',
-        layout: { visibility: 'none' },
-        paint: {
-          'heatmap-weight': ['interpolate', ['exponential', 2], ['get', POVERTY_ID], 0, 0, 2.022, 1],
-          'heatmap-intensity': ['interpolate', ['exponential', 2], ['zoom'], 0, 0, 10, 5],
-          'heatmap-radius': ['interpolate', ['linear'], ['zoom'], 0, 0, 10, 30],
-        },
-      }
-      $map.addLayer(layerDefinition)
-    }
-  }
-
-  const moveHeatmap = () => {
-    if (!$map) return
-    let firstSymbolId = undefined
-    for (const layer of $map.getStyle().layers) {
-      if (layer.type === 'symbol') {
-        firstSymbolId = layer.id
-        break
-      }
-    }
-    $map.moveLayer(POVERTY_ID, firstSymbolId)
-  }
-
-  const loadHeatmap = () => {
-    if (!$map) return
-    initHeatmap()
-    $map.setLayoutProperty(POVERTY_ID, 'visibility', heatmapChecked ? 'visible' : 'none')
-    $map.setPaintProperty(POVERTY_ID, 'heatmap-opacity', layerOpacity)
-    moveHeatmap()
+  let loadHeatmap = () => {
+    return
   }
 </script>
 
@@ -469,68 +399,12 @@
       <div class="drawer-content" style="width: {drawerWidth - 10}px; max-width: {drawerWidth - 10}px;">
         <Content style="padding: 15px; overflow: visible;">
           <p class="heading-text">UNDP Electricity Dashboard</p>
-          {#if showIntro}
-            <Paper>
-              <p>
-                Welcome to the UNDP GeoHub dashboard. This site serves as a simplified preview to familiarize users with
-                data being prepared for the full-fledged GeoHub web app launching soon. Presented here is a High
-                Resolution Electricity Access (HREA) created by the University of Michigan, used to support the 2030
-                Social Development Goal (SDG) number 7: ensuring access to affordable, reliable, sustainable and modern
-                energy for all.
-              </p>
-              <p>
-                This map displays two types of electricity access layers, with an optional poverty heatmap which can be
-                overlaid. Layer statistics can be explored by either hovering over the map, showing values for the
-                layers underneath the mouse, or by drawing a circle, providing summaries of the values contained within.
-              </p>
-              <br />
-              <Button variant="raised" on:click={hideIntro}>Explore Data</Button>
-            </Paper>
-          {/if}
+          <IntroductionPanel bind:showIntro />
 
           {#if !showIntro}
             <StyleControlGroup title="Layers">
-              <p class="title-text">Electricity Access</p>
-              <SegmentedButton
-                segments={electricityChoices}
-                let:segment
-                singleSelect
-                bind:selected={electricitySelected}>
-                <Segment {segment}>
-                  <div class="icon">
-                    <Fa icon={segment.icon} size="lg" />
-                  </div>
-                  <Label>{segment.name}</Label>
-                </Segment>
-              </SegmentedButton>
-              <div class="raster-time-slider">
-                <TimeSlider
-                  bind:electricitySelected
-                  bind:loadLayer={loadRasterLayer}
-                  bind:BEFORE_LAYER_ID={POVERTY_ID}
-                  {AZURE_URL} />
-              </div>
-              <FormField>
-                <Checkbox bind:checked={heatmapChecked} />
-                <p class="title-text">Poverty</p>
-              </FormField>
-              {#if heatmapChecked}
-                <div class="action">
-                  <div class="slider">
-                    <RangeSlider
-                      bind:values={rangeSliderValues}
-                      float
-                      min={0}
-                      max={100}
-                      step={1}
-                      pips
-                      first="label"
-                      last="label"
-                      rest={false}
-                      suffix="%" />
-                  </div>
-                </div>
-              {/if}
+              <ElectricityControl bind:electricitySelected bind:loadRasterLayer bind:getHreaUrl bind:getMlUrl />
+              <PovertyControl bind:loadHeatmap bind:POVERTY_ID />
             </StyleControlGroup>
 
             <StyleControlGroup title="Statistics">
@@ -541,7 +415,7 @@
               </SegmentedButton>
               {#if interactSelected === 'Admin'}
                 <br /><br />
-                <div class="title-text">{electricitySelected.name} Histogram - {$year}</div>
+                <div class="title-text">{electricitySelected?.name} Histogram - {$year}</div>
                 <div class="title-text">{adminHistogramAdmin}</div>
                 <div id="admin-histogram" />
               {/if}
@@ -682,21 +556,11 @@
     }
   }
 
-  .raster-time-slider {
-    padding-top: 1em;
-    padding-bottom: 1em;
-  }
-
   .chart-container {
     display: flex;
   }
 
   .chart-item {
     flex-grow: 1;
-  }
-
-  .icon {
-    padding-left: 10px;
-    padding-right: 20px;
   }
 </style>
