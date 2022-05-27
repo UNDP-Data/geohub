91c3001546e9e6c8e0c393c6bab23a6523cb8a61
Jin Igarashi <igarashi.jin@water-gis.com>
2022-05-27 21:09:11 +0100

    refactor: redesigned labeling (#412)

--- src/components/controls/VectorLabelPanel.svelte
+++ src/components/controls/VectorLabelPanel.svelte
@@ -1,7 +1,6 @@
 <script lang="ts">
+  import { onMount } from 'svelte'
   import type { LayerSpecification, SymbolLayerSpecification } from '@maplibre/maplibre-gl-style-spec/types.g'
-  import Switch from '@smui/switch'
-  import FormField from '@smui/form-field'
   import { map } from '$stores'
   import TextField from '$components/controls/vector-styles/TextField.svelte'
   import TextColor from '$components/controls/vector-styles/TextColor.svelte'
@@ -11,7 +10,8 @@
   import TextSize from '$components/controls/vector-styles/TextSize.svelte'
   import { LayerInitialValues, LayerTypes } from '$lib/constants'
   import type { Layer } from '$lib/types'
-  import IconKeepUpright from './vector-styles/IconKeepUpright.svelte'
+  import SymbolPlacement from './vector-styles/SymbolPlacement.svelte'
+  import StyleControlGroup from '$components/control-groups/StyleControlGroup.svelte'
 
   export let isLabelPanelVisible = false
   export let layer: Layer = LayerInitialValues
@@ -22,88 +22,86 @@
   const style: LayerSpecification = $map
     .getStyle()
     .layers.filter((layer: LayerSpecification) => layer.id === parentLayerId)[0]
-  let enabledTextLabel = false
   let updateLegend = () => undefined
 
-  $: enabledTextLabel, disableTextLabel()
-
-  const disableTextLabel = () => {
-    if (enabledTextLabel === true) {
-      if (style.type !== LayerTypes.SYMBOL) {
-        targetLayerId = `${parentLayerId}-label`
-        const childLayer: SymbolLayerSpecification = {
-          id: targetLayerId,
-          type: LayerTypes.SYMBOL,
-          source: style['source'],
-          'source-layer': style['source-layer'],
-          minzoom: style.minzoom,
-          maxzoom: style.maxzoom,
-          layout: {
-            visibility: 'visible',
-            'text-size': 16,
-            'text-max-width': 10,
-          },
-          paint: {
-            'text-color': 'rgba(0,0,0,1)',
-            'text-halo-color': 'rgba(255,255,255,1)',
-            'text-halo-width': 1,
-          },
-        }
-
-        $map.addLayer(childLayer)
-
-        if (!layer.children) {
-          layer.children = []
-        }
-
-        targetLayer = {
-          name: targetLayerId,
-          definition: childLayer,
-          type: LayerTypes.VECTOR,
-          info: layer.info,
-          visible: true,
-          url: layer.url,
-          parent: layer,
-        }
-
-        layer.children = [targetLayer, ...layer.children]
-      } else {
-        $map.setLayoutProperty(targetLayerId, 'text-size', 16)
-        $map.setLayoutProperty(targetLayerId, 'text-max-width', 10)
-        $map.setPaintProperty(targetLayerId, 'text-color', 'rgba(0,0,0,1)')
-        $map.setPaintProperty(targetLayerId, 'text-halo-color', 'rgba(255,255,255,1)')
-        $map.setPaintProperty(targetLayerId, 'text-halo-width', 1)
+  onMount(() => {
+    initialiseTextLabel()
+  })
+  const initialiseTextLabel = () => {
+    if (style.type !== LayerTypes.SYMBOL) {
+      targetLayerId = `${parentLayerId}-label`
+      const childLayer: SymbolLayerSpecification = {
+        id: targetLayerId,
+        type: LayerTypes.SYMBOL,
+        source: style['source'],
+        'source-layer': style['source-layer'],
+        minzoom: style.minzoom,
+        maxzoom: style.maxzoom,
+        layout: {
+          visibility: 'visible',
+          'text-size': 16,
+          'text-max-width': 10,
+        },
+        paint: {
+          'text-color': 'rgba(0,0,0,1)',
+          'text-halo-color': 'rgba(255,255,255,1)',
+          'text-halo-width': 1,
+        },
       }
-      return
-    }
 
-    if (style.type === LayerTypes.SYMBOL) {
-      const layoutFields = [
-        'text-field',
-        'text-variable-anchor',
-        'text-radial-offset',
-        'text-justify',
-        'text-size',
-        'text-max-width',
-      ]
-      layoutFields.forEach((prop) => {
-        if (!$map.getLayoutProperty(targetLayerId, prop)) return
-        $map.setLayoutProperty(targetLayerId, prop, undefined)
-      })
+      $map.addLayer(childLayer)
 
-      const paintFields = ['text-color', 'text-halo-color', 'text-halo-width']
-      paintFields.forEach((prop) => {
-        if (!$map.getPaintProperty(targetLayerId, prop)) return
-        $map.setPaintProperty(targetLayerId, prop, undefined)
-      })
-    } else {
-      if (layer.children && layer.children.length > 0) {
-        layer.children.forEach((l) => {
-          if (!$map.getLayer(l.definition.id)) return
-          $map.removeLayer(l.definition.id)
-        })
+      if (!layer.children) {
         layer.children = []
       }
+
+      targetLayer = {
+        name: targetLayerId,
+        definition: childLayer,
+        type: LayerTypes.VECTOR,
+        info: layer.info,
+        visible: true,
+        url: layer.url,
+        parent: layer,
+      }
+
+      layer.children = [targetLayer, ...layer.children]
+    } else {
+      $map.setLayoutProperty(targetLayerId, 'text-size', 16)
+      $map.setLayoutProperty(targetLayerId, 'text-max-width', 10)
+      $map.setPaintProperty(targetLayerId, 'text-color', 'rgba(0,0,0,1)')
+      $map.setPaintProperty(targetLayerId, 'text-halo-color', 'rgba(255,255,255,1)')
+      $map.setPaintProperty(targetLayerId, 'text-halo-width', 1)
+    }
+    return
+  }
+
+  if (style.type === LayerTypes.SYMBOL) {
+    const layoutFields = [
+      'text-field',
+      'text-variable-anchor',
+      'text-radial-offset',
+      'text-justify',
+      'text-size',
+      'text-max-width',
+    ]
+    layoutFields.forEach((prop) => {
+      if (!$map.getLayoutProperty(targetLayerId, prop)) return
+      $map.setLayoutProperty(targetLayerId, prop, undefined)
+    })
+
+    const paintFields = ['text-color', 'text-halo-color', 'text-halo-width']
+    paintFields.forEach((prop) => {
+      if (!$map.getPaintProperty(targetLayerId, prop)) return
+      $map.setPaintProperty(targetLayerId, prop, undefined)
+    })
+  } else {
+    if (layer.children && layer.children.length > 0) {
+      layer.children.forEach((l) => {
+        if (!$map.getLayer(l.definition.id)) return
+        $map.removeLayer(l.definition.id)
+      })
+      layer.children = []
     }
   }
 
@@ -114,72 +112,105 @@
 
 {#if isLabelPanelVisible === true}
   <div class="action" data-testid="vector-label-panel-container">
-    <div class="columns is-flex is-vcentered">
-      <div class="column is-3">
-        <FormField>
-          <Switch bind:checked={enabledTextLabel} />
-        </FormField>
+    <div class="columns is-vcentered first-row">
+      <div class="column text-field">
+        <div class="is-flex is-justify-content-center" style="position: relative;">
+          <TextField on:change={onStyleChange} bind:layer={targetLayer} />
+        </div>
       </div>
-      {#if enabledTextLabel === true}
-        <div class="column is-vcentered">
-          <div>
-            <TextField on:change={onStyleChange} bind:layer={targetLayer} {enabledTextLabel} />
-          </div>
+      <div class="is-divider-vertical" />
+      <div class="column is-3">
+        <div class="is-size-6 is-flex is-justify-content-center">Text wrap max Width:</div>
+      </div>
+      <div class="column text-max-width">
+        <div class="is-flex is-justify-content-center" style="position: relative;">
+          <TextMaxWidth on:change={onStyleChange} bind:layer={targetLayer} />
         </div>
-      {/if}
+      </div>
     </div>
-    {#if enabledTextLabel === true}
-      <div class="container notification is-secondary">
-        <div class="columns is-flex is-vcentered">
-          <div class="column is-3">
-            <div class="is-size-6">Font color</div>
-          </div>
-          <div class="column">
-            <div>
-              <TextColor on:change={onStyleChange} bind:layer={targetLayer} />
+    <div class="is-divider m-0" />
+    <div class="columns">
+      <div class="column is-6 p-1">
+        <StyleControlGroup title="Font">
+          <div class="columns first-row">
+            <div class="column text-color">
+              <div class="is-flex is-justify-content-center">Color:</div>
+              <div class="is-flex is-justify-content-center" style="position: relative;">
+                <TextColor on:change={onStyleChange} bind:layer={targetLayer} />
+              </div>
+            </div>
+            <div class="column text-size">
+              <div class="is-flex is-justify-content-center">Size:</div>
+              <div class="is-flex is-justify-content-center" style="position: relative;">
+                <TextSize on:change={onStyleChange} bind:layer={targetLayer} />
+              </div>
             </div>
           </div>
-          <div class="column is-2">
-            <div class="is-size-6">Font size</div>
-          </div>
-          <div class="column">
-            <div>
-              <TextSize on:change={onStyleChange} bind:layer={targetLayer} />
+        </StyleControlGroup>
+      </div>
+      <div class="column is-6 p-1">
+        <StyleControlGroup title="Font">
+          <div class="columns first-row">
+            <div class="column halo-color">
+              <div class="is-flex is-justify-content-center">Color:</div>
+              <div class="is-flex is-justify-content-center" style="position: relative;">
+                <TextHaloCalor on:change={onStyleChange} bind:layer={targetLayer} />
+              </div>
+            </div>
+            <div class="column halo-size">
+              <div class="is-flex is-justify-content-center">Size:</div>
+              <div class="is-flex is-justify-content-center" style="position: relative;">
+                <TextHaloWidth on:change={onStyleChange} bind:layer={targetLayer} />
+              </div>
             </div>
           </div>
+        </StyleControlGroup>
+      </div>
+    </div>
+    {#if style.type === LayerTypes.FILL || style.type === LayerTypes.LINE}
+      <div class="is-divider m-0 pb-3" />
+      <div class="columns first-row">
+        <div class="column is-5">
+          <div class="is-flex is-justify-content-center">Label position relative to geometry:</div>
         </div>
-        <div class="columns is-flex is-vcentered">
-          <div class="column is-3">
-            <div class="is-size-6">Halo color</div>
+        <div class="column is-4">
+          <div class="is-flex is-justify-content-center" style="position: relative;">
+            <SymbolPlacement on:change={onStyleChange} bind:layer={targetLayer} />
           </div>
-          <div class="column">
-            <div>
-              <TextHaloCalor on:change={onStyleChange} bind:layer={targetLayer} />
-            </div>
-          </div>
-          <div class="column is-2">
-            <div class="is-size-6">Halo width</div>
-          </div>
-          <div class="column">
-            <div>
-              <TextHaloWidth on:change={onStyleChange} bind:layer={targetLayer} />
-            </div>
-          </div>
-        </div>
-        <div class="columns is-flex is-vcentered">
-          <div class="column is-7">
-            <div class="is-size-6">Text Max Width</div>
-          </div>
-          <div class="column">
-            <div>
-              <TextMaxWidth on:change={onStyleChange} bind:layer={targetLayer} />
-            </div>
-          </div>
-        </div>
-        <div class="columns is-flex is-vcentered">
-          <IconKeepUpright on:change={onStyleChange} bind:layer={targetLayer} />
         </div>
       </div>
     {/if}
   </div>
 {/if}
+
+<style lang="scss">
+  .first-row {
+    margin-bottom: 0;
+
+    .text-field {
+      div:first-child {
+        margin: 0px;
+      }
+    }
+
+    .text-max-width,
+    .heat-map-color,
+    .text-color,
+    .halo-color {
+      div:first-child {
+        margin-bottom: 10px;
+      }
+
+      div:second-child {
+        height: 30px;
+      }
+    }
+
+    .text-size,
+    .halo-size {
+      div:first-child {
+        margin-bottom: 3px;
+      }
+    }
+  }
+</style>
