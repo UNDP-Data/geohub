80d8be1412fb5350a10b0af0fa13e888ace1169c
foochris <foochris@gmail.com>
2022-06-02 09:15:58 -0400

    chore: rename line dash array to line pattern component name

--- src/components/controls/VectorLineSimple.svelte
+++ src/components/controls/VectorLineSimple.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
   import LineColor from '$components/controls/vector-styles/LineColor.svelte'
-  import LineDasharray from '$components/controls/vector-styles/LineDasharray.svelte'
+  import LinePattern from '$components/controls/vector-styles/LinePattern.svelte'
   import LineWidth from '$components/controls/vector-styles/LineWidth.svelte'
   import { LayerInitialValues } from '$lib/constants'
   import type { Layer } from '$lib/types'
@@ -30,7 +30,7 @@
     <div class="column">
       <div class="pb-2">Line Pattern</div>
       <div class="is-flex is-justify-content-center">
-        <LineDasharray {layer} />
+        <LinePattern {layer} />
       </div>
     </div>
   </div>
