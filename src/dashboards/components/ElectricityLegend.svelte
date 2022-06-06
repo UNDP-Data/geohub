480e62999d3fb882a179267f07eed52acc3ed6ba
Thuhaa <josephthuha@gmail.com>
2022-06-06 16:12:21 +0300

    feat: dashboard legend 001

---
+++ src/dashboards/components/ElectricityLegend.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+  import chroma from 'chroma-js'
+  const hreaLegend = chroma.scale('RdYlBu').colors().reverse()
+  export let electricitySelected
+</script>
+
+{#if electricitySelected.name !== 'NONE'}
+  <div class="legend" style="background: linear-gradient(to right, {[...hreaLegend]});" />
+  <div style="display: flex; align-items: center; justify-content: space-between"><span>0%</span><span>100%</span></div>
+{/if}
+
+<style lang="scss">
+  .legend {
+    height: 20px;
+    width: 100%;
+    margin-top: 5%;
+  }
+</style>
---
+++ src/dashboards/components/PovertyLegend.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+  import chroma from 'chroma-js'
+
+  const povertyLegend = chroma.scale('RdYlBu').colors()
+  console.log(povertyLegend)
+  export let povertySelected
+</script>
+
+{#if povertySelected}
+  <div class="legend" style="background: linear-gradient(to right, {[...povertyLegend]});" />
+  <div style="display: flex; align-items: center; justify-content: space-between"><span>0%</span><span>100%</span></div>
+{/if}
+
+<style lang="scss">
+  .legend {
+    height: 20px;
+    width: 100%;
+    margin-top: 5%;
+  }
+</style>
