bf48f255f687c77ac57c0b8bc1f096c87b38c835
Thuhaa <josephthuha@gmail.com>
2022-06-08 01:56:53 +0300

    feat: mvtinfo endpoint

---
+++ src/routes/mvt-info.json.ts
@@ -0,0 +1,103 @@
+// Imports
+import { VectorTile } from '@mapbox/vector-tile'
+import Pbf from 'pbf'
+import arraystat from 'arraystat'
+
+// variables
+let pbf
+let tile
+const propsObj = {}
+let attributesArray = []
+let data
+
+// fetchMVT
+export const fetchMVTInfo = async (path, layerName) => {
+  await fetch(`${path}`)
+    .then((response) => response.arrayBuffer())
+    .then((arrayBuffer) => (pbf = new Pbf(arrayBuffer)))
+    .catch((error) => (attributesArray = error))
+
+  try {
+    tile = new VectorTile(pbf)
+    const layer = tile.layers[layerName]
+
+    // since we are pushing values, we need to force the attributesArray to be empty at this point
+    if (attributesArray.length > 0) {
+      attributesArray = []
+    }
+
+    // The layer._keys is a list with all the available attributes in the layer.
+    // Mapping through the attributes to get the attributeArray object
+    layer._keys.map((property) => {
+      propsObj[property] = []
+
+      for (let featureIndex = 0; featureIndex < layer.length; featureIndex++) {
+        const feature = layer.feature(featureIndex)
+        layer._keys[property] = propsObj[property].push(feature.properties[property])
+      }
+
+      if (isNaN(propsObj[property][0])) {
+        // The first value is not a number, mathematical operations will result in NaN
+        const attribute = {
+          attribute: property,
+          type: String(typeof propsObj[property][0]),
+          count: propsObj[property].length,
+          values: propsObj[property].slice(0, 100),
+        }
+        // Add the attribute to the attributes array
+        attributesArray.push(attribute)
+      } else {
+        // The first value is a number, so assume all values as number
+        // Need to generate the histogram here
+        const histogram = { count: [], bins: [] }
+        arraystat(propsObj[property]).histogram.map((item) => {
+          histogram.bins.push(item.max), histogram.count.push(item.nb)
+        })
+        histogram.bins.unshift(Math.min(...propsObj[property]))
+
+        const attribute = {
+          attribute: property,
+          type: String(typeof propsObj[property][0]),
+          count: propsObj[property].length,
+          min: Math.min(...propsObj[property]),
+          max: Math.max(...propsObj[property]),
+          histogram: histogram,
+        }
+        attributesArray.push(attribute)
+      }
+    })
+  } catch (error) {
+    // Fixme: If this catch is invoked. Need to return the actual error
+
+    attributesArray.push({
+      code: 500,
+      message: 'something went wrong!',
+    })
+  }
+  return attributesArray
+}
+
+export async function get(query: any) {
+  // If either of `path` or `layer_name` parameter is not provided. This is a bad request.
+  if (!query.url.searchParams.has('path') || !query.url.searchParams.has('layer_name')) {
+    data = {
+      code: '400',
+      message: "Bad Request. Please check if there is a parameter you haven't supplied",
+    }
+  }
+
+  // Both parameters are provided, so go ahead!
+  if (query.url.searchParams.has('path') && query.url.searchParams.has('layer_name')) {
+    const path = query.url.searchParams.get('path')
+    const layer_name = query.url.searchParams.get('layer_name')
+
+    // fetchMVTInfo function returns an array
+    await fetchMVTInfo(path, layer_name)
+      .then((response) => (data = response))
+      .catch((reason) => {
+        data = reason
+      })
+  }
+
+  return { body: data } // Returns an array of items
+}
