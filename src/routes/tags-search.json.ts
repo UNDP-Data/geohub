b90a852d57914452fe75d4991b59f34bef6d66a6
Chris Foo <foochris@gmail.com>
2022-06-20 23:21:24 -0400

    feat: add redis cache for tag search (#491)

--- src/routes/tags-search.json.ts
+++ src/routes/tags-search.json.ts
@@ -1,9 +1,16 @@
 import fs from 'fs'
 import path from 'path'
+// import Redis from 'ioredis'
 import { BlobServiceClient, StorageSharedKeyCredential } from '@azure/storage-blob'
 
-import { AZURE_STORAGE_ACCOUNT, AZURE_STORAGE_ACCESS_KEY } from '$lib/variables'
+import {
+  AZURE_STORAGE_ACCOUNT,
+  AZURE_STORAGE_ACCESS_KEY,
+  // AZURE_REDIS_HOSTNAME,
+  // AZURE_REDIS_CACHEKEY,
+} from '$lib/variables'
 import { TagKeys } from '$lib/constants'
+import type { TagLayer } from '$lib/types'
 
 const __dirname = path.resolve()
 const tagsFile = `${__dirname}/data/tags.json`
@@ -20,6 +27,8 @@ export async function get({ url }) {
   const blobs = []
   let tagsFilteredParam = []
 
+  // const redis = new Redis(`rediss://:${AZURE_REDIS_CACHEKEY}@${AZURE_REDIS_HOSTNAME}:6380`)
+
   // get tags parameter
   if (url.searchParams.get('tags')) {
     const tagsParam = url.searchParams.get('tags').split(',')
@@ -33,6 +42,12 @@ export async function get({ url }) {
 
       for (const tag of tagsFilteredParam) {
         for (const tagKey of TagKeys) {
+          // const data = JSON.parse(await redis.get(`${tagKey}='${tag}'`))
+          const tagKeyBlobs = []
+
+          // if (data === null) {
+          //   tagKeyBlobs = []
+
           for await (const blob of blobServiceClient.findBlobsByTags(`${tagKey}='${tag}'`)) {
             containers.add(blob.containerName)
             const containerClient = blobServiceClient.getContainerClient(blob.containerName)
@@ -46,17 +61,29 @@ export async function get({ url }) {
               blobName = blob.name.split('/').at(-2)
             }
 
-            blobs.push({
+            tagKeyBlobs.push({
               name: blobName,
               container: blob.containerName,
               tags: tags.tags,
               isVector,
             })
           }
+
+          // await redis.setex(`${tagKey}='${tag}'`, 120, JSON.stringify(tagKeyBlobs))
+          // } else {
+          //   tagKeyBlobs = data
+          // }
+
+          tagKeyBlobs.forEach((tagKeyBlob: TagLayer) => {
+            blobs.push(tagKeyBlob)
+            containers.add(tagKeyBlob.container)
+          })
         }
       }
     }
   }
+
+  // await redis.disconnect()
 
   const endTime = performance.now()
   console.log(`    `)
