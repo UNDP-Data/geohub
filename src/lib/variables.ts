7b14151896fd58368237ca2ae664e7fccbf9f76b
foochris <foochris@gmail.com>
2022-06-22 19:08:00 -0400

    fix: remove redis reference

--- src/lib/variables.ts
+++ src/lib/variables.ts
@@ -3,5 +3,3 @@ dotenv.config()
 
 export const AZURE_STORAGE_ACCOUNT: string = process.env.AZURE_STORAGE_ACCOUNT || ''
 export const AZURE_STORAGE_ACCESS_KEY: string = process.env.AZURE_STORAGE_ACCESS_KEY || ''
-export const AZURE_REDIS_HOSTNAME: string = process.env.AZURE_REDIS_HOSTNAME || ''
-export const AZURE_REDIS_CACHEKEY: string = process.env.AZURE_REDIS_CACHEKEY || ''
