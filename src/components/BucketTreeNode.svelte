348d00e39e4da1c52064701b3cfa769ca786e256
Jin Igarashi <igarashi.jin@water-gis.com>
2022-07-18 17:43:54 +0100

    refactor: created BucketTreeNode component

--- src/components/BucketTreeNode.svelte
+++ src/components/BucketTreeNode.svelte
@@ -23,6 +23,7 @@
   import { cloneDeep } from 'lodash-es'
 
   import AddLayerModal from '$components/controls/AddLayerModal.svelte'
+  import BucketTreeNodeCard from '$components/BucketTreeNodeCard.svelte'
   import {
     ClassificationMethodTypes,
     COLOR_CLASS_COUNT,
@@ -737,32 +738,7 @@
 
 {#if layerInfoMetadata?.visible}
   <div id="tooltip" data-testid="tooltip" use:popperContent={popperOptions} transition:fade>
-    <div class="columns is-vcentered is-mobile">
-      <div class="column is-full">
-        <div class="label">{clean(label)}</div>
-        <div class="description">{layerInfoMetadata?.description}</div>
-        <div class="source is-size-6">
-          <span class="has-text-weight-bold">Source: </span>{layerInfoMetadata?.source
-            ? layerInfoMetadata.source
-            : 'N/A'}
-        </div>
-        {#if layerInfoMetadata?.unit}
-          <div class="unit is-size-6">
-            <span class="has-text-weight-bold">Unit: </span>{layerInfoMetadata?.unit ? layerInfoMetadata.unit : 'N/A'}
-          </div>
-        {/if}
-
-        <div class="content is-size-7 tags pt-3">
-          {#if node.tags}
-            {#each Object.values(node.tags) as tag}
-              <span title="tag" style="margin-right: 5px; font-weight: bold;">
-                <span class="tag is-info is-small is-light">{clean(tag)}</span>
-              </span>
-            {/each}
-          {/if}
-        </div>
-      </div>
-    </div>
+    <BucketTreeNodeCard bind:layerInfoMetadata bind:node />
     <div id="arrow" data-popper-arrow />
   </div>
 {/if}
@@ -829,41 +805,5 @@
 
   #tooltip {
     max-width: 450px;
-    width: 450px;
-
-    .columns {
-      z-index: 10;
-      position: relative;
-
-      .is-full {
-        padding-right: 40px;
-
-        .description,
-        .source,
-        .unit {
-          font-weight: normal;
-          color: #000;
-          margin-bottom: 10px;
-
-          @media (prefers-color-scheme: dark) {
-            color: #fff;
-          }
-        }
-
-        .label {
-          border-bottom: 1px solid #ccc;
-          padding-bottom: 5px;
-          margin-bottom: 10px;
-
-          @media (prefers-color-scheme: dark) {
-            color: #fff;
-          }
-        }
-
-        .description {
-          margin-bottom: 15px;
-        }
-      }
-    }
   }
 </style>
