1ea093dc7ad3033c7c520678a7c667daa2df43cf
Jin Igarashi <igarashi.jin@water-gis.com>
2022-07-29 19:45:05 +0100

    Feat/data sharing (#573)

---
+++ src/routes/style/[id].json.ts
@@ -0,0 +1,48 @@
+import { Pool } from 'pg'
+
+const connectionString = import.meta.env.VITE_DATABASE_CONNECTION
+
+/**
+ * Get style.json which is stored in PostgreSQL database
+ * GET: ./style/{id}.json
+ */
+export async function get({ params }) {
+  const pool = new Pool({ connectionString })
+  const client = await pool.connect()
+  try {
+    const styleId = params.id
+    if (!styleId) {
+      throw new Error(`id parameter is required.`)
+    }
+    const query = {
+      text: `SELECT style FROM geohub.style WHERE id = $1`,
+      values: [styleId],
+    }
+
+    const res = await client.query(query)
+    if (res.rowCount === 0) {
+      throw new Error(`${styleId} does not exist in the database`)
+    }
+    const style = res.rows[0].style
+    return {
+      status: 200,
+      headers: {
+        'access-control-allow-origin': '*',
+      },
+      body: style,
+    }
+  } catch (err) {
+    return {
+      status: 400,
+      headers: {
+        'access-control-allow-origin': '*',
+      },
+      body: {
+        message: err.message,
+      },
+    }
+  } finally {
+    client.release()
+    pool.end()
+  }
+}
---
+++ src/routes/style/index.ts
@@ -0,0 +1,58 @@
+import { Pool } from 'pg'
+
+const connectionString = import.meta.env.VITE_DATABASE_CONNECTION
+
+/**
+ * Get style.json which is stored in PostgreSQL database
+ * POST: ./style
+ * body = {
+ *   name: [style name]
+ *   style: [style.json]
+ * }
+ */
+export async function post({ request, url }) {
+  const pool = new Pool({ connectionString })
+  const client = await pool.connect()
+  try {
+    const body = await request.json()
+    if (!body.name) {
+      throw new Error('name property is required')
+    }
+    if (!body.style) {
+      throw new Error('style property is required')
+    }
+
+    const query = {
+      text: `INSERT INTO geohub.style (name, style) VALUES ($1, $2) returning id`,
+      values: [body.name, JSON.stringify(body.style)],
+    }
+
+    const res = await client.query(query)
+    if (res.rowCount === 0) {
+      throw new Error('failed to insert to the database.')
+    }
+    const id = res.rows[0].id
+    return {
+      status: 200,
+      headers: {
+        'access-control-allow-origin': '*',
+      },
+      body: {
+        url: `${url.origin}/viewer?style=${url.origin}/style/${id}.json`,
+      },
+    }
+  } catch (err) {
+    return {
+      status: 400,
+      headers: {
+        'access-control-allow-origin': '*',
+      },
+      body: {
+        message: err.message,
+      },
+    }
+  } finally {
+    client.release()
+    pool.end()
+  }
+}
