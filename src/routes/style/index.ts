c03c0849a4661f0ac14bf19cd19806b600c1efbe
Jin Igarashi <igarashi.jin@water-gis.com>
2022-07-29 20:49:39 +0100

    added get api to retrieve saved style list from database

--- src/routes/style/index.ts
+++ src/routes/style/index.ts
@@ -4,7 +4,68 @@ const { Pool } = pkg
 const connectionString = import.meta.env.VITE_DATABASE_CONNECTION
 
 /**
- * Get style.json which is stored in PostgreSQL database
+ * Get the list of saved style from PostGIS database
+ * GET: ./style
+ * [
+ *   {
+ *     "id": 1,
+ *     "name": "UNDP GeoHub style",
+ *     "createdat": "2022-07-29T15:59:23.781Z",
+ *     "style": "http://localhost:3000/style/1.json",
+ *     "viewer": "http://localhost:3000/viewer?style=http://localhost:3000/style/1.json"
+ *   }
+ * ]
+ */
+export async function get({ url }) {
+  const pool = new Pool({ connectionString })
+  const client = await pool.connect()
+  try {
+    const query = {
+      text: `SELECT id, name, createdat FROM geohub.style`,
+      values: [],
+    }
+
+    const res = await client.query(query)
+    if (res.rowCount === 0) {
+      return {
+        status: 404,
+        headers: {
+          'access-control-allow-origin': '*',
+        },
+      }
+    }
+
+    res.rows.forEach((row) => {
+      const id = row.id
+      row.style = `${url.origin}/style/${id}.json`
+      row.viewer = `${url.origin}/viewer?style=${row.style}`
+    })
+
+    return {
+      status: 200,
+      headers: {
+        'access-control-allow-origin': '*',
+      },
+      body: res.rows,
+    }
+  } catch (err) {
+    return {
+      status: 400,
+      headers: {
+        'access-control-allow-origin': '*',
+      },
+      body: {
+        message: err.message,
+      },
+    }
+  } finally {
+    client.release()
+    pool.end()
+  }
+}
+
+/**
+ * Save style.json to PostgreSQL database
  * POST: ./style
  * body = {
  *   name: [style name]
