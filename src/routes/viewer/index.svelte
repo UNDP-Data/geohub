1b3f6e150adab80bbe4316f29fa838f58e5a0564
Jin Igarashi <igarashi.jin@water-gis.com>
2022-07-29 14:28:03 +0100

    feat: added viewer website

---
+++ src/routes/viewer/index.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+  import StyleViewer from '../../dashboards/components/StyleViewer.svelte'
+
+  const title = 'Geohub Simple Map Viewer'
+</script>
+
+<sveltekit:head>
+  <title>{title}</title>
+</sveltekit:head>
+
+<div>
+  <StyleViewer />
+</div>
