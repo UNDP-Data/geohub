bb11ae2bb1802d89f90a8ae3dd3302cdf274daee
Jin Igarashi <igarashi.jin@water-gis.com>
2022-08-03 17:12:28 +0100

    feat: added delete style menu

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -91,6 +91,7 @@ export enum ErrorMessages {
   VECTOR_INFO_BAD_REQUEST = 'Bad request. Please verify the URL and/or parameters.',
   MAP_FILTER_NOT_APPLIED = 'The map filter was not applied. Please check the that all filters are valid.',
   EXPRESSION_INVALID = 'The Raster Expression appears to be Invalid. Please check it and try again.',
+  NO_STYLE_EXISTS = 'No style id exists in the database',
 }
 export const LayerIconTypes = [
   {
