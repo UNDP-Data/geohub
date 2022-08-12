756610887912cd3227b59a217bc495cbf8c21fe5
Thuhaa <josephthuha@gmail.com>
2022-08-12 04:04:41 +0300

    minor redesign

--- src/components/UNDPComponents/Header_UNDP.svelte
+++ src/components/UNDPComponents/Header_UNDP.svelte
@@ -3,6 +3,7 @@
   import { faBars } from '@fortawesome/free-solid-svg-icons/faBars'
   import { faChalkboardUser } from '@fortawesome/free-solid-svg-icons/faChalkboardUser'
   import Fa from 'svelte-fa'
+  import { faX } from '@fortawesome/free-solid-svg-icons/faX'
 
   export let drawerOpen = true
 
@@ -29,19 +30,23 @@
     <div style="display: flex; justify-content: space-between; align-items: center">
       <div class="logo-div">
         <a href="https://undpgeohub.org" class="logo" tabIndex="0">
-          <img style="height: 8vh;" src="1200px-UNDP_logo.svg-148x300.png" alt="GeoHub | UNDP" loading="lazy" />
+          <img style="height: 8vh;" src="undp-images/undp-logo-blue.svg" alt="GeoHub | UNDP" loading="lazy" />
         </a>
         <div class="site-title" style="margin-left: 5px">
-          <span style="color: #006EB5" class="title">GeoHub</span>
+          <span style="color: #232E3D" class="title">GeoHub</span>
         </div>
       </div>
       <div style="width:5%; display:flex; align-items:center; justify-content:space-evenly; margin-right: 2%;">
         <div class="icon" on:click={() => window.open('/dashboards', '_blank')}>
           <Fa style="color: #006EB5" class="fa-icon" icon={faChalkboardUser} size="lg" />
         </div>
+
         <div class="icon" on:click={() => (drawerOpen = !drawerOpen)}>
-          <img src="undp-images/hamburger.svg" />
-          <!--          <Fa style="color: #006EB5" class='fa-icon' icon={faBars} size="lg" />-->
+          {#if drawerOpen}
+            <Fa style="color: #006EB5" class="fa-icon" icon={faX} size="lg" />
+          {:else}
+            <Fa style="color: #006EB5" class="fa-icon" icon={faBars} size="lg" />
+          {/if}
         </div>
       </div>
     </div>
@@ -75,5 +80,9 @@
   }
   .fa-icon {
     color: $dark-azure;
+  }
+  .title {
+    font-size: 1.5rem;
+    font-weight: bold;
   }
 </style>
