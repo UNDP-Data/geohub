5d690a1b9d430f306bc383cd50e3c5f970b5273c
Thuhaa <josephthuha@gmail.com>
2022-08-31 21:20:48 +0300

    fix: state management

--- src/components/IntervalsLegend.svelte
+++ src/components/IntervalsLegend.svelte
@@ -74,19 +74,21 @@
     const layerStats: RasterLayerStats = await fetchUrl(statsURL)
     info = { ...info, stats: layerStats }
     const band = info.active_band_no
-
     percentile98 = layerStats[band]['percentile_98']
     layerConfig.percentile98 = percentile98
     const skewness = 3 * ((info.stats[band].mean - info.stats[band].median) / info.stats[band].std)
-    if (skewness > 1 && skewness > -1) {
-      // Layer isn't higly skewed.
-      classificationMethod = ClassificationMethodTypes.EQUIDISTANT // Default classification method
-      layerConfig.intervals.classification = classificationMethod
+    if (layerConfig.intervals.classification !== ClassificationMethodTypes.LOGARITHMIC) {
+      //pass
     } else {
-      classificationMethod = ClassificationMethodTypes.LOGARITHMIC
-      layerConfig.intervals.classification = classificationMethod
+      if (skewness > 1 && skewness > -1) {
+        // Layer isn't higly skewed.
+        classificationMethod = ClassificationMethodTypes.EQUIDISTANT // Default classification method
+        layerConfig.intervals.classification = classificationMethod
+      } else {
+        classificationMethod = ClassificationMethodTypes.LOGARITHMIC
+        layerConfig.intervals.classification = classificationMethod
+      }
     }
-
     layerConfig = { ...layerConfig, info: info }
     const layers = $layerList.map((layer) => {
       return layerConfig.definition.id !== layer.definition.id ? layer : layerConfig
