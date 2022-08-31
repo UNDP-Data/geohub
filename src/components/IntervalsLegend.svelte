137bdd9e05cb2ce803f8dd4162f80f487e11ffb1
Joseph Thuha <josephthuha@gmail.com>
2022-08-31 21:31:31 +0300

    Fix  logarithmic classification (#589)
    * fix:fix logarithmic classification for highly skewed data
    
    * fix: state management

--- src/components/IntervalsLegend.svelte
+++ src/components/IntervalsLegend.svelte
@@ -70,15 +70,16 @@
   }
 
   onMount(async () => {
-    if (!('stats' in info)) {
-      const statsURL = `${TITILER_API_ENDPOINT}/statistics?url=${layerURL.searchParams.get('url')}&histogram_bins=20`
-      const layerStats: RasterLayerStats = await fetchUrl(statsURL)
-      info = { ...info, stats: layerStats }
-      const band = info.active_band_no
-
-      percentile98 = layerStats[band]['percentile_98']
-      layerConfig.percentile98 = percentile98
-      const skewness = 3 * ((info.stats[band].mean - info.stats[band].median) / info.stats[band].std)
+    const statsURL = `${TITILER_API_ENDPOINT}/statistics?url=${layerURL.searchParams.get('url')}&histogram_bins=20`
+    const layerStats: RasterLayerStats = await fetchUrl(statsURL)
+    info = { ...info, stats: layerStats }
+    const band = info.active_band_no
+    percentile98 = layerStats[band]['percentile_98']
+    layerConfig.percentile98 = percentile98
+    const skewness = 3 * ((info.stats[band].mean - info.stats[band].median) / info.stats[band].std)
+    if (layerConfig.intervals.classification !== ClassificationMethodTypes.LOGARITHMIC) {
+      //pass
+    } else {
       if (skewness > 1 && skewness > -1) {
         // Layer isn't higly skewed.
         classificationMethod = ClassificationMethodTypes.EQUIDISTANT // Default classification method
@@ -87,13 +88,12 @@
         classificationMethod = ClassificationMethodTypes.LOGARITHMIC
         layerConfig.intervals.classification = classificationMethod
       }
-
-      layerConfig = { ...layerConfig, info: info }
-      const layers = $layerList.map((layer) => {
-        return layerConfig.definition.id !== layer.definition.id ? layer : layerConfig
-      })
-      layerList.set([...layers])
     }
+    layerConfig = { ...layerConfig, info: info }
+    const layers = $layerList.map((layer) => {
+      return layerConfig.definition.id !== layer.definition.id ? layer : layerConfig
+    })
+    layerList.set([...layers])
     reclassifyImage()
   })
 
