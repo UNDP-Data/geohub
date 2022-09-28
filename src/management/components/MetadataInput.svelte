36675e50d3d64e0f23845ad409137c72130e8caa
Jin Igarashi <igarashi.jin@water-gis.com>
2022-09-28 15:04:05 +0100

    added status condition icon in metadata form

--- src/management/components/MetadataInput.svelte
+++ src/management/components/MetadataInput.svelte
@@ -12,6 +12,7 @@
     subnationals,
     units,
   } from '../constants'
+  import ConditionTag from './ConditionTag.svelte'
 
   export let metadata: Metadata = {}
   export let metadataFormCompleted = false
@@ -60,7 +61,7 @@
         <label class="label">{MetadataConfig.sdgTarget.title}</label>
       </div>
       <div class="field-body">
-        <div class="field">
+        <div class="field metadata-control">
           <p class="control">
             <input
               class="input"
@@ -68,6 +69,9 @@
               bind:value={metadata.sdgTarget}
               placeholder="represented by 2-3 keywords extracted from sdg target descriptions" />
           </p>
+          <div class="condition-tag">
+            <ConditionTag isSuccess={metadata.sdgTarget && metadata.sdgTarget.length > 0} />
+          </div>
         </div>
       </div>
     </div>
@@ -78,7 +82,7 @@
         <label class="label">{MetadataConfig.theme.title}</label>
       </div>
       <div class="field-body">
-        <div class="field">
+        <div class="field metadata-control">
           <p>
             <input
               class="input"
@@ -86,6 +90,9 @@
               bind:value={metadata.theme}
               placeholder="the overall theme of the data collection, e.g., vegetation health, pregnancies" />
           </p>
+          <div class="condition-tag">
+            <ConditionTag isSuccess={metadata.theme && metadata.theme.length > 0} />
+          </div>
         </div>
       </div>
     </div>
@@ -97,7 +104,7 @@
         <label class="label">{MetadataConfig.layerDescription.title}</label>
       </div>
       <div class="field-body">
-        <div class="field">
+        <div class="field metadata-control">
           <p>
             <input
               class="input"
@@ -105,6 +112,9 @@
               bind:value={metadata.layerDescription}
               placeholder="Description of the dataset" />
           </p>
+          <div class="condition-tag">
+            <ConditionTag isSuccess={metadata.layerDescription && metadata.layerDescription.length > 0} />
+          </div>
         </div>
       </div>
     </div>
@@ -116,7 +126,7 @@
         <label class="label">{MetadataConfig.year.title}</label>
       </div>
       <div class="field-body">
-        <div class="field">
+        <div class="field metadata-control">
           <p />
           <AutoComplete
             items={yearSelection}
@@ -124,6 +134,9 @@
             placeholder="Select the unit of the data"
             showClear={true}
             lock={true} />
+          <div class="condition-tag">
+            <ConditionTag isSuccess={metadata.year ? true : false} />
+          </div>
         </div>
       </div>
     </div>
@@ -135,13 +148,16 @@
         <label class="label">{MetadataConfig.extent.title}</label>
       </div>
       <div class="field-body">
-        <div class="field">
+        <div class="field metadata-control">
           <p />
           <AutoComplete
             items={dataExtents}
             bind:selectedItem={metadata.extent}
             placeholder="Select the geographic coverage of the data"
             showClear={true} />
+          <div class="condition-tag">
+            <ConditionTag isSuccess={metadata.extent && metadata.extent.length > 0} />
+          </div>
         </div>
       </div>
     </div>
@@ -153,26 +169,29 @@
         <label class="label">{MetadataConfig.resolution.title}</label>
       </div>
       <div class="field-body">
-        <div class="field">
+        <div class="field metadata-control">
           <p />
           <AutoComplete
             items={resolutions}
             bind:selectedItem={metadata.resolution}
             placeholder="Select the resolution of the data"
             showClear={true} />
+          <div class="condition-tag">
+            <ConditionTag isSuccess={metadata.resolution && metadata.resolution.length > 0} />
+          </div>
         </div>
       </div>
     </div>
   </div>
 
-  {#if !['', 'Global'].includes(metadata.extent)}
+  {#if metadata.extent && !['', 'Global'].includes(metadata.extent)}
     <div class="column is-6">
       <div class="field is-horizontal">
         <div class="field-label is-normal">
           <label class="label">{MetadataConfig.granularity.title}</label>
         </div>
         <div class="field-body">
-          <div class="field">
+          <div class="field metadata-control">
             <p />
             {#if metadata.extent === 'Continental'}
               <AutoComplete
@@ -199,6 +218,9 @@
                 placeholder="Select the level of detail at the data"
                 showClear={true} />
             {/if}
+            <div class="condition-tag">
+              <ConditionTag isSuccess={metadata.granularity && metadata.granularity.length > 0} />
+            </div>
           </div>
         </div>
       </div>
@@ -211,13 +233,16 @@
         <label class="label">{MetadataConfig.unit.title}</label>
       </div>
       <div class="field-body">
-        <div class="field">
+        <div class="field metadata-control">
           <p />
           <AutoComplete
             items={units}
             bind:selectedItem={metadata.unit}
             placeholder="Select the unit of the data"
             showClear={true} />
+          <div class="condition-tag">
+            <ConditionTag isSuccess={metadata.unit && metadata.unit.length > 0} />
+          </div>
         </div>
       </div>
     </div>
@@ -229,10 +254,13 @@
         <label class="label">{MetadataConfig.source.title}</label>
       </div>
       <div class="field-body">
-        <div class="field">
+        <div class="field metadata-control">
           <p>
             <input class="input" type="text" bind:value={metadata.source} placeholder="Source of the data" />
           </p>
+          <div class="condition-tag">
+            <ConditionTag isSuccess={metadata.source && metadata.source.length > 0} />
+          </div>
         </div>
       </div>
     </div>
@@ -244,10 +272,13 @@
         <label class="label">{MetadataConfig.email.title}</label>
       </div>
       <div class="field-body">
-        <div class="field">
+        <div class="field metadata-control">
           <p>
             <input class="input" type="email" bind:value={metadata.email} placeholder="Contact email" />
           </p>
+          <div class="condition-tag">
+            <ConditionTag isSuccess={metadata.email && EmailValidator.validate(metadata.email)} />
+          </div>
         </div>
       </div>
     </div>
@@ -276,5 +307,15 @@
     border-radius: 10px;
     border: 1px solid #ccc;
     box-shadow: 3px 3px 3px rgba(0, 0, 0, 0.1);
+  }
+
+  .metadata-control {
+    position: relative;
+  }
+
+  .condition-tag {
+    position: absolute;
+    top: 7px;
+    right: -20px;
   }
 </style>
