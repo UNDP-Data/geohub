abb0ed9e9eb6de788a82f066ff3b5e8131e34322
Jin Igarashi <igarashi.jin@water-gis.com>
2022-09-28 13:33:36 +0100

    introduced autocomplete for upload

--- src/management/constants/index.ts
+++ src/management/constants/index.ts
@@ -1,6 +1,7 @@
 export * from './continentals'
 export * from './countries'
 export * from './dataExtents'
+export * from './metadataConfig'
 export * from './regions'
 export * from './resolutions'
 export * from './subnationals'
