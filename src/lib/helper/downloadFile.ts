5bbf17acd42aade009da7f8a08ff80e0eef89bce
Jin Igarashi <igarashi.jin@water-gis.com>
2022-10-11 17:57:56 +0100

    separated every function under helper.ts

---
+++ src/lib/helper/downloadFile.ts
@@ -0,0 +1,21 @@
+import mime from 'mime'
+
+/**
+ * Download a file
+ * @param filename
+ * @param content
+ */
+export const downloadFile = (filename: string, content?: string) => {
+  const element = document.createElement('a')
+
+  if (content) {
+    const type = mime.getType(filename.split('.').pop())
+    element.href = `data:${type};charset=utf-8,` + encodeURIComponent(content)
+  } else {
+    element.href = filename
+  }
+
+  element.download = filename
+  element.click()
+  element.remove()
+}
