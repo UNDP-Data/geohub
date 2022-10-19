5da3f756ec0f880a53ca48382293e6c193fe6547
janf <ioan.ferencik@solargis.com>
2022-10-20 00:38:30 +0200

    fix: property buttons styles

--- src/components/controls/vector-styles/PropertySelectButtons.svelte
+++ src/components/controls/vector-styles/PropertySelectButtons.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
   import { onMount } from 'svelte'
   import { createEventDispatcher } from 'svelte'
-
+  import { clean } from '$lib/helper'
   export let layer
   export let propertySelectValue
   export let showEmptyFields = false
@@ -58,11 +58,51 @@
   $: propertySelectValue, propertyChanged()
 </script>
 
-<div style="max-height: 200px; overflow-y: auto" class="grid pt-5 pb-5" role="menu" on:click={handleClick}>
+<div style="max-height: 200px; overflow-y: auto" class="grid-wrapper" role="menu" on:click={handleClick}>
   {#if propertySelectOptions}
     {#each propertySelectOptions as propertySelectOption}
       {#if design1}
-        <div
+        <div 
+            class="grid-item card p-0 m-0 is-info is-clickable  has-text-centered"
+            on:click={() => {
+              propertySelectValue = propertySelectOption;
+              handleClick
+            }}
+            title={layer.info.json.vector_layers[0].fields[propertySelectOption].toLowerCase() === 'string'
+            ? `${clean(propertySelectOption)}, textual property`
+            : ` ${clean(propertySelectOption)0}, numeric property`}
+          >
+          <div
+            class="card-header is-size-6  pb-0 pt-0 m-0 {propertySelectValue === propertySelectOption
+              ? 'has-background-success'
+              : 'has-background-info-dark'} ">
+            <span
+              class="card-header-title grid-item is-centered is-v-centered {propertySelectValue === propertySelectOption
+                ? 'has-text-white-ter'
+                : 'has-text-white-ter'}  ">
+              {clean(propertySelectOption)}
+              {#if propertySelectOption === propertySelectValue}
+                <span class="icon ">
+                  <i class="fa-solid fa-check" />
+                </span>
+              {/if}
+            </span>
+          </div>
+          <div class="card-content has-text-centered ">
+            <div class="content has-text-weight-bold ">
+              
+              {#if layer.info.json.vector_layers[0].fields[propertySelectOption].toLowerCase() !== 'string'}
+                <span  class="tag is-size-6 has-background-danger-dark has-text-white p-1 m-0 ">123456</span>
+              {:else}
+              <span class="tag is-size-6 has-background-danger-dark has-text-white p-2 m-0 ">Abcdef</span>
+                
+              {/if}
+            </div>
+          </div>
+            
+          
+        </div>
+        <!-- <div
           class="card grid-item p-0 m-0 is-clickable  "
           on:click={() => {
             propertySelectValue = propertySelectOption;
@@ -99,7 +139,7 @@
               {/if}
             </div>
           </div>
-        </div>
+        </div> -->
       {:else}
         <div
           class="card grid-item p-0 m-0  {propertySelectOption === propertySelectValue
@@ -136,61 +176,72 @@
 </div>
 
 <style lang="scss">
-  .grid {
+  .grid-wrapper {
     display: grid;
-    grid-template-columns: repeat(2, 1fr);
-    grid-gap: 15px;
-    padding: 0px;
-    scrollbar-width: thin; /* "auto" or "thin" */
-    scrollbar-color: blue orange;
+    grid-template-columns: repeat(2, minmax(0, 1fr));
+    gap: 10px;
+    //display: grid;
+    //grid-auto-columns: minmax(2, auto);
+    //grid-auto-flow: ;;
+    // grid-template-columns:repeat(2, minmax(0, 1fr));;
+    // grid-gap: 10px;
+    // padding: 0px;
+    //scrollbar-width: thin; /* "auto" or "thin" */
+    //scrollbar-color: blue orange;
   }
   .grid-item {
-    cursor: pointer;
-    text-align: center;
+    display: inline-block;
+    // border: 1px solid gray;
+    text-overflow: ellipsis;
+    white-space: nowrap;
+    overflow: hidden;
+    //max-width: 60px;
+    // cursor: pointer;
+    // text-align: center;
   }
 
-  .fa-spell-check {
-    position: absolute;
-    top: 0;
-    right: 0;
-  }
-  :global(.vector-property-card) {
-    margin: 0;
-    padding: 0;
-    width: 100%;
-    height: 50px;
-  }
+  // .fa-spell-check {
+  //   position: absolute;
+  //   top: 0;
+  //   right: 0;
+  // }
+  // :global(.vector-property-card) {
+  //   margin: 0;
+  //   padding: 0;
+  //   width: 100%;
+  //   height: 50px;
+  // }
 
-  .vector-property-card:hover {
-    background-color: rgba(0, 0, 0, 0.1);
-    cursor: pointer;
-  }
+  // .vector-property-card:hover {
+  //   background-color: rgba(0, 0, 0, 0.1);
+  //   cursor: pointer;
+  // }
 
-  .clicked {
-    background-color: rgba(0, 0, 0, 0.1);
-    border: 2px solid #000;
-  }
+  // .clicked {
+  //   background-color: rgba(0, 0, 0, 0.1);
+  //   border: 2px solid #000;
+  // }
 
-  .property-info {
-    position: absolute;
-    font-size: 10px;
-    top: 0;
-    right: 0;
-  }
-  :global(.vector-expression-card-content) {
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    height: 100%;
-    width: 100%;
-  }
+  // .property-info {
+  //   position: absolute;
+  //   font-size: 10px;
+  //   top: 0;
+  //   right: 0;
+  // }
+  // :global(.vector-expression-card-content) {
+  //   display: flex;
+  //   justify-content: center;
+  //   align-items: center;
+  //   height: 100%;
+  //   width: 100%;
+  // }
 
-  :global(.text-centered) {
-    font-size: 10px;
-    text-align: center;
-    vertical-align: middle;
-    word-break: break-word;
-    width: 90%;
-    height: fit-content;
-  }
+  // :global(.text-centered) {
+  //   font-size: 10px;
+  //   text-align: center;
+  //   vertical-align: middle;
+  //   word-break: break-word;
+  //   width: 90%;
+  //   height: fit-content;
+  // }
 </style>
