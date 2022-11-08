d8bcdc05cfe00a89bb0a89f447acdc83e704a182
Jin Igarashi <igarashi.jin@water-gis.com>
2022-11-08 13:59:38 +0000

    deleted propertyOptions from IntervalLegend

--- src/lib/types/IntervalLegend.ts
+++ src/lib/types/IntervalLegend.ts
@@ -1,9 +1,7 @@
-import type { ClassificationMethodTypes } from '$lib/constants'
 import type { IntervalLegendColorMapRow } from './IntervalLegendColorMapRow'
 
 export interface IntervalLegend {
   numberOfClasses?: number
   colorMapRows?: IntervalLegendColorMapRow[]
   propertyName?: string
-  propertyOptions?: string[]
 }
