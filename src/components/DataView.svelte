c6b4e14e9251b0c40395a8d81e7ecd3ba39ff6ca
Jin Igarashi <igarashi.jin@water-gis.com>
2022-11-28 18:50:34 +0000

    added sort button

--- src/components/DataView.svelte
+++ src/components/DataView.svelte
@@ -5,13 +5,21 @@
   import DataCard from './DataCard.svelte'
   import { map, indicatorProgress } from '$stores'
   import TextFilter from './controls/TextFilter.svelte'
-  import { indexOf } from 'lodash'
+  import { indexOf, last, startsWith } from 'lodash'
   import Notification from './controls/Notification.svelte'
   import { STAC_MINIMUM_ZOOM, SEARCH_PAGINATION_LIMIT } from '$lib/constants'
 
   let containerDivElement: HTMLDivElement
   let selectedCategories: DataCategory[] = []
   const LIMIT = SEARCH_PAGINATION_LIMIT
+  let query: string
+  let sortingColumn: 'name' | 'source' | 'license' | 'createdat' | 'updatedat' = 'name'
+  let isAsc = true
+  $: isShownSortbyButton =
+    (selectedCategories &&
+      selectedCategories.length > 0 &&
+      selectedCategories[selectedCategories.length - 1].url.startsWith('/datasets')) ||
+    (DataItemFeatureCollection ? true : false)
 
   let categories: DataCategory[] = [
     {
@@ -45,6 +53,8 @@
 
   let DataItemFeatureCollection: StacItemFeatureCollection
 
+  $: isAsc, handleSortbyChanged()
+
   const handleSelectCategory = async (category: DataCategory) => {
     try {
       $indicatorProgress = true
@@ -59,20 +69,8 @@
         ]
       }
 
-      const res = await fetch(category.url)
-      const json = await res.json()
-      const values: string[] = json[Object.keys(json)[0]]
       if (category.name === 'SDG') {
-        selectedCategories = [...selectedCategories, category]
-
-        const num_values: number[] = values.map((v) => Number(v)).sort((a, b) => a - b)
-        subCategories = num_values.map((num) => {
-          return {
-            name: `SDG${num}`,
-            icon: `/sdgs/${num}.png`,
-            url: `/datasets?sdg_goal=${num}`,
-          }
-        })
+        await searchCategory(category)
       } else {
         await handleSelectSubcategory(category)
       }
@@ -100,13 +98,32 @@
       }
 
       if (category.url.startsWith('/datasets')) {
-        const apiUrl = new URL(`${$page.url.origin}${category.url}`)
-        apiUrl.searchParams.set('limit', LIMIT.toString())
-        const res = await fetch(apiUrl.toString())
-        const json = await res.json()
-        DataItemFeatureCollection = json
+        const url = `${$page.url.origin}${category.url}`
+        await searchDatasets(url)
       }
       return category
+    } finally {
+      $indicatorProgress = false
+    }
+  }
+
+  const handleSortbyChanged = async () => {
+    if ($indicatorProgress === true) return
+    if (!DataItemFeatureCollection) return
+    try {
+      $indicatorProgress = true
+
+      const link = DataItemFeatureCollection?.links.find((link) => link.rel === 'self')
+      let url: string
+      if (link) {
+        url = link.href
+      } else {
+        const lastCategory = selectedCategories[selectedCategories.length - 1]
+        if (!lastCategory.url.startsWith('/datasets')) return
+        url = lastCategory.url
+      }
+
+      await searchDatasets(url)
     } finally {
       $indicatorProgress = false
     }
@@ -130,8 +147,44 @@
     }
   }
 
+  const searchCategory = async (category: DataCategory) => {
+    const apiUrl = new URL(`${$page.url.origin}${category.url}`)
+    const res = await fetch(apiUrl.toString())
+    const json = await res.json()
+    const values: string[] = json[Object.keys(json)[0]]
+
+    selectedCategories = [...selectedCategories, category]
+
+    const num_values: number[] = values.map((v) => Number(v)).sort((a, b) => a - b)
+    subCategories = num_values.map((num) => {
+      return {
+        name: `SDG${num}`,
+        icon: `/sdgs/${num}.png`,
+        url: `/datasets?sdg_goal=${num}`,
+      }
+    })
+  }
+
+  const searchDatasets = async (url: string) => {
+    const apiUrl = new URL(url)
+    if (query) {
+      if (query.length === 0) {
+        apiUrl.searchParams.delete('query')
+      } else {
+        apiUrl.searchParams.set('query', query)
+      }
+    }
+    apiUrl.searchParams.set('sortby', [sortingColumn, `${isAsc ? 'asc' : 'desc'}`].join(','))
+    apiUrl.searchParams.set('limit', LIMIT.toString())
+    apiUrl.searchParams.delete('offset')
+    const res = await fetch(apiUrl.toString())
+    if (!res.ok) return
+    const json: StacItemFeatureCollection = await res.json()
+    DataItemFeatureCollection = json
+  }
+
   const handleFilterInput = async (e) => {
-    const query = e.detail.query
+    query = e.detail.query
 
     try {
       $indicatorProgress = true
@@ -141,23 +194,14 @@
       if (link) {
         url = link.href
       }
-      const apiUrl = new URL(url)
-      if (query.length === 0) {
-        apiUrl.searchParams.delete('query')
-      } else {
-        apiUrl.searchParams.set('query', query)
-      }
-      apiUrl.searchParams.set('limit', LIMIT.toString())
-      const res = await fetch(apiUrl.toString())
-      if (!res.ok) return
-      const json: StacItemFeatureCollection = await res.json()
-      DataItemFeatureCollection = json
+      await searchDatasets(url)
     } finally {
       $indicatorProgress = false
     }
   }
 
   const clearFilter = async () => {
+    query = ''
     if (selectedCategories) {
       const lastCategory = selectedCategories[selectedCategories.length - 1]
       if (lastCategory?.url?.startsWith('/datasets')) {
@@ -189,46 +233,58 @@
   on:clear={clearFilter} />
 
 <div
-  class="container data-view-container m-4"
+  class="container data-view-container mx-4"
   on:scroll={handleScroll}
   bind:this={containerDivElement}>
-  {#if selectedCategories && selectedCategories.length > 0}
-    <nav
-      class="breadcrumb has-succeeds-separator bread-crumb-margin"
-      aria-label="breadcrumbs">
-      <ul class="bread-crumb-margin">
-        {#each selectedCategories as category, index}
-          {#if index === 0}
-            <li class="bread-crumb-margin">
+  <div class="data-list-header">
+    {#if selectedCategories && selectedCategories.length > 0}
+      <nav
+        class="breadcrumb has-succeeds-separator breadcrumb-margin"
+        aria-label="breadcrumbs">
+        <ul class="breadcrumb-margin">
+          {#each selectedCategories as category, index}
+            {#if index === 0}
+              <li class="breadcrumb-margin">
+                <!-- svelte-ignore a11y-missing-attribute -->
+                <a
+                  on:click={() => {
+                    selectedCategories = []
+                    subCategories = []
+                    DataItemFeatureCollection = undefined
+                    isShownSortbyButton = false
+                  }}>{category.name}</a>
+              </li>
+            {:else if index === selectedCategories.length - 1}
               <!-- svelte-ignore a11y-missing-attribute -->
-              <a
-                on:click={() => {
-                  selectedCategories = []
-                  subCategories = []
-                  DataItemFeatureCollection = undefined
-                }}>{category.name}</a>
-            </li>
-          {:else if index === selectedCategories.length - 1}
-            <!-- svelte-ignore a11y-missing-attribute -->
-            <li class="bread-crumb-margin is-active"><a>{category.name}</a></li>
-          {:else}
-            <!-- svelte-ignore a11y-missing-attribute -->
-            <li class="bread-crumb-margin">
-              <a
-                on:click={() => {
-                  let last = selectedCategories[selectedCategories.length - 1]
-                  while (last.name !== category.name) {
-                    selectedCategories.pop()
-                    last = selectedCategories[selectedCategories.length - 1]
-                  }
-                  DataItemFeatureCollection = undefined
-                }}>{category.name}</a>
-            </li>
-          {/if}
-        {/each}
-      </ul>
-    </nav>
-  {/if}
+              <li class="breadcrumb-margin is-active"><a>{category.name}</a></li>
+            {:else}
+              <!-- svelte-ignore a11y-missing-attribute -->
+              <li class="breadcrumb-margin">
+                <a
+                  on:click={() => {
+                    let last = selectedCategories[selectedCategories.length - 1]
+                    while (last.name !== category.name) {
+                      selectedCategories.pop()
+                      last = selectedCategories[selectedCategories.length - 1]
+                    }
+                    DataItemFeatureCollection = undefined
+                    isShownSortbyButton = last.url.startsWith('/datasets')
+                  }}>{category.name}</a>
+              </li>
+            {/if}
+          {/each}
+        </ul>
+      </nav>
+    {/if}
+
+    {#if isShownSortbyButton}
+      <span
+        class="icon sortby-icon"
+        on:click={() => (isAsc = !isAsc)}>
+        <i class="fas {`${isAsc ? 'fa-arrow-down-a-z' : 'fa-arrow-up-a-z'}`} fa-lg" />
+      </span>
+    {/if}
+  </div>
 
   {#if DataItemFeatureCollection && DataItemFeatureCollection.features.length > 0}
     {#each DataItemFeatureCollection.features as feature}
@@ -241,7 +297,7 @@
     <Notification type="warning">No data found</Notification>
   {:else}
     <div
-      class={`${
+      class={`container mt-2 ${
         selectedCategories && selectedCategories.length === 0 ? 'category-container' : 'sub-category-container'
       }`}>
       {#if selectedCategories && selectedCategories.length === 0}
@@ -283,8 +339,19 @@
       color: white !important;
     }
 
-    .bread-crumb-margin {
-      margin-bottom: 0px;
+    .data-list-header {
+      display: flex;
+
+      .breadcrumb-margin {
+        float: left;
+        margin-bottom: 0.2rem;
+      }
+
+      .sortby-icon {
+        cursor: pointer;
+        margin-top: 0.4rem;
+        margin-left: auto;
+      }
     }
 
     .category-container {
