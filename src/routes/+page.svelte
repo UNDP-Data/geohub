8bfa52631b555ab7af294f8adcac593edffdf7d2
Jin Igarashi <igarashi.jin@water-gis.com>
2022-11-30 15:30:58 +0000

    fixed +page.svelte for body css

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,13 +1,10 @@
 <script lang="ts">
-  import { style } from 'svelte-body'
   import Header from '$components/Header.svelte'
   import Content from '$components/Content.svelte'
   import Map from '$components/Map.svelte'
 
   let drawerOpen = true
 </script>
-
-<svelte:body use:style={{ height: '100vh', margin: '0px', padding: '0px', border: '0px solid red' }} />
 
 <Header bind:drawerOpen />
 <Content bind:drawerOpen>
