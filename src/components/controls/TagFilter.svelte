da3812bb49e6443170892b332dc86d82f0e401aa
Jin Igarashi <igarashi.jin@water-gis.com>
2022-12-01 18:14:29 +0000

    added clear all tags button

--- src/components/controls/TagFilter.svelte
+++ src/components/controls/TagFilter.svelte
@@ -47,48 +47,68 @@
     const tags = selectedTags?.filter((t) => t.key === key)
     return !(tags && tags.length > 0)
   }
+
+  const clearAllTags = () => {
+    selectedTags = []
+  }
 </script>
 
-<div class="box px-2 py-0">
+<div class="box px-2 py-0 mb-2">
   <TreeView
     lineColor="#ff0000"
     iconBackgroundColor="#ff0000"
     iconColor="#FFFFFF"
     branchHoverColor="#ff0000">
-    {#if tagSearchKeys}
-      {#each tagSearchKeys as key}
-        <TreeBranch
-          rootContent={key.label}
-          defaultClosed={checkChildrenTicked(key.key)}>
-          {#if tags[key.key]}
-            {#each tags[key.key] as tag}
-              <TreeLeaf>
-                <div class="form-check">
-                  <input
-                    type="checkbox"
-                    id="{tag.key}-{tag.value}"
-                    name="{tag.key}-{tag.value}"
-                    on:click={() => {
-                      handleTagChecked(tag)
-                    }}
-                    checked={existTag(tag)} />
-                  <label for="{tag.key}-{tag.value}">{tag.value} ({tag.count})</label>
-                </div>
-              </TreeLeaf>
-            {/each}
-          {/if}
-        </TreeBranch>
-      {/each}
-    {/if}
+    {#key selectedTags}
+      {#if tagSearchKeys}
+        {#each tagSearchKeys as key}
+          <TreeBranch
+            rootContent={key.label}
+            defaultClosed={checkChildrenTicked(key.key)}>
+            {#if tags[key.key]}
+              {#each tags[key.key] as tag}
+                <TreeLeaf>
+                  <div class="form-check">
+                    <input
+                      type="checkbox"
+                      id="{tag.key}-{tag.value}"
+                      name="{tag.key}-{tag.value}"
+                      on:click={() => {
+                        handleTagChecked(tag)
+                      }}
+                      checked={existTag(tag)} />
+                    <label for="{tag.key}-{tag.value}">{tag.value} ({tag.count})</label>
+                  </div>
+                </TreeLeaf>
+              {/each}
+            {/if}
+          </TreeBranch>
+        {/each}
+      {/if}
+    {/key}
   </TreeView>
 </div>
+{#if selectedTags?.length > 0}
+  <!-- svelte-ignore a11y-missing-attribute -->
+  <a
+    class="button button-primary button-without-arrow clear-tag-button"
+    role="button"
+    on:click={clearAllTags}>
+    Clear all tags
+  </a>
+{/if}
 
 <style lang="scss">
   @use '../../styles/undp-design/base-minimal.min.css';
   @use '../../styles/undp-design/checkbox.min.css';
+  @use '../../styles/undp-design/buttons.min.css';
 
   .box {
     max-height: 250px;
     overflow-y: auto;
+  }
+
+  .clear-tag-button {
+    width: 100%;
   }
 </style>
