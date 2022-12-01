b087640be2741abe59e493f42321ba695a127fb6
Jin Igarashi <igarashi.jin@water-gis.com>
2022-12-01 15:08:11 +0000

    deleted unused immport

--- src/components/Map.svelte
+++ src/components/Map.svelte
@@ -10,7 +10,7 @@
   import { styles } from '$lib/constants'
   import { loadImageToDataUrl, fetchUrl, clipSprite } from '$lib/helper'
   import type { Sprite } from '$lib/types'
-  import { indicatorProgress, map, spriteImageList } from '$stores'
+  import { map, spriteImageList } from '$stores'
   import { PUBLIC_AZURE_URL } from '$lib/variables/public'
 
   let adminLayer: AdminLayer = null
