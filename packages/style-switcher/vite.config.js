ded56f290560ea226deda6dce29d9d2b0bfafbd7
Jin Igarashi <igarashi.jin@water-gis.com>
2022-12-07 17:39:07 +0000

    created style-switcher package

---
+++ packages/style-switcher/vite.config.js
@@ -0,0 +1,14 @@
+import { sveltekit } from '@sveltejs/kit/vite';
+import { resolve } from 'path';
+
+/** @type {import('vite').UserConfig} */
+const config = {
+	plugins: [sveltekit()],
+	resolve: {
+		alias: {
+			$example: resolve('./src/example')
+		}
+	}
+};
+
+export default config;
