a9a1e8f65fa8d154f6248bc0d8286a243c4a8d34
Jin Igarashi <jin.igarashi@undp.org>
2022-12-21 19:39:16 +0000

    Feature/introduce carousel to dashboard (#998)

---
+++ packages/svelte-undp-design/src/lib/FluidCarousel/FluidCarousel.stories.ts
@@ -0,0 +1,58 @@
+import type { CarouselContent } from '$lib/interfaces';
+import type { Meta, StoryObj } from '@storybook/svelte';
+
+import FluidCarousel from './FluidCarousel.svelte';
+
+// More on how to set up stories at: https://storybook.js.org/docs/7.0/svelte/writing-stories/introduction
+const meta = {
+	title: 'Example/FluidCarousel',
+	component: FluidCarousel,
+	tags: ['docsPage'],
+	argTypes: {
+		contents: {
+			// type: 'string',
+			description: 'The arraylist of carousel objects to be shown',
+			defaultValue: undefined
+		}
+	}
+} satisfies Meta<FluidCarousel>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+const contents: CarouselContent[] = [
+	{
+		tag: 'Blog post',
+		imageUrl: 'media/fluid-carousel-img.jpg',
+		title: 'Entry and Exit Points: Violent Extremism in South-East Asia',
+		description:
+			'This study summarizes four complementary in-depth papers that explore the localized…',
+		linkName: 'Read More',
+		linkUrl: '#'
+	},
+	{
+		tag: 'Blog post',
+		imageUrl: 'media/fluid-carousel-img.jpg',
+		title: 'Entry and Exit Points: Violent Extremism in South-East Asia',
+		description:
+			'This study summarizes four complementary in-depth papers that explore the localized…',
+		linkName: 'Read More',
+		linkUrl: '#'
+	},
+	{
+		tag: 'Blog post',
+		imageUrl: 'media/fluid-carousel-img.jpg',
+		title: 'Entry and Exit Points: Violent Extremism in South-East Asia',
+		description:
+			'This study summarizes four complementary in-depth papers that explore the localized…',
+		linkName: 'Read More',
+		linkUrl: '#'
+	}
+];
+
+// More on writing stories with args: https://storybook.js.org/docs/7.0/svelte/writing-stories/args
+export const Primary: Story = {
+	args: {
+		contents: contents
+	}
+};
--- sites/geohub/vite.config.ts
+++ sites/geohub/vite.config.ts
@@ -5,13 +5,16 @@ import { UserConfig } from 'vite'
 const config: UserConfig = {
   plugins: [sveltekit()],
   ssr: {
-    noExternal: [/^@material(?:-extra)?\//, 'vega-embed'],
+    noExternal: [/^@material(?:-extra)?\//, 'vega-embed', 'svelte-carousel', 'simply-reactive'],
   },
   resolve: {
     alias: {
       $components: resolve('./src/components'),
       $stores: resolve('./src/stores/index.ts'),
     },
+  },
+  optimizeDeps: {
+    include: ['lodash.get', 'lodash.isequal', 'lodash.clonedeep'],
   },
 }
 
