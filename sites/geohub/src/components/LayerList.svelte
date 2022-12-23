a6742e95250187d54b2f87c63845c386c07bf813
Jin Igarashi <jin.igarashi@undp.org>
2022-12-23 17:24:57 +0000

    fix: set classification from restored style.json fro vector and raster layer (#1021)

--- sites/geohub/src/components/LayerList.svelte
+++ sites/geohub/src/components/LayerList.svelte
@@ -1,16 +1,68 @@
 <script lang="ts">
+  import { onMount } from 'svelte'
+  import type { StyleSpecification } from 'maplibre-gl'
+  import { goto } from '$app/navigation'
+  import { page } from '$app/stores'
   import RasterLayer from '$components/RasterLayer.svelte'
   import VectorLayer from '$components/VectorLayer.svelte'
-  import { map, layerList } from '$stores'
-  import { LayerTypes, TabNames } from '$lib/constants'
+  import { map, layerList, indicatorProgress } from '$stores'
+  import { ClassificationMethodTypes, LayerTypes, TabNames } from '$lib/constants'
   import { getLayerStyle } from '$lib/helper'
   import Notification from './controls/Notification.svelte'
   import LayerOrder from './LayerOrder.svelte'
 
   export let headerHeight: number = undefined
   export let tabsHeight: number = undefined
+  export let activeTab: string
   let marginTop = 5
   let layerHeaderHeight = 39
+
+  let restoredStyle: StyleSpecification
+
+  onMount(async () => {
+    try {
+      $indicatorProgress = true
+      const styleId = $page.url.searchParams.get('style')
+      if (!(styleId && $layerList.length === 0)) return
+      const res = await fetch(`/api/style/${styleId}`)
+      if (!res.ok) {
+        $page.url.searchParams.delete('style')
+        goto(`?${$page.url.searchParams.toString()}`)
+        return
+      }
+      const styleInfo = await res.json()
+      restoredStyle = styleInfo.style
+      if (styleInfo.layers) {
+        $layerList = styleInfo.layers
+        const style: StyleSpecification = styleInfo.style
+        $map.setStyle(style)
+        $map.flyTo({
+          center: [style.center[0], style.center[1]],
+          zoom: style.zoom,
+          bearing: style.bearing,
+          pitch: style.pitch,
+        })
+        activeTab = TabNames.LAYERS
+      } else {
+        $page.url.searchParams.delete('style')
+        goto(`?${$page.url.searchParams.toString()}`)
+      }
+    } finally {
+      $indicatorProgress = false
+    }
+  })
+
+  const getClassificationMethod = (layerId: string) => {
+    const layerStyle = restoredStyle?.layers.find((l) => l.id === layerId)
+    let classificationMethod = ClassificationMethodTypes.EQUIDISTANT
+    if (['fill', 'symbol'].includes(layerStyle.type)) {
+      classificationMethod = ClassificationMethodTypes.NATURAL_BREAK
+    }
+    if (layerStyle && layerStyle['classification']) {
+      classificationMethod = layerStyle['classification']
+    }
+    return classificationMethod
+  }
 </script>
 
 {#if $layerList?.length > 0}
@@ -38,9 +90,13 @@
   {#each $layerList as layer (layer.id)}
     <div class="box p-0 mx-1 my-3">
       {#if getLayerStyle($map, layer.id).type === LayerTypes.RASTER}
-        <RasterLayer {layer} />
+        <RasterLayer
+          {layer}
+          classificationMethod={getClassificationMethod(layer.id)} />
       {:else}
-        <VectorLayer {layer} />
+        <VectorLayer
+          {layer}
+          classificationMethod={getClassificationMethod(layer.id)} />
       {/if}
     </div>
   {/each}
