3835d3d5999c5689ba0bef6cfe0c4124bd0b809c
Jin Igarashi <jin.igarashi@undp.org>
2022-12-27 14:00:36 +0000

    Feature/1030 state of dashboard url (#1042)
    * add limit and offset in URL to keep state of page of style list
    
    * format
    
    * disabled prerender for dashboard

--- sites/geohub/src/routes/dashboards/+page.svelte
+++ sites/geohub/src/routes/dashboards/+page.svelte
@@ -11,7 +11,7 @@
       title: 'GeoHub Electricity Dashboard',
       description:
         'This dashboard presented here are two raster layers that display the likelihood of full electrification for a given area: High Resolution Electricity Access (HREA) and Machine Learning (ML). These are created by the University of Michigan, used to support the 2030 Social Development Goal (SDG) 7: ensuring access to affordable, reliable, sustainable and modern energy for all.',
-      linkName: 'Explore More',
+      linkName: 'Open dashboard',
       linkUrl: 'dashboards/electricity',
     },
   ]
