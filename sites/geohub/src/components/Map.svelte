5502311f380dd13b5edb71cf3adaf9e9946ba8c4
Jin Igarashi <igarashi.jin@water-gis.com>
2023-01-13 15:12:05 +0000

    added resize function on load event to initialise map size on geohub

--- sites/geohub/src/components/Map.svelte
+++ sites/geohub/src/components/Map.svelte
@@ -42,7 +42,7 @@
     newMap.addControl(exportControl, 'top-right')
 
     newMap.on('load', () => {
-      // initAdminLayer()
+      newMap.resize()
       const styleUrl = newMap.getStyle().sprite.replace('/sprite/sprite', '/sprite-non-sdf/sprite')
       const promise = Promise.all([loadImageToDataUrl(`${styleUrl}@4x.png`), fetchUrl(`${styleUrl}@4x.json`)])
       promise
