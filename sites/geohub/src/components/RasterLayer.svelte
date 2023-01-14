0cabfbec0ec103122906e70523a549439794cf93
janf <ioan.ferencik@solargis.com>
2023-01-14 18:19:17 +0100

    improve RasterIntervalsLegend

--- sites/geohub/src/components/RasterLayer.svelte
+++ sites/geohub/src/components/RasterLayer.svelte
@@ -1,5 +1,4 @@
 <script lang="ts">
-  import { onMount, tick } from 'svelte'
   import { map, layerList } from '$stores'
   import { fade } from 'svelte/transition'
   import RasterLegendContainer from '$components/controls/RasterLegendContainer.svelte'
