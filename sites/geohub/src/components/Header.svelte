f7c43cbf5b3522c7a016e78e8bf3bf209ba2a660
Jin Igarashi <igarashi.jin@water-gis.com>
2023-01-27 17:13:54 +0000

    feat: combined /viewer and main app

--- sites/geohub/src/components/Header.svelte
+++ sites/geohub/src/components/Header.svelte
@@ -11,7 +11,7 @@
 
   let isStyleShareVisible = false
 
-  const isReadonly = $page.url.pathname === '/viewer'
+  const isReadonly = $page.data.readOnly
 
   $: showProgressBar = $indicatorProgress
 
