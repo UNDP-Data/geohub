74a5490bd6d0e4aa1d0d5b1575451571fec542d6
Jin Igarashi <jin.igarashi@undp.org>
2023-01-30 20:25:03 +0000

    Refactor/1217 rename default classify (#1251)

--- sites/geohub/src/components/controls/LegendTypeSwitcher.svelte
+++ sites/geohub/src/components/controls/LegendTypeSwitcher.svelte
@@ -1,26 +1,26 @@
 <script lang="ts">
-  export let legendType: 'simple' | 'advanced' = 'simple'
+  export let legendType: 'default' | 'classify' = 'default'
 </script>
 
 <div class="centered pb-2">
   <div class="field has-addons">
     <p class="control">
       <button
-        class="button is-normal {`${legendType === 'simple' ? 'is-primary is-active' : 'is-primary is-light'}`}"
-        on:click={() => (legendType = 'simple')}>
+        class="button is-normal {`${legendType === 'default' ? 'is-primary is-active' : 'is-primary is-light'}`}"
+        on:click={() => (legendType = 'default')}>
         <span>
           <i class="fa-solid fa-minus" />
-          Simple
+          Default
         </span>
       </button>
     </p>
     <p class="control">
       <button
-        class="button is-normal {`${legendType === 'advanced' ? 'is-primary is-active' : 'is-primary is-light'}`}"
-        on:click={() => (legendType = 'advanced')}>
+        class="button is-normal {`${legendType === 'classify' ? 'is-primary is-active' : 'is-primary is-light'}`}"
+        on:click={() => (legendType = 'classify')}>
         <span>
           <i class="fa-solid fa-list" />
-          Advanced
+          Classify
         </span>
       </button>
     </p>
