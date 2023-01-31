355e6d009c49550e0a7b698d184be614178b9169
Jin Igarashi <igarashi.jin@water-gis.com>
2023-01-31 17:15:14 +0000

    fix: bug of weird maplibre error by using new way to upload source URL

--- sites/geohub/src/lib/helper/updateParamsInURL.ts
+++ sites/geohub/src/lib/helper/updateParamsInURL.ts
@@ -6,7 +6,6 @@ import type {
   SymbolLayerSpecification,
 } from 'maplibre-gl'
 
-//import type { Layer } from '$lib/types'
 import { get } from 'svelte/store'
 import { map as mapStore } from '$stores'
 import { loadMap } from './loadMap'
@@ -24,26 +23,11 @@ export const updateParamsInURL = (
     layerURL.searchParams.set(key, params[key])
   })
   const map = get(mapStore)
-  if ('getStyle' in map) {
-    const style = map.getStyle()
-
-    if (style?.sources) {
-      // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-      // @ts-ignore
-      style.sources[layerStyle.source].tiles = [decodeURI(layerURL.toString())]
-      // delete all props which have undefined value
-      // probably it is a bug of maplibre to add undefined property (like url, bounds) to the style,
-      // and maplibre complains it has error which some of properties are not defined.
-      Object.keys(style.sources).forEach((key) => {
-        const src = style.sources[key]
-        Object.keys(src).forEach((prop) => {
-          if (!src[prop]) {
-            delete src[prop]
-          }
-        })
-      })
-      map.setStyle(style)
-    }
+  if (map.getSource(layerStyle.source)) {
+    map.getSource(layerStyle.source).tiles = [decodeURI(layerURL.toString())]
+    map.style.sourceCaches[layerStyle.source].clearTiles()
+    map.style.sourceCaches[layerStyle.source].update(map.transform)
+    map.triggerRepaint()
   }
 }
 
