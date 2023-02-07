6250d2eaed0db2e42d782513896d2ce918f57f95
Joseph Thuha <josephthuha@gmail.com>
2023-02-08 00:04:08 +0300

    Style improvements (#1336)
    * minor style improvement of user account popup
    
    * migrate from using popper to bulma dropdown in UserAccount
    
    * minor cleanup

--- sites/geohub/src/components/UserAccount.svelte
+++ sites/geohub/src/components/UserAccount.svelte
@@ -1,98 +1,70 @@
 <script lang="ts">
   import { signIn, signOut } from '@auth/sveltekit/client'
   import { page } from '$app/stores'
-  import { slide } from 'svelte/transition'
   import { clickOutside } from 'svelte-use-click-outside'
-  import Popper from '$lib/popper'
   import { Button } from '@undp-data/svelte-undp-design'
   import chroma from 'chroma-js'
 
-  let isPanelOpen = false
   let panelWidth = '350px'
-
+  let dropdownActive = false
   let innerWidth = 0
-  $: isMobile = innerWidth < 768 ? true : false
+  $: isMobile = innerWidth < 768
 
   const name = $page.data.session?.user.name
   const names = name?.split(' ') ?? []
-
-  const {
-    ref: popperRef,
-    options: popperOptions,
-    content: popperContent,
-  } = new Popper(
-    {
-      placement: 'bottom-end',
-      strategy: 'fixed',
-    },
-    [0, 45],
-  ).init()
-
-  const togglePanel = () => {
-    isPanelOpen = !isPanelOpen
-  }
-
-  const handleKeyDown = (event) => {
-    if (event.key === 'Enter') {
-      togglePanel()
-    }
-  }
 </script>
 
 <svelte:window bind:innerWidth />
-
 {#if $page.data.session}
-  <div
-    role="button"
-    tabindex="0"
-    on:click={togglePanel}
-    on:keydown={handleKeyDown}
-    use:clickOutside={() => (isPanelOpen = false)}
-    use:popperRef>
-    {#if $page.data.session.user?.image}
-      <span
-        style="background-image: url('{$page.data.session.user.image}')"
-        class="avatar" />
-    {:else}
-      <span
-        class="initial-avator is-flex is-justify-content-center	is-align-items-center"
-        style="background-color: {chroma.random()}">
-        {#each names as name}
-          <p
-            class="name"
-            style="color: white">
-            {name.slice(0, 1)}
-          </p>
-        {/each}
-      </span>
-    {/if}
-  </div>
-
-  {#if isPanelOpen}
-    <div
-      id="tooltip"
-      data-testid="tooltip"
-      style="max-width: {panelWidth};"
-      use:popperContent={popperOptions}
-      transition:slide>
-      <div class="panel container p-4">
-        <button
-          class="delete close-button"
-          aria-label="delete"
-          on:click={() => (isPanelOpen = false)} />
-        <p class="title is-4">{$page.data.session.user.name}</p>
-        {#if $page.data.session.user['jobTitle']}
-          <p class="subtitle is-5 mb-2">{$page.data.session.user['jobTitle']}</p>
+  <div class="dropdown is-right {dropdownActive ? 'is-active' : null}">
+    <div class="dropdown-trigger">
+      <div
+        role="button"
+        tabindex="0"
+        on:keydown={() => (dropdownActive = !dropdownActive)}
+        on:click={() => (dropdownActive = !dropdownActive)}>
+        {#if $page.data.session.user?.image}
+          <span
+            style="background-image: url('{$page.data.session.user.image}')"
+            class="avatar" />
+        {:else}
+          <span
+            class="initial-avator is-flex is-justify-content-center	is-align-items-center"
+            style="background-color: {chroma.random()}">
+            {#each names as name}
+              <p
+                class="name"
+                style="color: white">
+                {name.slice(0, 1)}
+              </p>
+            {/each}
+          </span>
         {/if}
-        <p class="subtitle is-6">{$page.data.session.user.email}</p>
-
-        <Button
-          title="Sign Out"
-          isPrimary={false}
-          on:clicked={() => signOut('azure-ad')} />
       </div>
     </div>
-  {/if}
+    <div
+      use:clickOutside={() => (dropdownActive = false)}
+      class="dropdown-menu"
+      style="max-width: {panelWidth}"
+      role="menu">
+      <div class="dropdown-content">
+        <div class="dropdown-item has-text-centered">
+          <p class="title mb-2 is-4">{$page.data.session.user.name}</p>
+          {#if $page.data.session.user['jobTitle']}
+            <p class="has-text-weight-bold">{$page.data.session.user['jobTitle']}</p>
+          {/if}
+          <hr class="dropdown-divider" />
+          <p>{$page.data.session.user.email}</p>
+        </div>
+        <div class="dropdown-item">
+          <Button
+            title="Sign Out"
+            isPrimary={false}
+            on:clicked={() => signOut('azure-ad')} />
+        </div>
+      </div>
+    </div>
+  </div>
 {:else}
   <button
     class="button is-primary {isMobile ? 'is-small' : 'is-normal'}"
@@ -100,26 +72,6 @@
 {/if}
 
 <style lang="scss">
-  @import '../styles/popper.scss';
-
-  .panel {
-    position: relative;
-    cursor: default;
-
-    .close-button {
-      position: absolute;
-      top: 5px;
-      right: 5px;
-    }
-  }
-
-  #tooltip {
-    background-color: white;
-    margin: 0;
-    padding: 0;
-    z-index: 20;
-  }
-
   .avatar {
     border-radius: 2rem;
     float: left;
