4dd1479de2ce3faff8c3c42ab9f3303f20100ae3
Jin Igarashi <igarashi.jin@water-gis.com>
2023-02-10 12:45:40 +0000

    deleted storage table from document and fixed dataset endpoint

--- packages/geohub-cli/docs/geohub-database.sql
+++ packages/geohub-cli/docs/geohub-database.sql
@@ -2,7 +2,6 @@
 CREATE TABLE geohub.dataset
 (
   id         character varying        NOT NULL,
-  storage_id character varying        NOT NULL,
   url        character varying        NOT NULL,
   name       character varying        NOT NULL,
   description     character varying       ,
@@ -18,8 +17,6 @@ CREATE TABLE geohub.dataset
 COMMENT ON TABLE geohub.dataset IS 'this table manages metadata of files';
 
 COMMENT ON COLUMN geohub.dataset.id IS 'md5 hash generated from URL';
-
-COMMENT ON COLUMN geohub.dataset.storage_id IS 'storage ID';
 
 COMMENT ON COLUMN geohub.dataset.url IS 'stores URL for dataset. e.g., URL for azure blob, URL for mosaicjson';
 
@@ -47,40 +44,6 @@ COMMENT ON TABLE geohub.dataset_tag IS 'this table connects file_metadata and ta
 COMMENT ON COLUMN geohub.dataset_tag.dataset_id IS 'unique ID for dataset';
 
 COMMENT ON COLUMN geohub.dataset_tag.tag_id IS 'unique ID for tag name';
-
-CREATE TABLE geohub.storage
-(
-  id          character varying NOT NULL,
-  name        character varying NOT NULL,
-  url         character varying NOT NULL,
-  label       character varying NOT NULL,
-  description character varying,
-  icon        character varying,
-  PRIMARY KEY (id)
-);
-
-COMMENT ON TABLE geohub.storage IS 'manges storage';
-
-COMMENT ON COLUMN geohub.storage.id IS 'md5 hash generated from URL';
-
-COMMENT ON COLUMN geohub.storage.name IS 'name';
-
-COMMENT ON COLUMN geohub.storage.label IS 'label for visualising';
-
-COMMENT ON COLUMN geohub.storage.icon IS 'fontawesome icon name or icon URL';
-
-CREATE TABLE geohub.storage_tag
-(
-  storage_id character varying NOT NULL,
-  tag_id     serial            NOT NULL,
-  CONSTRAINT storage_tag_pkey PRIMARY KEY (storage_id, tag_id)
-);
-
-COMMENT ON TABLE geohub.storage_tag IS 'this table connects between storage and tag';
-
-COMMENT ON COLUMN geohub.storage_tag.storage_id IS 'storage type ID';
-
-COMMENT ON COLUMN geohub.storage_tag.tag_id IS 'unique ID for tag name';
 
 CREATE TABLE geohub.style
 (
@@ -123,21 +86,6 @@ ALTER TABLE geohub.dataset_tag
   ADD CONSTRAINT FK_dataset_TO_dataset_tag
     FOREIGN KEY (dataset_id)
     REFERENCES geohub.dataset (id);
-
-ALTER TABLE geohub.dataset
-  ADD CONSTRAINT FK_storage_TO_dataset
-    FOREIGN KEY (storage_id)
-    REFERENCES geohub.storage (id);
-
-ALTER TABLE geohub.storage_tag
-  ADD CONSTRAINT FK_tag_TO_storage_tag
-    FOREIGN KEY (tag_id)
-    REFERENCES geohub.tag (id);
-
-ALTER TABLE geohub.storage_tag
-  ADD CONSTRAINT FK_storage_TO_storage_tag
-    FOREIGN KEY (storage_id)
-    REFERENCES geohub.storage (id);
 
 -- DROP INDEX IF EXISTS geohub.tag_idx_key_value;
 
