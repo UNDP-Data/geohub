1abaae458e279bb817f1a25e77f903b451dfb898
Jin Igarashi <igarashi.jin@water-gis.com>
2023-03-03 10:49:10 +0000

    feat: added +page.svelte with swagger-ui-dist

---
+++ sites/geohub/src/routes/api/+page.ts
@@ -0,0 +1,11 @@
+import type { PageLoad } from './$types'
+
+export const load: PageLoad = async () => {
+  const title = 'API Docs | GeoHub'
+  const spec = '/api/swagger/spec.json'
+
+  return {
+    title,
+    spec,
+  }
+}
