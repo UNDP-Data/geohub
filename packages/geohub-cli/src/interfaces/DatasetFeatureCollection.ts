68ada614d73e8d1bd586b78bb340d38b958a9a07
Jin Igarashi <igarashi.jin@water-gis.com>
2023-03-06 15:28:41 +0000

    fix: typescript error

--- packages/geohub-cli/src/interfaces/DatasetFeatureCollection.ts
+++ packages/geohub-cli/src/interfaces/DatasetFeatureCollection.ts
@@ -1,10 +1,8 @@
 import type { DatasetFeature } from './DatasetFeature';
-import type { Pages } from './Pages';
 import type { StacLink } from './StacLink';
 
 export interface DatasetFeatureCollection {
 	type: 'FeatureCollection';
 	features: DatasetFeature[];
 	links: StacLink[];
-	pages: Pages;
 }
