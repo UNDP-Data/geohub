d952b41ebdad99907a7d8d269cba8f24b95bd752
Jin Igarashi <igarashi.jin@water-gis.com>
2023-03-09 17:04:42 +0000

    fix: unique value condition

--- sites/geohub/src/components/controls/VectorClassifyLegend.svelte
+++ sites/geohub/src/components/controls/VectorClassifyLegend.svelte
@@ -292,7 +292,7 @@
 
             const propertySelectValues = []
             const values = stat.values
-            if (stat.type !== 'number' && values && values.length > 0 && values.length <= UniqueValueThreshold) {
+            if (values && values.length > 0 && values.length <= UniqueValueThreshold) {
               hasUniqueValues = true
               applyToOption = VectorApplyToTypes.COLOR
 
