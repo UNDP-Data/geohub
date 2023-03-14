2eaa607f8efa44d2fd9558fab21a95d64fc38ece
Jin Igarashi <jin.igarashi@undp.org>
2023-03-14 17:44:13 +0000

    doc: add admin api to swagger (#1548)

--- sites/geohub/src/routes/api/continents/+server.ts
+++ sites/geohub/src/routes/api/continents/+server.ts
@@ -5,14 +5,21 @@ import DatabaseManager from '$lib/server/DatabaseManager'
  * Continent API
  * return continent data
  */
-export const GET: RequestHandler = async () => {
+export const GET: RequestHandler = async ({ url }) => {
   const dbm = new DatabaseManager()
   const client = await dbm.start()
   try {
+    let isTagFilter = false
+    const filterByTag = url.searchParams.get('filterbytag')
+    if (filterByTag && filterByTag === 'true') {
+      isTagFilter = true
+    }
+
     const sql = {
       text: `
       SELECT region1_code as continent_code, region1_name as continent_name
       FROM geohub.country
+      ${isTagFilter ? `WHERE EXISTS (select id FROM geohub.tag WHERE key='continent' and value=region1_name)` : ''}
       GROUP BY region1_code, region1_name
       ORDER BY region1_name`,
     }
