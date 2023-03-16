96ab69eae1f3d52d580875cdf8234cafbc5bb3a5
Joseph Thuha <josephthuha@gmail.com>
2023-03-16 20:23:57 +0300

    fix: z-index for settings header fix (#1573)

--- sites/geohub/src/routes/settings/+page.svelte
+++ sites/geohub/src/routes/settings/+page.svelte
@@ -7,19 +7,35 @@
   let links: HeaderLink[] = HeaderItems(['home', 'maps', 'data', 'dashboard', 'userguide'])
 </script>
 
-<Header
-  region="UNDP's one stop shop for spatial data and analytics"
-  siteTitle="GeoHub"
-  url="https://geohub.data.undp.org"
-  logoUrl="assets/undp-images/undp-logo-blue.svg"
-  bind:height={headerHeight}
-  bind:links
-  isPositionFixed={true}>
-  <div slot="custom-button">
-    <UserAccount />
-  </div>
-</Header>
-<Settings bind:headerHeight />
+<div class="settings-page-header">
+  <Header
+    region="UNDP's one stop shop for spatial data and analytics"
+    siteTitle="GeoHub"
+    url="https://geohub.data.undp.org"
+    logoUrl="assets/undp-images/undp-logo-blue.svg"
+    bind:height={headerHeight}
+    bind:links
+    isPositionFixed={true}>
+    <div slot="custom-button">
+      <UserAccount />
+    </div>
+  </Header>
+</div>
+
+<Settings
+  class="settings-page"
+  bind:headerHeight />
 <Footer
   logoUrl="assets/undp-images/undp-logo-white.svg"
   footerItems={FooterItems} />
+
+<style lang="scss">
+  // Similar styles to the header as you can't set the z-index of the header at this place
+  .settings-page-header {
+    position: fixed;
+    top: 0;
+    left: 0;
+    right: 0;
+    z-index: 1000;
+  }
+</style>
