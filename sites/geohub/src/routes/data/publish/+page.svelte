203290667d2023349fa537b4101af236d26d3128
Jin Igarashi <igarashi.jin@water-gis.com>
2023-03-16 23:01:28 +0000

    fix: enable multiple select continents and regions

--- sites/geohub/src/routes/data/publish/+page.svelte
+++ sites/geohub/src/routes/data/publish/+page.svelte
@@ -32,63 +32,80 @@
   let tags = ''
   let isRegistering = false
 
-  let selectedContinent: Continent
+  let selectedContinents: Continent[] = []
   let continentsMaster: Continent[] = []
-  let selectedRegion: Region
+  let selectedRegions: Region[] = []
   let regionsMaster: Region[] = []
 
   const init = () => {
     data.promises.continents.then((cts) => {
       continentsMaster = cts
-      let t = feature.properties.tags.find((t) => t.key === 'continent')
-      if (t) {
-        selectedContinent = cts.find((c) => c.continent_name === t.value)
-        continentSelected(selectedContinent)
-      }
+      let filters = feature.properties.tags.filter((t) => t.key === 'continent')
+      filters?.forEach((f) => {
+        let continent = cts.find((c) => c.continent_name === f.value)
+        continentSelected(continent, true)
+      })
     })
 
     data.promises.regions.then((rs) => {
       regionsMaster = rs
-      let t = feature.properties.tags.find((t) => t.key === 'region')
-      if (t) {
-        selectedRegion = rs.find((c) => c.region_name === t.value)
-        regionSelected(selectedRegion)
-      }
+      let filters = feature.properties.tags.filter((t) => t.key === 'region')
+      filters?.forEach((f) => {
+        let region = rs.find((c) => c.region_name === f.value)
+        regionSelected(region)
+      })
     })
   }
 
   init()
 
-  // $: selectedContinent, continentSelected()
-  const continentSelected = (c: Continent) => {
-    if (c !== selectedContinent) {
-      selectedContinent = c
-      selectedRegion = undefined
-    }
-    if (!selectedContinent) {
-      continents = []
+  const continentSelected = (c: Continent, isInit = false) => {
+    if (selectedContinents.includes(c)) {
+      const rs = selectedRegions.filter((r) => r.continent_code === c.continent_code)
+      for (const r of rs) {
+        regionSelected(r)
+      }
+      selectedContinents.splice(selectedContinents.indexOf(c), 1)
     } else {
-      continents = [
-        {
+      selectedContinents.push(c)
+      if (!isInit) {
+        regionsMaster
+          .filter((r) => r.continent_code === c.continent_code)
+          ?.forEach((r) => {
+            regionSelected(r)
+          })
+      }
+    }
+    selectedContinents = [...selectedContinents]
+    if (selectedContinents.length === 0) {
+      continents = []
+      selectedRegions = []
+    } else {
+      continents = selectedContinents.map((c) => {
+        return {
           key: 'continent',
-          value: selectedContinent.continent_name,
-        },
-      ]
+          value: c.continent_name,
+        }
+      })
     }
   }
 
-  // $: selectedRegion, regionSelected()
   const regionSelected = (r: Region) => {
-    selectedRegion = r
-    if (!selectedRegion) {
+    if (selectedRegions.includes(r)) {
+      selectedRegions.splice(selectedRegions.indexOf(r), 1)
+    } else {
+      selectedRegions.push(r)
+    }
+    selectedRegions = [...selectedRegions]
+    if (selectedRegions.length === 0) {
       regions = []
     } else {
-      regions = [
-        {
+      regions = selectedRegions.map((r) => {
+        return {
           key: 'region',
-          value: selectedRegion.region_name,
-        },
-      ]
+          value: r.region_name,
+        }
+      })
     }
   }
 
@@ -106,7 +123,7 @@
     'table',
   ]
 
-  const initTags = (key: 'provider' | 'sdg_goal' | 'continents' | 'regions' | 'country' | 'other') => {
+  const initTags = (key: 'provider' | 'sdg_goal' | 'continent' | 'region' | 'country' | 'other') => {
     const _tags: Tag[] = feature?.properties?.tags
     if (key === 'other') {
       const keys = ['provider', 'sdg_goal', 'country', 'region', 'continent', 'extent', ...excludedTagForEditing]
@@ -119,8 +136,8 @@
 
   let providers: Tag[] = initTags('provider')
   let sdgs: Tag[] = initTags('sdg_goal')
-  let continents: Tag[] = []
-  let regions: Tag[] = []
+  let continents: Tag[] = initTags('continent')
+  let regions: Tag[] = initTags('region')
   let countries: Tag[] = initTags('country')
   let otherTags: Tag[] = initTags('other')
 
@@ -193,8 +210,8 @@
   const handleGlobalRegionalChanged = (type: 'global' | 'regional') => {
     isGlobal = type
     if (isGlobal === 'global') {
-      selectedContinent = undefined
-      selectedRegion = undefined
+      selectedContinents = []
+      selectedRegions = []
       continents = []
       regions = []
       countries = []
@@ -207,33 +224,31 @@
       countries = []
       return
     }
-    if (_countries.length === 1) {
-      const firstCountry = _countries[0]
-      if (selectedContinent?.continent_code !== firstCountry.continent_code) {
-        selectedContinent = continentsMaster.find((c) => c.continent_code === firstCountry.continent_code)
+
+    _countries.forEach((c) => {
+      const ct = continentsMaster.find((a) => a.continent_code === c.continent_code)
+      const re = regionsMaster.find((a) => a.region_code === c.region_code)
+      if (!selectedContinents.includes(ct)) {
+        selectedContinents.push(ct)
       }
-      setTimeout(() => {
-        if (selectedRegion?.region_code !== firstCountry.region_code) {
-          selectedRegion = regionsMaster.find((c) => c.region_code === firstCountry.region_code)
-        }
-      }, 100)
-    }
+      if (!selectedRegions.includes(re)) {
+        selectedRegions.push(re)
+      }
+    })
+    selectedContinents = [...selectedContinents]
+    selectedRegions = [...selectedRegions]
 
-    if (!selectedContinent) {
-      _countries.forEach((c) => {
-        if (continents.find((x) => x.value === c.continent_name)) return
-        continents.push({ key: 'continent', value: c.continent_name })
-      })
-      continents = [...continents]
-    }
+    selectedContinents?.forEach((c) => {
+      if (continents.find((x) => x.value === c.continent_name)) return
+      continents.push({ key: 'continent', value: c.continent_name })
+    })
+    continents = [...continents]
 
-    if (!selectedRegion) {
-      _countries.forEach((c) => {
-        if (regions.find((x) => x.value === c.region_name)) return
-        regions.push({ key: 'region', value: c.region_name })
-      })
-      regions = [...regions]
-    }
+    selectedRegions?.forEach((c) => {
+      if (regions.find((x) => x.value === c.region_name)) return
+      regions.push({ key: 'region', value: c.region_name })
+    })
+    regions = [...regions]
 
     const temp: Tag[] = []
     _countries.forEach((c) => {
@@ -442,20 +457,14 @@
         {#await data.promises.continents}
           <Loader size="x-small" />
         {:then continents}
-          <div class="field has-addons segmentbuttons">
-            <p class="control">
-              <button
-                type="button"
-                class="button {selectedContinent === undefined ? 'is-primary is-active' : 'is-primary is-light'}"
-                on:click={() => continentSelected(undefined)}>
-                <span>All</span>
-              </button>
-            </p>
+          <div class="field has-addons is-flex is-flex-wrap-wrap">
             {#each continents as continent}
-              <p class="control">
+              <p class="control pt-1">
                 <button
                   type="button"
-                  class="button {selectedContinent === continent ? 'is-primary is-active' : 'is-primary is-light'}"
+                  class="button {selectedContinents.includes(continent)
+                    ? 'is-primary is-active'
+                    : 'is-primary is-light'}"
                   on:click={() => continentSelected(continent)}>
                   <span class="icon is-small">
                     <i
@@ -472,7 +481,7 @@
       </div>
     </div>
 
-    {#if isGlobal === 'regional' && selectedContinent?.continent_name !== 'Antarctica'}
+    {#if isGlobal === 'regional' && selectedContinents.length > 0}
       <div class="field">
         <!-- svelte-ignore a11y-label-has-associated-control -->
         <label class="label">Please select a region for your data.</label>
@@ -480,21 +489,13 @@
           {#await data.promises.regions}
             <Loader size="x-small" />
           {:then regions}
-            <div class="field has-addons segmentbuttons">
-              <p class="control">
-                <button
-                  type="button"
-                  class="button {selectedRegion?.region_code ? 'is-primary is-light' : 'is-primary is-active'}"
-                  on:click={() => regionSelected(undefined)}>
-                  <span>All</span>
-                </button>
-              </p>
+            <div class="field has-addons is-flex is-flex-wrap-wrap">
               {#each regions as region}
-                {#if selectedContinent?.continent_code === region.continent_code}
-                  <p class="control">
+                {#if selectedContinents.filter((c) => c.continent_code === region.continent_code).length > 0}
+                  <p class="control pt-1">
                     <button
                       type="button"
-                      class="button {selectedRegion === region ? 'is-primary is-active' : 'is-primary is-light'}"
+                      class="button {selectedRegions.includes(region) ? 'is-primary is-active' : 'is-primary is-light'}"
                       on:click={() => regionSelected(region)}>
                       <span>{region.region_name}</span>
                     </button>
@@ -513,8 +514,8 @@
         <CountryPicker
           on:change={handleCountrySelected}
           bind:tags={countries}
-          bind:selectedContinent
-          bind:selectedRegion />
+          bind:selectedContinents
+          bind:selectedRegions />
       </div>
     </div>
   {/if}
@@ -563,10 +564,5 @@
   .description {
     resize: none;
     height: 100px;
-  }
-
-  .segmentbuttons {
-    max-width: 100vw;
-    overflow-x: auto;
   }
 </style>
