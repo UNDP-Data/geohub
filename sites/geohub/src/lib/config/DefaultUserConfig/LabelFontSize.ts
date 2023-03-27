b4980bf1635f3378feda33569cfe4d47acf8dbe6
Thuhaa <josephthuha@gmail.com>
2023-03-28 02:10:19 +0300

    feat: add more settings extension

---
+++ sites/geohub/src/lib/config/DefaultUserConfig/LabelFontSize.ts
@@ -0,0 +1,1 @@
+export const LabelFontSize = 16
--- sites/geohub/src/lib/config/DefaultUserConfig/index.ts
+++ sites/geohub/src/lib/config/DefaultUserConfig/index.ts
@@ -8,6 +8,16 @@ import { NumberOfClasses } from './NumberOfClasses'
 import { SearchLimit } from './SearchLimit'
 import { TagSearchOperator } from './TagSearchOperator'
 import { SidebarPosition } from './SidebarPosition'
+import type { ClassificationMethodTypes } from '$lib/config/AppConfig'
+import { ClassificationMethod } from '$lib/config/DefaultUserConfig/ClassificationMethod'
+import { LabelFontSize } from '$lib/config/DefaultUserConfig/LabelFontSize'
+import { LabelHaloWidth } from '$lib/config/DefaultUserConfig/LabelHaloWidth'
+import { IconOverlapPriority } from '$lib/config/DefaultUserConfig/IconOverlapPriority'
+import { RasterResamplingMethod } from '$lib/config/DefaultUserConfig/RasterResamplingMethod'
+import { IconSize } from '$lib/config/DefaultUserConfig/IconSize'
+import { IconSymbol } from '$lib/config/DefaultUserConfig/IconSymbol'
+import { LayerOpacity } from '$lib/config/DefaultUserConfig/LayerOpacity'
+import { LinePattern } from '$lib/config/DefaultUserConfig/LinePattern'
 
 export interface UserConfig {
   SearchLimit: number
@@ -20,6 +30,15 @@ export interface UserConfig {
   LineWidth: number
   NumberOfClasses: number
   SidebarPosition: 'left' | 'right'
+  ClassificationMethod: ClassificationMethodTypes
+  RasterResamplingMethod: 'nearest' | 'linear'
+  LayerOpacity: number
+  IconSymbol: string
+  IconSize: number
+  IconOverlapPriority: 'always' | 'never' | 'cooperative'
+  LinePattern: 'solid' | 'dash' | 'dot' | 'dashdot'
+  LabelFontSize: number
+  LabelHaloWidth: number
 }
 
 export const DefaultUserConfig = {
@@ -33,4 +52,13 @@ export const DefaultUserConfig = {
   MapPageSortingColumn,
   LineWidth,
   NumberOfClasses,
+  ClassificationMethod,
+  RasterResamplingMethod,
+  LabelFontSize,
+  LabelHaloWidth,
+  IconOverlapPriority,
+  IconSize,
+  IconSymbol,
+  LayerOpacity,
+  LinePattern,
 }
