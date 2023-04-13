01d061af57371e5b7e0a2fbfa42cd026975a8616
Thuhaa <josephthuha@gmail.com>
2023-04-13 20:38:04 +0300

    add decodeURIComponent function to remove special characters for readability

--- sites/geohub/src/lib/helper/clean.ts
+++ sites/geohub/src/lib/helper/clean.ts
@@ -4,8 +4,11 @@
  */
 export const clean = (val: string) => {
   // apply start/title case
-  return val
-    .replace(/[_-]/g, ' ') // remove underscore and hyphen
-    .replace(/\.[^/.]+$/, '') // remove extension
-    .replace(/\b\w/g, (str) => str.toUpperCase())
+  return decodeURIComponent(
+    val // decodeURIComponent to handle special characters like %20, %2F, etc.
+      .replace(/[_-]/g, ' ') // remove underscore and hyphen
+      .replace(/\.[^/.]+$/, '') // remove extension
+      .replace(/\b\w/g, (str) => str.toUpperCase()),
+    // apply start/title case
+  )
 }
