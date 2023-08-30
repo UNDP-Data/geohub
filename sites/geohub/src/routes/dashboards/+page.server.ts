70bf11b3e7dc9873352c5a16cca7c5f900b90f60
Jin Igarashi <igarashi.jin@water-gis.com>
2023-08-30 14:08:00 +0100

    refactor: moved redirect logic to hooks.server.ts

--- sites/geohub/src/routes/dashboards/+page.server.ts
+++
@@ -1,7 +0,0 @@
-import type { PageServerLoad } from './$types';
-import { redirect } from '@sveltejs/kit';
-
-export const load: PageServerLoad = async () => {
-	// redirect to route page
-	throw redirect(300, '/');
-};
