dbe84406157f2c84b7987792938921ff43dab0cf
Jin Igarashi <igarashi.jin@water-gis.com>
2023-08-30 13:45:32 +0100

    refactor: moved session and config loading to map's +layout.server.ts. Also, moved azureUrl to map and dashboard's server.ts

--- sites/geohub/src/routes/dashboards/electricity/+page.server.ts
+++ sites/geohub/src/routes/dashboards/electricity/+page.server.ts
@@ -3,6 +3,7 @@ import { env } from '$env/dynamic/private';
 
 export const load: PageServerLoad = async () => {
 	return {
+		azureUrl: `https://${env.AZURE_STORAGE_ACCOUNT}.blob.core.windows.net`,
 		titilerUrl: env.TITILER_ENDPOINT
 	};
 };
