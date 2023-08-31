6c4e85c6ab9a4472fbbae8f9bbb1fffeff4b5010
Jin Igarashi <igarashi.jin@water-gis.com>
2023-08-31 11:12:47 +0100

    fix: setupTest

--- sites/geohub/setupTest.ts
+++ sites/geohub/setupTest.ts
@@ -1,22 +1,22 @@
-import matchers from '@testing-library/jest-dom/matchers'
-import { expect, vi } from 'vitest'
-expect.extend(matchers)
+import * as matchers from '@testing-library/jest-dom/matchers';
+import { expect, vi } from 'vitest';
+expect.extend(matchers);
 
 // create mock of Map object for test
 // please add maplibre's properties which are used in test
 vi.mock('maplibre-gl', () => {
-  return {
-    Map: vi.fn(() => ({
-      addControl: vi.fn(),
-      on: vi.fn(),
-      remove: vi.fn(),
-      getStyle: vi.fn(),
-      getPaintProperty: vi.fn(),
-      setPaintProperty: vi.fn(),
-      getLayoutProperty: vi.fn(),
-      setLayoutProperty: vi.fn(),
-    })),
-    GeolocateControl: vi.fn(),
-    NavigationControl: vi.fn(),
-  }
-})
+	return {
+		Map: vi.fn(() => ({
+			addControl: vi.fn(),
+			on: vi.fn(),
+			remove: vi.fn(),
+			getStyle: vi.fn(),
+			getPaintProperty: vi.fn(),
+			setPaintProperty: vi.fn(),
+			getLayoutProperty: vi.fn(),
+			setLayoutProperty: vi.fn()
+		})),
+		GeolocateControl: vi.fn(),
+		NavigationControl: vi.fn()
+	};
+});
