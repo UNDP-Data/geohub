5096a0189d23d7634cbb2142ee0aebe8f6b66e8e
Jin Igarashi <igarashi.jin@water-gis.com>
2023-08-31 12:19:24 +0100

    feat: updated style.json version to v1.1.0 to use Proxima Nova font

--- sites/geohub/src/lib/config/AppConfig/MapStyles.ts
+++ sites/geohub/src/lib/config/AppConfig/MapStyles.ts
@@ -3,10 +3,10 @@ import type { StyleDefinition } from '@undp-data/style-switcher';
 export const MapStyles: StyleDefinition[] = [
 	{
 		title: 'Carto',
-		uri: 'https://unpkg.com/@undp-data/style@1.0.3/dist/style.json'
+		uri: 'https://unpkg.com/@undp-data/style@1.1.0/dist/style.json'
 	},
 	{
 		title: 'Bing Aerial',
-		uri: 'https://unpkg.com/@undp-data/style@1.0.3/dist/aerialstyle.json'
+		uri: 'https://unpkg.com/@undp-data/style@1.1.0/dist/aerialstyle.json'
 	}
 ];
