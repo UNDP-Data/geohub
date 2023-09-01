1f18c0cee1087d52d9dca01d21198e5b7065ed19
Jin Igarashi <igarashi.jin@water-gis.com>
2023-09-01 20:28:00 +0100

    commit

--- sites/geohub/vite.config.ts
+++ sites/geohub/vite.config.ts
@@ -1,33 +1,49 @@
-import { sveltekit } from '@sveltejs/kit/vite'
-import { defineConfig } from 'vitest/config'
-import { resolve } from 'path'
+import { sveltekit } from '@sveltejs/kit/vite';
+import { defineConfig } from 'vitest/config';
+import { resolve } from 'path';
 
 export default defineConfig({
-  plugins: [sveltekit()],
-  ssr: {
-    noExternal: [/^@material(?:-extra)?\//, 'vega-embed', 'svelte-carousel', 'simply-reactive'],
-  },
-  resolve: {
-    alias: {
-      $components: resolve('./src/components'),
-      $stores: resolve('./src/stores/index.ts'),
-    },
-  },
-  server: {
-    fs: {
-      // Allow serving files from one level up to the project root
-      allow: ['../..'],
-    },
-  },
-  test: {
-    include: ['src/**/*.{test,spec}.{js,ts}'],
-    threads: false,
-    globals: true,
-    environment: 'jsdom',
-    setupFiles: ['./setupTest.ts'],
-    coverage: {
-      provider: 'istanbul',
-      reporter: ['text', 'json', 'html'],
-    },
-  },
-})
+	plugins: [sveltekit()],
+	ssr: {
+		noExternal: [
+			/^@material(?:-extra)?\//,
+			'vega-embed',
+			'svelte-carousel',
+			'simply-reactive',
+			'@maplibre/maplibre-gl-native',
+			'sharp'
+		]
+	},
+	build: {
+		// for maplibre-native
+		commonjsOptions: {
+			// dynamicRequireTargets: ['@maplibre/maplibre-gl-native']
+			// ignoreDynamicRequires: true,
+			// transformMixedEsModules: true
+			ignore: ['@maplibre/maplibre-gl-native']
+		}
+	},
+	resolve: {
+		alias: {
+			$components: resolve('./src/components'),
+			$stores: resolve('./src/stores/index.ts')
+		}
+	},
+	server: {
+		fs: {
+			// Allow serving files from one level up to the project root
+			allow: ['../..']
+		}
+	},
+	test: {
+		include: ['src/**/*.{test,spec}.{js,ts}'],
+		threads: false,
+		globals: true,
+		environment: 'jsdom',
+		setupFiles: ['./setupTest.ts'],
+		coverage: {
+			provider: 'istanbul',
+			reporter: ['text', 'json', 'html']
+		}
+	}
+});
