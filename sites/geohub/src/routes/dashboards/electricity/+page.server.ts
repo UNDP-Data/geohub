80f23aae3625bc9dc9037733db3341f152e1b437
Jin Igarashi <jin.igarashi@undp.org>
2023-09-11 18:11:51 +0100

    refactor: moved map route to (map), and enabled SSR for electricity dashboard (#1859)

--- sites/geohub/src/routes/dashboards/electricity/+page.server.ts
+++ sites/geohub/src/routes/dashboards/electricity/+page.server.ts
@@ -1,8 +1,10 @@
 import type { PageServerLoad } from './$types';
 import { env } from '$env/dynamic/private';
 
-export const load: PageServerLoad = async () => {
+export const load: PageServerLoad = async ({ locals }) => {
+	const session = await locals.getSession();
 	return {
+		session,
 		azureUrl: `https://${env.AZURE_STORAGE_ACCOUNT}.blob.core.windows.net`,
 		titilerUrl: env.TITILER_ENDPOINT
 	};
--- sites/geohub/src/routes/dashboards/electricity/+page.svelte
+++ sites/geohub/src/routes/dashboards/electricity/+page.svelte
@@ -1,17 +1,59 @@
 <script lang="ts">
 	import { page } from '$app/stores';
-	import { MapStyles, SiteInfo } from '$lib/config/AppConfig';
-	import maplibregl from 'maplibre-gl';
+	import { AdminControlOptions, MapStyles, SiteInfo } from '$lib/config/AppConfig';
+	import MaplibreCgazAdminControl from '@undp-data/cgaz-admin-tool';
+	import '@undp-data/cgaz-admin-tool/dist/maplibre-cgaz-admin-control.css';
+	import StyleSwicher from '@undp-data/style-switcher';
+	import { MenuControl } from '@watergis/svelte-maplibre-menu';
+	import maplibregl, {
+		AttributionControl,
+		GeolocateControl,
+		Map,
+		NavigationControl,
+		ScaleControl
+	} from 'maplibre-gl';
+	import 'maplibre-gl/dist/maplibre-gl.css';
 	import * as pmtiles from 'pmtiles';
 	import { onMount } from 'svelte';
-	import Content from './components/Content.svelte';
-	import Map from './components/Map.svelte';
+	import type { PageData } from './$types';
+	import Charts from './components/Charts.svelte';
+	import DownloadData from './components/DownloadData.svelte';
+	import ElectricityControl from './components/ElectricityControl.svelte';
+	import IntroductionPanel from './components/IntroductionPanel.svelte';
+	import OverlayControl from './components/OverlayControl.svelte';
 	import { ELECTRICITY_DATASETS } from './constansts';
 	import type { Dataset } from './interfaces';
-	import { hrea, ml } from './stores';
+	import { hrea, map as mapStore, ml } from './stores';
+	import { loadAdmin, setAzureUrl } from './utils/adminLayer';
 
 	let protocol = new pmtiles.Protocol();
 	maplibregl.addProtocol('pmtiles', protocol.tile);
+
+	export let data: PageData;
+
+	const azureUrl = data.azureUrl;
+	setAzureUrl(azureUrl);
+
+	let styles = MapStyles;
+
+	let title = 'Electricity Dashboard | GeoHub';
+	let content = 'Electricity dashboard';
+
+	let mapContainer: HTMLDivElement;
+	let map: Map;
+
+	let showIntro = true;
+	let electricitySelected: {
+		name: string;
+		icon: string;
+		title: string;
+	};
+	let drawerWidth = 355;
+	let isResizingDrawer = false;
+
+	let loadRasterLayer = () => {
+		return;
+	};
 
 	onMount(() => {
 		const promises = loadDatasets();
@@ -20,6 +62,47 @@
 		});
 		promises.ml.then((datasets) => {
 			ml.update(() => datasets);
+		});
+
+		map = new Map({
+			container: mapContainer,
+			style: styles[0].uri,
+			center: [0, 0],
+			zoom: 2.5,
+			hash: true,
+			attributionControl: false
+		});
+
+		map.addControl(new NavigationControl({}), 'top-right');
+		map.addControl(
+			new GeolocateControl({
+				positionOptions: { enableHighAccuracy: true },
+				trackUserLocation: true
+			}),
+			'top-right'
+		);
+		map.addControl(new ScaleControl({ maxWidth: 80, unit: 'metric' }), 'bottom-left');
+		map.addControl(new AttributionControl({ compact: true }), 'bottom-right');
+		map.getCanvas().style.cursor = 'pointer';
+
+		map.on('load', () => {
+			map.resize();
+
+			const adminOptions = AdminControlOptions;
+			adminOptions.isHover = true;
+			map.addControl(new MaplibreCgazAdminControl(AdminControlOptions), 'top-left');
+		});
+
+		mapStore.update(() => map);
+
+		document.addEventListener('mousemove', (e) => handleMousemove(e));
+		document.addEventListener('mouseup', handleMouseup);
+		mapStore.subscribe(() => {
+			if ($mapStore) {
+				$mapStore.on('load', () => {
+					loadLayers();
+				});
+			}
 		});
 	});
 
@@ -68,13 +151,17 @@
 	};
 
 	let loadLayers = () => {
-		return;
+		loadRasterLayer();
+		loadAdmin(true);
 	};
 
-	let styles = MapStyles;
+	const handleMousemove = (e: MouseEvent | TouchEvent) => {
+		if (!isResizingDrawer) return;
 
-	let title = 'Electricity Dashboard | GeoHub';
-	let content = 'Electricity dashboard';
+		if (e instanceof MouseEvent) drawerWidth = e.clientX;
+		if (e instanceof TouchEvent) drawerWidth = e.touches?.[0].pageX;
+	};
+	const handleMouseup = () => (isResizingDrawer = false);
 </script>
 
 <svelte:head>
@@ -112,12 +199,83 @@
 	</style>
 </svelte:head>
 
-<Content bind:loadLayers>
-	<Map on:styleChanged={loadLayers} bind:styles />
-</Content>
+<MenuControl
+	bind:map={$mapStore}
+	position={'top-left'}
+	isMenuShown={true}
+	minSidebarWidth={`${drawerWidth}px`}
+	initialSidebarWidth={drawerWidth}
+>
+	<div slot="sidebar" class="drawer-content container m-0 px-4 pt-4">
+		<p class="title is-4 m-0 p-0 pb-2 has-text-centered">UNDP Electricity Dashboard</p>
+		<IntroductionPanel bind:showIntro />
 
-<style lang="scss">
+		{#if !showIntro}
+			<div class="box mx-0 my-1">
+				<p class="title is-5 p-0 m-0 has-text-centered pb-2">Raw Data - Electricity Access</p>
+				<ElectricityControl bind:electricitySelected bind:loadRasterLayer />
+			</div>
+			<div class="box mx-0 my-1">
+				<p class="title is-5 p-0 m-0 has-text-centered pb-2">Overlays</p>
+				<OverlayControl />
+			</div>
+			<div class="box mx-0 my-1">
+				<p class="title is-5 p-0 m-0 has-text-centered pb-2">Statistics - Electricity Access</p>
+				<Charts />
+			</div>
+			<div class="box mx-0 my-1">
+				<p class="title is-5 p-0 m-0 has-text-centered pb-2">Statistics - Download</p>
+				<DownloadData />
+			</div>
+		{/if}
+		<div />
+	</div>
+	<div slot="map" class="main-content">
+		<div class="map" id="map" bind:this={mapContainer} />
+		<StyleSwicher bind:map={$mapStore} {styles} position="bottom-left" />
+	</div>
+</MenuControl>
+
+<style global lang="scss">
 	@import '@undp-data/undp-bulma/bulma.scss';
 	@import 'https://use.fontawesome.com/releases/v6.1.1/css/all.css';
 	@import '@creativebulma/bulma-tooltip/dist/bulma-tooltip.min.css';
+
+	p {
+		padding: 10px;
+		border-radius: 5px;
+	}
+
+	.main-content {
+		overflow: hidden;
+		display: flex;
+		height: 100%;
+		flex-grow: 1;
+		z-index: -1;
+		flex-direction: row;
+		flex-wrap: wrap;
+
+		.map {
+			position: absolute;
+			top: 0;
+			bottom: 0;
+			width: 100%;
+			height: 100%;
+			z-index: 1;
+		}
+
+		:global(.maplibregl-ctrl-bottom-right) {
+			padding-left: 80px;
+		}
+	}
+
+	.drawer-content {
+		width: 100%;
+		height: 100%;
+		overflow: auto;
+		display: flex;
+		flex-direction: column;
+		flex-basis: 100%;
+		flex: 1;
+	}
 </style>
