8eb31e075594333d9f8187fb7dd8a4a7a3b845e4
Jin Igarashi <jin.igarashi@undp.org>
2023-09-14 19:20:19 +0100

    feat: added SDG filtering in data page (#1875)

--- sites/geohub/src/components/data-upload/PublishedDatasets.svelte
+++ sites/geohub/src/components/data-upload/PublishedDatasets.svelte
@@ -7,12 +7,14 @@
 	import { DatasetSortingColumns, LimitOptions } from '$lib/config/AppConfig';
 	import type { UserConfig } from '$lib/config/DefaultUserConfig';
 	import { handleEnterKey } from '$lib/helper';
-	import type { DatasetFeature, DatasetFeatureCollection } from '$lib/types';
+	import type { DatasetFeature, DatasetFeatureCollection, Tag } from '$lib/types';
 	import { Loader, Pagination, Radios } from '@undp-data/svelte-undp-design';
 	import { debounce } from 'lodash-es';
 	import { createEventDispatcher } from 'svelte';
 	import PublishedDatasetHeader from './PublishedDatasetHeader.svelte';
 	import PublishedDatasetRow from './PublishedDatasetRow.svelte';
+	import SdgCard from './SdgCard.svelte';
+	import SdgPicker from './SdgPicker.svelte';
 	const dispatch = createEventDispatcher();
 
 	export let datasets: Promise<DatasetFeatureCollection>;
@@ -58,6 +60,21 @@
 
 	let showMyData = $page.url.searchParams.get('mydata') === 'true' ? true : false;
 	let showFavourite = $page.url.searchParams.get('staronly') === 'true' ? true : false;
+
+	const getSDGsFromUrl = () => {
+		const key = 'sdg_goal';
+		const sdgs = $page.url.searchParams.getAll(key);
+		const tags: Tag[] = [];
+		sdgs?.forEach((value) => {
+			tags.push({
+				key,
+				value
+			});
+		});
+		return tags;
+	};
+
+	let selectedSDGs: Tag[] = getSDGsFromUrl();
 
 	$: isQueryEmpty = !query || query?.length === 0;
 
@@ -192,9 +209,32 @@
 
 		await reload(href);
 	};
+
+	const updateSDGtags = async () => {
+		const apiUrl = $page.url;
+		apiUrl.searchParams.delete('sdg_goal');
+		selectedSDGs?.forEach((t) => {
+			apiUrl.searchParams.append(t.key, t.value);
+		});
+
+		await reload(apiUrl);
+	};
+
+	const handleSDGtagChanged = async (e) => {
+		selectedSDGs = e.detail.tags;
+		selectedSDGs = [...selectedSDGs];
+		await updateSDGtags();
+	};
+
+	const handleSDGDeleted = async (e) => {
+		const sdg = e.detail.sdg;
+		const filtered = selectedSDGs.filter((t) => t.value !== `${sdg}`);
+		selectedSDGs = [...filtered];
+		await updateSDGtags();
+	};
 </script>
 
-<div class="datasets-header tile is-ancestor">
+<div class="datasets-header mb-4">
 	<div class="columns">
 		{#if $page.data.session}
 			<div class="column px-0 py-1">
@@ -225,7 +265,15 @@
 			</div>
 		{/if}
 		<div class="column px-0 py-1 mr-4">
-			<div class="is-flex is-justify-content-end">
+			<div class="is-flex is-justify-content-end is-align-items-center">
+				<div class="field px-1 m-0">
+					<SdgPicker
+						bind:tags={selectedSDGs}
+						size="small"
+						showSelectionOnButton={false}
+						on:change={handleSDGtagChanged}
+					/>
+				</div>
 				<div class="control has-icons-left filter-text-box pl-1">
 					<input
 						data-testid="filter-bucket-input"
@@ -251,7 +299,7 @@
 					{/if}
 				</div>
 
-				<div class="field tag-filter">
+				<div class="field tag-filter m-0">
 					<PanelButton
 						icon="fas fa-sliders"
 						tooltip="Explore tags and filter data"
@@ -264,7 +312,7 @@
 					</PanelButton>
 				</div>
 
-				<div class="field sort-control">
+				<div class="field sort-control m-0">
 					<PanelButton icon="fas fa-arrow-down-short-wide" tooltip="Sort" width="200px">
 						<p class="title is-5 m-0 p-0 pb-2">Sort settings</p>
 
@@ -292,6 +340,30 @@
 	</div>
 </div>
 
+{#if selectedSDGs.length > 0}
+	<div class="field">
+		<!-- svelte-ignore a11y-label-has-associated-control -->
+		<label class="label">Filtered by SDG{selectedSDGs.length > 1 ? '(s)' : ''}</label>
+		<div class="control">
+			<div class="is-flex">
+				{#key selectedSDGs}
+					{#each selectedSDGs as tag}
+						<div class="m-1">
+							<SdgCard
+								sdg={Number(tag.value)}
+								isSelectable={false}
+								showDelete={true}
+								size="small"
+								on:deleted={handleSDGDeleted}
+							/>
+						</div>
+					{/each}
+				{/key}
+			</div>
+		</div>
+	</div>
+{/if}
+
 {#await datasets}
 	<div class="align-center my-4">
 		<Loader />
@@ -302,6 +374,11 @@
 			<Loader />
 		</div>
 	{:else if featureCollection.pages?.totalCount > 0}
+		<div class="m-4">
+			<Notification type="info" showCloseButton={true}>
+				{featureCollection.pages?.totalCount} datasets found
+			</Notification>
+		</div>
 		<PublishedDatasetHeader />
 		{#each featureCollection.features as feature}
 			<PublishedDatasetRow bind:feature on:deleted={handleDeleted} />
