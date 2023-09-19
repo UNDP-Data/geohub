563c8f3054b34239e7a94b0bd818ff88ca8d054e
Jin Igarashi <jin.igarashi@undp.org>
2023-09-19 17:27:18 +0100

    feat: add exclude param in style.json api to remove basemap's sources and layers (#1891)

--- sites/geohub/src/routes/api/style/[id].json/+server.ts
+++ sites/geohub/src/routes/api/style/[id].json/+server.ts
@@ -1,5 +1,7 @@
 import type { RequestHandler } from './$types';
 import { getStyleById } from '$lib/server/helpers';
+import type { DashboardMapStyle } from '$lib/types';
+import type { LayerSpecification, SourceSpecification } from 'maplibre-gl';
 
 /**
  * Get style.json which is stored in PostgreSQL database
@@ -15,12 +17,35 @@ export const GET: RequestHandler = async ({ params, url, locals }) => {
 				status: 400
 			});
 		}
-		const style = await getStyleById(styleId, url, session?.user?.email);
+		const style = (await getStyleById(styleId, url, session?.user?.email)) as DashboardMapStyle;
 
 		if (!style) {
 			return new Response(undefined, {
 				status: 404
 			});
+		}
+
+		// if 'exclude' is True, remove basemap's sources and layers from the output style.json
+		const excludeString = url.searchParams.get('exclude');
+		const exclude = excludeString === 'true' ? true : false;
+		if (exclude) {
+			const layerIds = style.layers.map((l) => l.id);
+
+			const layers: LayerSpecification[] = [];
+			for (const layer of style.style.layers) {
+				if (layerIds.includes(layer.id)) {
+					layers.push(layer);
+				}
+			}
+			style.style.layers = layers;
+			const sourceIds = layers.map((l) => l['source']);
+			const sources: { [key: string]: SourceSpecification } = {};
+			Object.keys(style.style.sources).forEach((key) => {
+				if (sourceIds.includes(key)) {
+					sources[key] = style.style.sources[key];
+				}
+			});
+			style.style.sources = sources;
 		}
 
 		return new Response(JSON.stringify(style.style));
