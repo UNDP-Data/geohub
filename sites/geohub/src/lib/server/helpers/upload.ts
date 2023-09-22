9cbb80cdb0536483d8426a9f4dd789112a0d2989
Jin Igarashi <igarashi.jin@water-gis.com>
2023-09-22 17:06:32 +0100

    moving code of generating user hash key to hooks.server.ts

--- sites/geohub/src/lib/server/helpers/upload.ts
+++ sites/geohub/src/lib/server/helpers/upload.ts
@@ -1,5 +1,3 @@
-import { generateHashKey } from '.';
-
 export const UPLOAD_CONTAINER_NAME = 'userdata';
 export const UPLOAD_RAW_FOLDER_NAME = 'raw';
 export const UPLOAD_DATASETS_FOLDER_NAME = 'datasets';
@@ -8,8 +6,7 @@ export const UPLOAD_BASE_URL = (account: string) => {
 	return `https://${account}.blob.core.windows.net`;
 };
 
-export const UPLOAD_BLOB_URL = (account: string, user_email: string, fileName: string) => {
-	const userHash = generateHashKey(user_email);
+export const UPLOAD_BLOB_URL = (account: string, userHash: string, fileName: string) => {
 	const folder = `${userHash}/${UPLOAD_RAW_FOLDER_NAME}`;
 	return `${UPLOAD_BASE_URL(account)}/${UPLOAD_CONTAINER_NAME}/${folder}/${fileName}`;
 };
