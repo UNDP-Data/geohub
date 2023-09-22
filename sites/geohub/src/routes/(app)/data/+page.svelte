7020c5f14fe468459ec0ca345de8b1ea1d7108d2
Jin Igarashi <igarashi.jin@water-gis.com>
2023-09-22 12:12:53 +0100

    removed comments

--- sites/geohub/src/routes/(app)/data/+page.svelte
+++ sites/geohub/src/routes/(app)/data/+page.svelte
@@ -36,9 +36,6 @@
 		updateDatasets();
 	};
 
-	// let title = 'Data | GeoHub';
-	// let content = 'Data Portal';
-
 	enum TabNames {
 		DATA = 'Data',
 		MYDATA = 'My data'
@@ -216,23 +213,6 @@
 		scrollTo('manual-search');
 	};
 </script>
-
-<!-- <svelte:head>
-	<title>{title}</title>
-	<meta property="og:site_name" content={SiteInfo.site_name} />
-	<meta property="og:type" content="article" />
-	<meta name="description" content={SiteInfo.site_description} />
-	<meta property="og:description" content={SiteInfo.site_description} />
-	<meta name="twitter:description" content={SiteInfo.site_description} />
-	<meta property="og:title" content={title} />
-	<meta property="og:image" content="{$page.url.origin}/api/og?content={content}" />
-	<meta property="og:image:width" content="1200" />
-	<meta property="og:image:height" content="630" />
-	<meta name="twitter:card" content="summary_large_image" />
-	<meta name="twitter:title" content={title} />
-	<meta name="twitter:image" content="{$page.url.origin}/api/og?content={content}" />
-	<meta property="og:url" content="{$page.url.origin}{$page.url.pathname}" />
-</svelte:head> -->
 
 {#if data.session}
 	<div class="tabs is-fullwidth is-medium data-tabs">
