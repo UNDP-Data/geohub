b6bb465f45bdea083e8979915403e7c74cbffd62
Jin Igarashi <igarashi.jin@water-gis.com>
2023-09-29 13:59:50 +0100

    feat: add root page for management tol

--- sites/geohub/src/routes/(app)/management/+page.svelte
+++ sites/geohub/src/routes/(app)/management/+page.svelte
@@ -1,5 +1,9 @@
+<script>
+	import { CtaLink } from '@undp-data/svelte-undp-design';
+</script>
+
 <div class="p-4">
 	<h1 class="title">GeoHub Management tools</h1>
 
-	<a href="/management/pgtileserv">pg_tileserv management tool</a>
+	<CtaLink href="/management/pgtileserv" label="pg_tileserv management tool" isArrow={true} />
 </div>
