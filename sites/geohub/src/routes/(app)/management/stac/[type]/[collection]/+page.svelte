f7648da9fa8385e799ccba1be24b2acd80813366
Jin Igarashi <igarashi.jin@water-gis.com>
2023-10-10 08:49:05 +0100

    adjust thumbnail position

--- sites/geohub/src/routes/(app)/management/stac/[type]/[collection]/+page.svelte
+++ sites/geohub/src/routes/(app)/management/stac/[type]/[collection]/+page.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
 	import StacAssetExplorer from '$components/stac/StacAssetExplorer.svelte';
 	import type { StacCollection } from '$lib/types';
+	import { marked } from 'marked';
 	import type { PageData } from './$types';
 
 	export let data: PageData;
@@ -13,46 +14,31 @@
 <section class=" p-4">
 	<h1 class="title is-1">{collection.title}</h1>
 
-	{#if thumbnail}
-		<div class="columns">
+	<div class="columns">
+		{#if thumbnail}
 			<div class="column is-6">
 				<img src={thumbnail.href} alt={thumbnail.title} />
 			</div>
-			<div class="column is-6">
-				<div class="field">
-					<!-- svelte-ignore a11y-label-has-associated-control -->
-					<label class="label">ID</label>
-					<div class="control">
-						<p>{collection.id}</p>
-					</div>
-				</div>
-
-				<div class="field">
-					<!-- svelte-ignore a11y-label-has-associated-control -->
-					<label class="label">Description</label>
-					<div class="control">
-						<p>{collection.description}</p>
-					</div>
+		{/if}
+		<div class="column is-6">
+			<div class="field">
+				<!-- svelte-ignore a11y-label-has-associated-control -->
+				<label class="label">ID</label>
+				<div class="control">
+					<p>{collection.id}</p>
 				</div>
 			</div>
-		</div>
-	{:else}
-		<div class="field">
-			<!-- svelte-ignore a11y-label-has-associated-control -->
-			<label class="label">ID</label>
-			<div class="control">
-				<p>{collection.id}</p>
-			</div>
-		</div>
 
-		<div class="field">
-			<!-- svelte-ignore a11y-label-has-associated-control -->
-			<label class="label">Description</label>
-			<div class="control">
-				<p>{collection.description}</p>
+			<div class="field">
+				<!-- svelte-ignore a11y-label-has-associated-control -->
+				<label class="label">Description</label>
+				<div class="control">
+					<!-- eslint-disable svelte/no-at-html-tags -->
+					<p>{@html marked(collection.description)}</p>
+				</div>
 			</div>
 		</div>
-	{/if}
+	</div>
 
 	<div class="field">
 		<!-- svelte-ignore a11y-label-has-associated-control -->
