133d9ced631a04aeed2f553a97f2a05e9fce6fcf
Jin Igarashi <igarashi.jin@water-gis.com>
2023-10-11 14:42:16 +0100

    fix: use OG api from static image api

--- sites/geohub/src/routes/+layout.server.ts
+++ sites/geohub/src/routes/+layout.server.ts
@@ -1,8 +1,17 @@
 import type { LayoutServerLoad } from './$types';
+import { env } from '$env/dynamic/private';
 
-export const load: LayoutServerLoad = async ({ locals }) => {
+export const load: LayoutServerLoad = async ({ locals, url }) => {
 	const session = await locals.getSession();
+
+	let geohubApi = url.origin;
+	if (geohubApi.indexOf('localhost') > -1) {
+		geohubApi = env.GEOHUB_API_ENDPOINT;
+	}
+
 	return {
-		session
+		session,
+		geohubApi,
+		staticImageApi: env.GEOHUB_STATIC_IMAGE_API
 	};
 };
--- sites/geohub/src/routes/+layout.svelte
+++ sites/geohub/src/routes/+layout.svelte
@@ -3,6 +3,8 @@
 	import { page } from '$app/stores';
 	import { SiteInfo } from '$lib/config/AppConfig';
 
+	const styleId = 209;
+
 	let title = $page.data.title ?? 'GeoHub';
 	let content = $page.data.content ?? 'GeoHub';
 	let site_name = $page.data.site_name ?? SiteInfo.site_name;
@@ -24,12 +26,20 @@
 	<meta property="og:description" content={site_description} />
 	<meta name="twitter:description" content={site_description} />
 	<meta property="og:title" content={title} />
-	<meta property="og:image" content="{$page.url.origin}/api/og?content={content}" />
+	<meta
+		property="og:image"
+		content="{$page.data.staticImageApi}/og?content={content}&url={$page.data
+			.geohubApi}/api/style/{styleId}.json"
+	/>
 	<meta property="og:image:width" content="1200" />
 	<meta property="og:image:height" content="630" />
 	<meta name="twitter:card" content="summary_large_image" />
 	<meta name="twitter:title" content={title} />
-	<meta name="twitter:image" content="{$page.url.origin}/api/og?content={content}" />
+	<meta
+		name="twitter:image"
+		content="{$page.data.staticImageApi}/og?content={content}&url={$page.data
+			.geohubApi}/api/style/{styleId}.json"
+	/>
 	<meta property="og:url" content="{$page.url.origin}{$page.url.pathname}" />
 
 	<style type="text/css">
