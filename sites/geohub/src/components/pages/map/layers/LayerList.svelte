5d4d6cb914af401c2271851d565a6e90492f6798
Jin Igarashi <igarashi.jin@water-gis.com>
2023-10-13 16:29:27 +0100

    fix: adjusted plus position of layer add button and stac explorer button

--- sites/geohub/src/components/pages/map/layers/LayerList.svelte
+++ sites/geohub/src/components/pages/map/layers/LayerList.svelte
@@ -1,15 +1,16 @@
 <script lang="ts">
 	import { page } from '$app/stores';
+	import LayerOrderPanelButton from '$components/pages/map/layers/order/LayerOrderPanelButton.svelte';
 	import RasterLayer from '$components/pages/map/layers/raster/RasterLayer.svelte';
 	import VectorLayer from '$components/pages/map/layers/vector/VectorLayer.svelte';
+	import Modal from '$components/util/Modal.svelte';
+	import Notification from '$components/util/Notification.svelte';
+	import Star from '$components/util/Star.svelte';
 	import { TabNames } from '$lib/config/AppConfig';
 	import { getLayerStyle } from '$lib/helper';
 	import type { DashboardMapStyle } from '$lib/types';
-	import { layerList, MAPSTORE_CONTEXT_KEY, type MapStore } from '$stores';
+	import { MAPSTORE_CONTEXT_KEY, layerList, type MapStore } from '$stores';
 	import { getContext } from 'svelte';
-	import LayerOrderPanelButton from '$components/pages/map/layers/order/LayerOrderPanelButton.svelte';
-	import Notification from '$components/util/Notification.svelte';
-	import Star from '$components/util/Star.svelte';
 
 	const map: MapStore = getContext(MAPSTORE_CONTEXT_KEY);
 
@@ -20,6 +21,38 @@
 	let layerHeaderHeight = 39;
 
 	$: totalHeight = contentHeight - layerHeaderHeight;
+
+	let isDeleteDialogVisible = false;
+	const openDeleteDialog = () => {
+		isDeleteDialogVisible = true;
+	};
+	const handleDeleteAll = () => {
+		for (const layer of $layerList) {
+			const delSourceId = getLayerStyle($map, layer.id).source;
+			if (layer.children && layer.children.length > 0) {
+				layer.children.forEach((child) => {
+					if ($map.getLayer(child.id)) {
+						$map.removeLayer(child.id);
+					}
+				});
+				layer.children = [];
+
+				if ($map.getLayer(layer.id)) {
+					$map.removeLayer(layer.id);
+				}
+				const layerListforDelSource = $layerList.filter(
+					(item) => getLayerStyle($map, item.id).source === delSourceId
+				);
+				if (layerListforDelSource.length === 0) {
+					$map.removeSource(delSourceId);
+				}
+			}
+		}
+	};
+
+	const handleCancel = () => {
+		isDeleteDialogVisible = false;
+	};
 </script>
 
 {#if $layerList?.length > 0}
@@ -30,7 +63,19 @@
 		{#if style}
 			<Star bind:id={style.id} bind:isStar={style.is_star} table="style" />
 		{/if}
-		<div class="layer-order">
+		<div class="layer-header-buttons">
+			<button
+				class="button has-tooltip-arrow has-tooltip-bottom"
+				disabled={$layerList?.length === 0}
+				data-tooltip="Delete all layers"
+				on:click={openDeleteDialog}
+			>
+				<span class="delete-all-icon icon fa-stack fa-xl" role="button" tabindex="0">
+					<i class="fa-solid fa-list fa-stack-lg" />
+					<i class="fa-solid fa-xmark fa-sm fa-stack-1x" />
+				</span>
+			</button>
+
 			<LayerOrderPanelButton />
 		</div>
 	</div>
@@ -53,12 +98,45 @@
 	{/each}
 </div>
 
+<Modal
+	bind:dialogOpen={isDeleteDialogVisible}
+	on:cancel={handleCancel}
+	on:continue={handleDeleteAll}
+	target={$layerList.map((l) => l.name).join(', ')}
+	title="Delete all layers"
+	message="Are you sure you want to delete the all following layers?"
+	cancelText="Cancel"
+	continueText="Delete all"
+/>
+
 <style lang="scss">
 	.layer-header {
 		width: 100%;
 
-		.layer-order {
+		.layer-header-buttons {
 			margin-left: auto;
+			display: grid;
+			grid-template-columns: repeat(2, 1fr);
+			gap: 5px;
+			width: fit-content;
+
+			.delete-all-icon {
+				width: 30px;
+				height: 30px;
+				position: relative;
+
+				.fa-stack-lg {
+					position: absolute;
+					top: 0px;
+					left: 0px;
+				}
+
+				.fa-stack-1x {
+					width: auto;
+					top: 1px;
+					right: -23px;
+				}
+			}
 		}
 	}
 
