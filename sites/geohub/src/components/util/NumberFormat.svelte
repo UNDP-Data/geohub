efb7eb2cf734507902d0e5b6f1580c7a77606804
Jin Igarashi <igarashi.jin@water-gis.com>
2023-10-17 09:55:57 +0100

    fix: bug of restoring decimal position

--- sites/geohub/src/components/util/NumberFormat.svelte
+++ sites/geohub/src/components/util/NumberFormat.svelte
@@ -1,11 +1,25 @@
 <script lang="ts">
 	import NumberInput from '$components/util/NumberInput.svelte';
+	import { createEventDispatcher } from 'svelte';
+	const dispatch = createEventDispatcher();
 
 	export let decimalPosition = 0;
 
 	let maxValue = 10;
 	let minValue = 1;
 	let stepValue = 1;
+
+	const handleChanged = () => {
+		dispatch('change', {
+			decimalPosition
+		});
+	};
 </script>
 
-<NumberInput bind:value={decimalPosition} bind:minValue bind:maxValue bind:step={stepValue} />
+<NumberInput
+	bind:value={decimalPosition}
+	bind:minValue
+	bind:maxValue
+	bind:step={stepValue}
+	on:change={handleChanged}
+/>
