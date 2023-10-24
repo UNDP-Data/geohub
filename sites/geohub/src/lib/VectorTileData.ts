605479146766d80f435fa648badb7428407ce865
Jin Igarashi <igarashi.jin@water-gis.com>
2023-10-24 15:13:23 +0100

    refactor: deleted commented code

--- sites/geohub/src/lib/VectorTileData.ts
+++ sites/geohub/src/lib/VectorTileData.ts
@@ -59,29 +59,6 @@ export class VectorTileData {
 		this.metadata = metadata;
 	};
 
-	// private getDefaultColor = (
-	// 	map: Map,
-	// 	layerId: string,
-	// 	property: 'icon-color' | 'fill-color' | 'fill-outline-color' | 'line-color',
-	// 	defaultColor: string
-	// ): string => {
-	// 	if (!map.getLayer(layerId)) return;
-	// 	let color = map.getPaintProperty(layerId, property);
-	// 	// eslint-disable-next-line @typescript-eslint/ban-ts-comment
-	// 	// @ts-ignore
-	// 	if (
-	// 		!color ||
-	// 		(color && (color.type === 'interval' || (color && color.type === 'categorical')))
-	// 	) {
-	// 		if (property === 'fill-outline-color') {
-	// 			color = chroma(defaultColor).darken(2.5).hex();
-	// 		} else {
-	// 			color = chroma.random().hex();
-	// 		}
-	// 	}
-	// 	return color as string;
-	// };
-
 	public add = async (
 		map?: Map,
 		layerType?: 'point' | 'heatmap' | 'polygon' | 'linestring',
