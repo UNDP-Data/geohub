d2773ca7988df697d94c98c88e8af9d89dc9fb4c
Jin Igarashi <igarashi.jin@water-gis.com>
2023-11-17 14:26:55 +0000

    chore: add CI to bump geohub data pipeline version

---
+++ backends/k8s/data-pipeline/bump-version.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+yaml_file=./yaml/ingest-environment.yaml
+url=https://api.github.com/repos/undp-data/geohub-data-pipeline/releases/latest
+
+# extract the latest tag version
+tag_name=$(curl -s $url | jq --raw-output '.tag_name')
+
+echo "Latest release version: $tag_name"
+
+imagename="undpgeohub.azurecr.io/geohub-data-pipeline"
+pattern="${imagename}:[^ ]*"
+sed -E "s|$pattern|$imagename:$tag_name|g" $yaml_file > "${yaml_file}_new"
+
+cat "${yaml_file}_new"
+
+# replace yaml file with new version
+mv "${yaml_file}_new" $yaml_file
+
+echo "tag version was replace to $tag_name"
