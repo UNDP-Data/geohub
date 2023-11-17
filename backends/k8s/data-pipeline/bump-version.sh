0323e2b662b8c3b47a2ef8265a9b2b07d0a6848c
Jin Igarashi <igarashi.jin@water-gis.com>
2023-11-17 15:41:10 +0000

    fix: Use github action to fetch release version

--- backends/k8s/data-pipeline/bump-version.sh
+++ backends/k8s/data-pipeline/bump-version.sh
@@ -1,18 +1,14 @@
 #!/bin/bash
 
 yaml_file=./yaml/ingest-environment.yaml
-url=https://api.github.com/repos/undp-data/geohub-data-pipeline/releases/latest
 
-# extract the latest tag version
-tag_name=$(curl -s $url | jq --raw-output '.tag_name')
-
-echo "Latest release version: $tag_name"
+echo "Latest release version: $PIPELINE_VERSION"
 
 imagename="undpgeohub.azurecr.io/geohub-data-pipeline"
 pattern="${imagename}:[^ ]*"
-sed "s|$pattern|$imagename:$tag_name|g" $yaml_file > "${yaml_file}_new"
+sed "s|$pattern|$imagename:$PIPELINE_VERSION|g" $yaml_file > "${yaml_file}_new"
 
 # replace yaml file with new version
 mv "${yaml_file}_new" $yaml_file
 
-echo "tag version was replace to $tag_name"
+echo "tag version was replace to $PIPELINE_VERSION"
