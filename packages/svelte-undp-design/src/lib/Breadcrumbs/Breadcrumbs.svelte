5d0c86a0c5bd9c96375252349e0d465384937045
Thuhaa <josephthuha@gmail.com>
2023-11-17 21:54:59 +0300

    fix: minor inprovements of breadcrumbs styles

--- packages/svelte-undp-design/src/lib/Breadcrumbs/Breadcrumbs.svelte
+++ packages/svelte-undp-design/src/lib/Breadcrumbs/Breadcrumbs.svelte
@@ -32,7 +32,11 @@
 						<span class="icon-text">
 							<span class="icon">
 								{#if breadcrumb.icon.startsWith('fa')}
-									<i class={breadcrumb.icon} />
+									<i class="{breadcrumb.icon} {fontSize === 'small'
+										? 'fa-sm'
+										: fontSize === 'medium'
+											? 'fa-1x'
+											: 'fa-2x'}" />
 								{:else if breadcrumb.icon.startsWith('fi')}
 									<!--Class for flag-icon CSS https://www.npmjs.com/package/flag-icons -->
 									<span class={breadcrumb.icon} />
@@ -51,18 +55,22 @@
 					</li>
 				{:else}
 					<li>
-						<div
-							class={disabled ? 'isDisabled' : ''}
+						<!-- svelte-ignore a11y-missing-attribute -->
+						<a class={disabled ? 'isDisabled' : ''}
 							aria-label={breadcrumb.name}
-							role="button"
 							tabindex="0"
+							role="button"
 							on:click={() => handleClicked(index)}
 							on:keydown={handleKeyDown}
 						>
 							<span class="icon-text">
 								<span class="icon">
 									{#if breadcrumb.icon.startsWith('fa')}
-										<i class={breadcrumb.icon} />
+										<i class="{breadcrumb.icon} {fontSize === 'small'
+										? 'fa-sm'
+										: fontSize === 'medium'
+											? 'fa-1x'
+											: 'fa-2x'}" />
 									{:else}
 										<img src={breadcrumb.icon} alt="{breadcrumb.name}_image" />
 									{/if}
@@ -75,7 +83,7 @@
 										: '1.5'}rem">{breadcrumb.name}</span
 								>
 							</span>
-						</div>
+						</a>
 					</li>
 				{/if}
 			{/each}
