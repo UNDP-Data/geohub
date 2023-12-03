2d0773454dd406b521a71dc5dac32cb20adebfe3
Jin Igarashi <jin.igarashi@undp.org>
2023-12-03 16:54:02 +0000

    Update getBase64EncodedUrl.test.ts

--- sites/geohub/src/lib/helper/getBase64EncodedUrl.test.ts
+++ sites/geohub/src/lib/helper/getBase64EncodedUrl.test.ts
@@ -5,12 +5,9 @@ describe('getBase64EncodedUrl', () => {
 	// there is very weird error of 'The string to be encoded contains invalid characters.' only in CI. I tried to fix it, but I could not manage it.
 	// the below test works well in my local PC and I cannot reproduce the error in locally,
 	// it('should return a base64 encoded url', () => {
-	// 	const url =
-	// 		'https://undpgeohub.blob.core.windows.net/userdata/a85516c81c0b78d3e89d3f00099b8b15/datasets/Dem_Rwanda_10m_allt_20230921150153.tif/Dem_Rwanda_10m_allt_20230921150153_band1.tif?sv=2023-11-03&ss=b&srt=o&se=2024-12-02T16%3A01%3A58Z&sp=r&sig=yKCoZklpzFdSn7mQnQ0%2BevKUkIukVVeq7Oy6XkNToBk%3D';
+	// 	const url = 'https://exampleurl.com?first=1&second=2';
 	// 	const encodedUrl = getBase64EncodedUrl(url);
-	// 	expect(encodedUrl).toBe(
-	// 		'https://undpgeohub.blob.core.windows.net/userdata/a85516c81c0b78d3e89d3f00099b8b15/datasets/Dem_Rwanda_10m_allt_20230921150153.tif/Dem_Rwanda_10m_allt_20230921150153_band1.tif?c3Y9MjAyMy0xMS0wMyZzcz1iJnNydD1vJnNlPTIwMjQtMTItMDJUMTYlM0EwMSUzQTU4WiZzcD1yJnNpZz15S0NvWmtscHpGZFNuN21RblEwJTJCZXZLVWtJdWtWVmVxN095NlhrTlRvQmslM0Q='
-	// 	);
+	// 	expect(encodedUrl).toBe('https://exampleurl.com?Zmlyc3Q9MSZzZWNvbmQ9Mg==');
 	// });
 
 	it('should return same URL if no query params', () => {
