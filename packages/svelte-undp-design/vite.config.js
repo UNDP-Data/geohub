043fd1cb8138ff5e9a06608650bd6c2b31d794f2
Jin Igarashi <igarashi.jin@water-gis.com>
2023-12-03 09:44:37 +0000

    fix: vite.config.js error

--- packages/svelte-undp-design/vite.config.js
+++ packages/svelte-undp-design/vite.config.js
@@ -3,16 +3,8 @@ import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
 	plugins: [sveltekit()],
-	optimizeDeps: {
-		include: ['lodash.get', 'lodash.isequal', 'lodash.clonedeep']
-	},
 	test: {
 		include: ['src/**/*.{test,spec}.{js,ts}'],
-		deps: {
-			optimizer: {
-				include: ['@sveltejs/kit']
-			}
-		},
 		globals: true,
 		environment: 'jsdom',
 		coverage: {
