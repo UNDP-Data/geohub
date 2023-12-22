d33f97fc49ca2bbfa856f51c9c127508d5ec85e1
Thuhaa <josephthuha@gmail.com>
2023-12-22 20:11:43 +0300

    refactor: start fix of products mosaicjson

--- sites/geohub/src/routes/api/stac/[id]/[collection]/[...item]/products/[product_id]/+server.ts
+++ sites/geohub/src/routes/api/stac/[id]/[collection]/[...item]/products/[product_id]/+server.ts
@@ -1,13 +1,16 @@
 import type { RequestHandler } from '@sveltejs/kit';
 import type { StacTemplate } from '$lib/stac/StacTemplate';
 import { getStacInstance } from '$lib/stac/getStacInstance';
-import { createDatasetLinks, getSTACs } from '$lib/server/helpers';
+import {
+	createDatasetLinks,
+	createMosaicDataSetFeature,
+	createTitilerMosaicJsonEndpoint,
+	getSTACs
+} from '$lib/server/helpers';
 import { error } from '@sveltejs/kit';
-// import type { DatasetFeature } from '$lib/types';
-// import { AccessLevel, attribution, StacProducts } from '$lib/config/AppConfig';
-// import { env } from '$env/dynamic/private';
-// import { BlobServiceClient, StorageSharedKeyCredential } from '@azure/storage-blob';
-// import { generateHashKey } from '$lib/helper';
+import type { DatasetFeature } from '$lib/types';
+import { StacProducts } from '$lib/config/AppConfig';
+import { env } from '$env/dynamic/private';
 
 export const GET: RequestHandler = async ({ params, url }) => {
 	const product = params.product_id;
@@ -26,24 +29,40 @@ export const GET: RequestHandler = async ({ params, url }) => {
 		const productFeature = await getProductFeature(stacInstance, product, item, url);
 		return new Response(JSON.stringify(productFeature));
 	} else {
-		// const features: DatasetFeature[] = [];
-		// const sortedItems = items.sort();
+		const features: DatasetFeature[] = [];
+		const sortedItems = items.sort();
 		//
-		// for (const item of sortedItems) {
-		// 	const productFeature = await getProductFeature(stacInstance, product_id, item, url);
-		// 	features.push(productFeature);
-		// }
-		// const items = StacProducts.find((p) => p.id === product_id)?.items;
+		for (const item of sortedItems) {
+			const productFeature = await getProductFeature(stacInstance, product, item, url);
+			features.push(productFeature);
+		}
+		const productAssets: string[] = StacProducts.find(
+			(p) => p.collection_id === collection
+		).products.find((p) => p.name.toLowerCase() === product).assets;
+		let assetUrls = [];
+		for (const feature of features) {
+			const itemUrl = feature.properties.url;
+			const itemJson = await fetch(itemUrl).then((r) => r.json());
+			const assets = itemJson.assets;
+			for (const asset of productAssets) {
+				const url = assets[asset].href;
+				if (url) {
+					assetUrls = [...assetUrls, url];
+				}
+			}
+		}
+		console.log(assetUrls);
+
 		// const urls: string[] = features.map((f) => f.properties.url);
-		// const name = `${type}/${collection}/${sortedItems.join('/')}/mosaicjson.json`;
-		// const mosaicjson = await createTitilerMosaicJsonEndpoint(urls, name);
-		// const mosaicjsonFeature = await createMosaicProductFeature(features, mosaicjson);
-		// mosaicjsonFeature.properties = createDatasetLinks(
-		// 	mosaicjsonFeature,
-		// 	url.origin,
-		// 	'https://titiler.xyz/stac'
-		// );
-		// return new Response(JSON.stringify(mosaicjsonFeature));
+		const name = `${type}/${collection}/${sortedItems.join('/')}/mosaicjson.json`;
+		const mosaicjson = await createTitilerMosaicJsonEndpoint(assetUrls, name);
+		const mosaicjsonFeature = await createMosaicDataSetFeature(features, mosaicjson);
+		mosaicjsonFeature.properties = createDatasetLinks(
+			mosaicjsonFeature,
+			url.origin,
+			env.TITILER_ENDPOINT
+		);
+		return new Response(JSON.stringify(mosaicjsonFeature));
 	}
 };
 
@@ -56,10 +75,6 @@ const getProductFeature = async (
 	const stacItem = await instance.getStacItem(item);
 	await instance.getStacCollection();
 	const productFeature = await instance.generateDataSetFeature(stacItem, undefined, product);
-	productFeature.properties = createDatasetLinks(
-		productFeature,
-		url.origin,
-		'https://titiler.xyz/stac'
-	);
+	productFeature.properties = createDatasetLinks(productFeature, url.origin, env.TITILER_ENDPOINT);
 	return productFeature;
 };
