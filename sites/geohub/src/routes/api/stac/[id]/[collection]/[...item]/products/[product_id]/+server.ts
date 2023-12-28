72ea46161147fb198d7563c0f4aa9e2da36f0f41
Thuhaa <josephthuha@gmail.com>
2023-12-28 19:09:45 +0300

    refactor: cleanup

--- sites/geohub/src/routes/api/stac/[id]/[collection]/[...item]/products/[product_id]/+server.ts
+++ sites/geohub/src/routes/api/stac/[id]/[collection]/[...item]/products/[product_id]/+server.ts
@@ -1,15 +1,8 @@
 import type { RequestHandler } from '@sveltejs/kit';
 import type { StacTemplate } from '$lib/stac/StacTemplate';
 import { getStacInstance } from '$lib/stac/getStacInstance';
-import {
-	createDatasetLinks,
-	createMosaicDataSetFeature,
-	createTitilerMosaicJsonEndpoint,
-	getSTACs
-} from '$lib/server/helpers';
+import { createDatasetLinks, getSTACs } from '$lib/server/helpers';
 import { error } from '@sveltejs/kit';
-import type { DatasetFeature } from '$lib/types';
-import { StacProducts } from '$lib/config/AppConfig';
 import { env } from '$env/dynamic/private';
 
 export const GET: RequestHandler = async ({ params, url }) => {
@@ -23,46 +16,14 @@ export const GET: RequestHandler = async ({ params, url }) => {
 	}
 	const collection = params.collection;
 	const items = params.item.split('/');
+
 	const stacInstance = getStacInstance(stac, collection);
 	if (items.length === 1) {
 		const item = items[0];
 		const productFeature = await getProductFeature(stacInstance, product, item, url);
 		return new Response(JSON.stringify(productFeature));
 	} else {
-		const features: DatasetFeature[] = [];
-		const sortedItems = items.sort();
-		//
-		for (const item of sortedItems) {
-			const productFeature = await getProductFeature(stacInstance, product, item, url);
-			features.push(productFeature);
-		}
-		const productAssets: string[] = StacProducts.find(
-			(p) => p.collection_id === collection
-		).products.find((p) => p.name.toLowerCase() === product).assets;
-		let assetUrls = [];
-		for (const feature of features) {
-			const itemUrl = feature.properties.url;
-			const itemJson = await fetch(itemUrl).then((r) => r.json());
-			const assets = itemJson.assets;
-			for (const asset of productAssets) {
-				const url = assets[asset].href;
-				if (url) {
-					assetUrls = [...assetUrls, url];
-				}
-			}
-		}
-		console.log(assetUrls);
-
-		// const urls: string[] = features.map((f) => f.properties.url);
-		const name = `${type}/${collection}/${sortedItems.join('/')}/mosaicjson.json`;
-		const mosaicjson = await createTitilerMosaicJsonEndpoint(assetUrls, name);
-		const mosaicjsonFeature = await createMosaicDataSetFeature(features, mosaicjson);
-		mosaicjsonFeature.properties = createDatasetLinks(
-			mosaicjsonFeature,
-			url.origin,
-			env.TITILER_ENDPOINT
-		);
-		return new Response(JSON.stringify(mosaicjsonFeature));
+		return new Response(JSON.stringify({}));
 	}
 };
 
@@ -75,6 +36,10 @@ const getProductFeature = async (
 	const stacItem = await instance.getStacItem(item);
 	await instance.getStacCollection();
 	const productFeature = await instance.generateDataSetFeature(stacItem, undefined, product);
-	productFeature.properties = createDatasetLinks(productFeature, url.origin, env.TITILER_ENDPOINT);
+	productFeature.properties = createDatasetLinks(
+		productFeature,
+		url.origin,
+		env.TITILER_ENDPOINT.replace('cog', 'stac')
+	);
 	return productFeature;
 };
