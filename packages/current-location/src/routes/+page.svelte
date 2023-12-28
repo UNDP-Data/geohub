c21100ef35c14bf440d0d7de13d25c925dfcc256
Jin Igarashi <igarashi.jin@water-gis.com>
2023-12-28 16:54:34 +0000

    merge Map.svelte to +page.svelte

--- packages/current-location/src/routes/+page.svelte
+++ packages/current-location/src/routes/+page.svelte
@@ -1,9 +1,39 @@
 <script lang="ts">
-	import Map from '$example/Map.svelte';
+	import CurrentLocation from '$lib';
+	import { Map, NavigationControl, ScaleControl } from 'maplibre-gl';
+	import { onMount } from 'svelte';
+
+	let mapContainer: HTMLDivElement;
+	let map: Map;
+
+	onMount(() => {
+		map = new Map({
+			container: mapContainer,
+			style: 'https://unpkg.com/@undp-data/style@1.0.1/dist/style.json',
+			hash: true
+		});
+
+		map.addControl(new NavigationControl({}), 'top-right');
+		map.addControl(new ScaleControl({}), 'bottom-left');
+	});
 </script>
 
 <sveltekit:head>
 	<title>UNDP Current location example</title>
 </sveltekit:head>
 
-<Map />
+<div class="map" bind:this={mapContainer} />
+<CurrentLocation bind:map isHover={true} position="top-left" />
+
+<style>
+	@import 'maplibre-gl/dist/maplibre-gl.css';
+
+	.map {
+		position: absolute;
+		top: 0;
+		bottom: 0;
+		width: 100%;
+		height: 100%;
+		z-index: 1;
+	}
+</style>
