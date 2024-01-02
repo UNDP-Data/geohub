c629967f49d65d53d8c6194dac8758b13a23671c
Jin Igarashi <igarashi.jin@water-gis.com>
2024-01-02 16:38:07 +0000

    fix: fixed toggle icon size and bring animation back

--- sites/geohub/src/components/util/Accordion.svelte
+++ sites/geohub/src/components/util/Accordion.svelte
@@ -17,35 +17,31 @@
 	};
 </script>
 
-<article class="is-flex is-flex-direction-column border">
-	<div class="header is-flex py-4">
-		<div
-			class="is-flex is-align-items-center toggle"
-			role="button"
-			tabindex="0"
-			on:keydown={handleEnterKey}
-			on:click={() => {
-				isExpanded = !isExpanded;
-			}}
-		>
-			<div class="mr-3">
-				<i class="fa-solid fa-chevron-{isExpanded ? 'up' : 'down'} has-text-primary"></i>
-			</div>
+<div class="header is-flex py-4 {isExpanded ? '' : 'border'}">
+	<span
+		class="accordion-title is-size-6 has-text-grey-dark mr-3"
+		use:tippyTooltip={{ content: title }}
+		role="button"
+		tabindex="0"
+		on:keydown={handleEnterKey}
+		on:click={() => {
+			isExpanded = !isExpanded;
+		}}
+	>
+		<span class="mr-2">
+			<i
+				class="fa-solid fa-chevron-down toggle-icon {isExpanded ? 'active' : ''} has-text-primary"
+			/>
+		</span>
+		{clean(title)}
+	</span>
 
-			<span
-				class="accordion-title is-size-6 has-text-grey-dark mr-3"
-				use:tippyTooltip={{ content: title }}
-			>
-				{clean(title)}
-			</span>
-		</div>
+	<slot name="buttons" />
+</div>
 
-		<slot name="buttons" />
-	</div>
-	<div class="pb-2" hidden={!isExpanded}>
-		<slot name="content" />
-	</div>
-</article>
+<div class="border pb-2" hidden={!isExpanded}>
+	<slot name="content" />
+</div>
 
 <style lang="scss">
 	.border {
@@ -55,17 +51,31 @@
 	.header {
 		max-height: 60px;
 
-		.toggle {
+		.accordion-title {
 			cursor: pointer;
 			width: 100%;
+			overflow: hidden;
+			display: -webkit-box;
+			-webkit-box-orient: vertical;
+			-webkit-line-clamp: 1;
+			word-break: break-all;
 		}
-	}
 
-	.accordion-title {
-		overflow: hidden;
-		display: -webkit-box;
-		-webkit-box-orient: vertical;
-		-webkit-line-clamp: 1;
-		word-break: break-all;
+		.toggle-icon {
+			-webkit-transition: all 0.3s ease;
+			-moz-transition: all 0.3s ease;
+			-ms-transition: all 0.3s ease;
+			-o-transition: all 0.3s ease;
+			transition: all 0.3s ease;
+
+			&.active {
+				transform: rotate(-180deg);
+				-webkit-transform: rotate(-180deg);
+				-moz-transform: rotate(-180deg);
+				-ms-transform: rotate(-180deg);
+				-o-transform: rotate(-180deg);
+				transition: rotateZ(-180deg);
+			}
+		}
 	}
 </style>
