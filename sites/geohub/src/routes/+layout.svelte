618806ccd3197f2d1532a38aa483ddc270ac20c7
Jin Igarashi <igarashi.jin@water-gis.com>
2024-01-03 15:27:55 +0000

    fix: remove border-radius from bulma button to look like UNDP design button

--- sites/geohub/src/routes/+layout.svelte
+++ sites/geohub/src/routes/+layout.svelte
@@ -55,4 +55,9 @@
 <style global lang="scss">
 	@import '@undp-data/undp-bulma/bulma.scss';
 	@import 'https://use.fontawesome.com/releases/v6.1.1/css/all.css';
+
+	.button {
+		// remove border-radius from bulma button to look like UNDP design system
+		border-radius: 0;
+	}
 </style>
