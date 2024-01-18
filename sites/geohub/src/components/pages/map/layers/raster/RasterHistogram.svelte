0f75d9a8175880e6f6cc0d301eadd26a508093d1
Thuhaa <josephthuha@gmail.com>
2024-01-18 21:40:59 +0300

    refactor: modify appearance of histogram based on mockup

--- sites/geohub/src/components/pages/map/layers/raster/RasterHistogram.svelte
+++ sites/geohub/src/components/pages/map/layers/raster/RasterHistogram.svelte
@@ -9,35 +9,44 @@
 	const width = 300;
 	const height = 200;
 	const marginTop = 20;
-	const marginRight = 20;
-	const marginBottom = 40;
-	const marginLeft = 50;
+	const marginRight = 10;
+	const marginBottom = 30;
+	const marginLeft = 40;
 
 	onMount(async () => {
 		const band = metadata.active_band_no;
 		const counts = metadata.stats[band]['histogram'][0];
-		const interval = metadata.stats[band]['histogram'][1];
+		let interval = metadata.stats[band]['histogram'][1];
 		const [minCount, maxCount] = [Math.min(...counts), Math.max(...counts)];
 		const unit = tags.find((tag) => tag.key === 'unit')?.value ?? 'Intervals';
+
+		const data = counts.map((count, i) => ({ count, interval: [interval[i], interval[i + 1]] }));
 
 		// X-scale and X-axis
 		const x = d3
 			.scaleBand()
 			.domain(interval.map((d) => d))
 			.paddingInner(1)
-			.paddingOuter(0.05)
+			.paddingOuter(1)
 			.range([marginLeft, width - marginRight]);
 
-		const xAxis = d3.axisBottom(x).tickValues(x.domain()).tickFormat(d3.format('.2f'));
+		const xAxis = d3
+			.axisBottom(x)
+			.tickValues(x.domain().filter((_, i) => i % Math.ceil(x.domain().length / 6) === 0))
+			.tickFormat(d3.format('.2f'));
 
 		// Y-scale and Y-axis
 		const y = d3
 			.scaleLinear(d3.extent(counts))
 			.domain([minCount, maxCount])
 			.nice()
-			.range([height - marginBottom, marginTop]);
+			.range([height - marginBottom, marginTop])
+			.clamp(false);
 
-		const yAxis = d3.axisLeft(y).tickValues(d3.ticks(minCount, maxCount, 5));
+		const yAxis = d3
+			.axisLeft(y)
+			.tickValues(d3.ticks(minCount, maxCount, 4))
+			.tickFormat(d3.format('.2s'));
 
 		// SVG element
 		const svg = d3
@@ -45,9 +54,21 @@
 			.append('svg')
 			.attr('width', width)
 			.attr('height', height)
+			.style('overflow', 'visible')
 			.attr('viewBox', [0, 0, width, height]);
 
-		// Appending the x-axis to the SVG container
+		// horizontal dotted grid lines
+		svg
+			.append('g')
+			.attr('class', 'grid')
+			.attr('transform', `translate(${marginLeft}, 0)`)
+			.style('stroke-dasharray', '2,2')
+			.style('stroke-opacity', 0.5)
+			.call(yAxis.tickSize(-width + marginLeft + marginRight));
+
+		// removing the y-axis line
+		svg.select('.domain').remove();
+
 		svg
 			.append('g')
 			.attr('transform', `translate(0, ${height - marginBottom})`)
@@ -58,8 +79,6 @@
 			.attr('dy', '0.15em')
 			.attr('transform', 'rotate(-45)');
 
-		svg.append('g').attr('transform', `translate(${marginLeft}, 0)`).call(yAxis);
-
 		// Bars
 		svg
 			.selectAll('rect')
@@ -68,7 +87,7 @@
 			.append('rect')
 			.attr('x', (d, i) => x(interval[i]))
 			.attr('y', (d) => y(d))
-			.attr('width', 23)
+			.attr('width', 20)
 			.attr('height', (d) => height - marginBottom - y(d))
 			.attr('padding', 1)
 			.attr('stroke', 'white')
@@ -79,45 +98,81 @@
 				const xPos = parseFloat(d3.select(this).attr('x')) + width / counts.length / 2;
 				const yPos = parseFloat(d3.select(this).attr('y'));
 
-				// change color of bar on hover
-				d3.select(this).attr('fill', 'steelblue');
+				d3.select(this.parentNode).selectAll('rect').attr('opacity', 0.2);
+				// make the whole chart except the hovered bar transparent
+				d3.select(this.parentNode).selectAll('g').attr('opacity', 0.5);
+				d3.select(this).attr('opacity', 1);
 				d3.select(this).style('cursor', 'pointer');
 
-				// tooltip box
-				svg
-					.append('polygon')
-					.attr('id', 'tooltip-fill')
-					.attr('points', `${xPos - 5},${yPos - 5} ${xPos + 5},${yPos - 5} ${xPos},${yPos}`)
-					.attr('transform', `translate(-5, 0)`)
-					.attr('fill', 'black');
-				svg
+				// Setting the position for the tooltip
+				const tooltipWidth = 150;
+				const tooltipHeight = 80;
+				const offsetX = 5;
+				const offsetY = 80;
+
+				const adjustedX = Math.min(xPos - offsetX, width - tooltipWidth);
+				const adjustedY = Math.min(yPos - offsetY, height - tooltipHeight);
+
+				// Append a group element for the tooltip
+				var tooltipGroup = svg
+					.append('g')
+					.attr('id', 'tooltip-group')
+					.attr('transform', 'translate(' + adjustedX + ',' + adjustedY + ')');
+
+				tooltipGroup
 					.append('rect')
 					.attr('id', 'tooltip-rect')
-					.attr('x', xPos - 35)
-					.attr('y', yPos - 30)
-					.attr('width', 60)
-					.attr('height', 20)
-					.attr('fill', 'black')
-					.attr('rx', 5)
-					.attr('ry', 5);
-				// add text to tooltip box and center it
-				svg
+					.attr('width', tooltipWidth)
+					.attr('height', tooltipHeight)
+					.attr('fill', '#F7F7F7')
+					.attr('stroke', '#D4D6D8');
+				const currentInterval = data.find((item) => item.count === d).interval;
+				const maxTextLength = 20;
+				const text = `[${currentInterval[0].toPrecision(2)}, ${currentInterval[1].toPrecision(
+					2
+				)}]  ${unit.toUpperCase()}`;
+				const truncatedText =
+					text.length > maxTextLength ? `${text.slice(0, maxTextLength - 3)}...` : text;
+
+				tooltipGroup
 					.append('text')
-					.attr('id', 'tooltip-text')
-					.attr('x', xPos - 5)
-					.attr('y', yPos - 15)
+					.attr('x', tooltipWidth / 2)
+					.attr('y', 20)
 					.attr('text-anchor', 'middle')
-					.attr('fill', 'white')
+					.attr('text-overflow', 'clip')
+					.style('fill', '#006eb5')
+					.style('font-weight', 'bold')
+					.text(truncatedText);
+
+				tooltipGroup
+					.append('line')
+					.attr('x1', 10)
+					.attr('y1', 30)
+					.attr('x2', tooltipWidth - 10)
+					.attr('y2', 30)
+					.attr('class', 'is-divider mt-1 mb-0')
+					.style('stroke', '#D4D6D8');
+
+				tooltipGroup
+					.append('text')
+					.attr('x', 2)
+					.attr('y', 45)
+					.attr('class', 'has-text-weight-medium')
+					.text('Number of Pixels');
+
+				tooltipGroup
+					.append('text')
+					.attr('x', 2)
+					.attr('y', 65)
+					.attr('class', 'has-text-weight-bold')
 					.text(d);
 			})
 			.on('mouseout', function () {
-				// Remove the tooltip on mouseout
-				d3.select('#tooltip-text').remove();
-				d3.select('#tooltip-rect').remove();
-				d3.select('#tooltip-fill').remove();
-				// change color of bar
+				d3.selectAll('rect').attr('opacity', 1);
+				d3.select(this.parentNode).selectAll('g').attr('opacity', 1);
 				d3.select(this).attr('fill', '#006eb5');
 				d3.select(this).style('cursor', 'default');
+				d3.select('#tooltip-group').remove();
 			});
 
 		// Y-axis label
@@ -133,10 +188,10 @@
 		svg
 			.append('text')
 			.attr('text-anchor', 'middle')
-			.attr('transform', `translate(${width / 2}, ${height}) rotate(0)`)
+			.attr('transform', `translate(${width / 2}, ${height + 10}) rotate(0)`)
 			.style('font-size', '12px')
 			.style('font-weight', 'bold')
-			.text(`${unit}`);
+			.text(`${unit.toUpperCase()}`);
 	});
 </script>
 
