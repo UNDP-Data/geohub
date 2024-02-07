dfc07b3a75318061af9627af8ffc0b32796825fd
Jin Igarashi <jin.igarashi@undp.org>
2024-02-07 10:06:18 +0100

    fix: switched go back to previous page button to breadcrumbs for each  page (#2830)

---
+++ .changeset/pink-dragons-kick.md
@@ -0,0 +1,5 @@
+---
+"geohub": patch
+---
+
+fix: switched go back to previous page button to breadcrumbs for each page
---
+++ sites/geohub/src/components/util/Breadcrumbs.svelte
@@ -0,0 +1,35 @@
+<script context="module" lang="ts">
+	export interface BreadcrumbPage {
+		title: string;
+		url: string;
+	}
+</script>
+
+<script lang="ts">
+	export let pages: BreadcrumbPage[];
+</script>
+
+<nav class="breadcrumb has-text-weight-bold is-uppercase" aria-label="breadcrumbs">
+	<ul>
+		{#each pages as page, index}
+			{#if index === pages.length - 1}
+				<li class="is-active">
+					<!-- svelte-ignore a11y-missing-attribute -->
+					<a
+						aria-current="page"
+						data-sveltekit-preload-data="off"
+						data-sveltekit-preload-code="off"
+					>
+						{page.title}
+					</a>
+				</li>
+			{:else}
+				<li>
+					<a aria-current="page" href={page.url}>
+						{page.title}
+					</a>
+				</li>
+			{/if}
+		{/each}
+	</ul>
+</nav>
