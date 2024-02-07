0c7e8e621326f14dccaad5ef92134381790d2d46
Jin Igarashi <igarashi.jin@water-gis.com>
2024-02-07 14:57:09 +0100

    chore: upgrade maplibre/pmtiles for cgaz tool

--- packages/cgaz-admin-tool/vite.config.ts
+++ packages/cgaz-admin-tool/vite.config.ts
@@ -4,6 +4,9 @@ import { resolve } from 'path';
 
 export default defineConfig({
 	plugins: [sveltekit()],
+	ssr: {
+		noExternal: 'maplibre-gl'
+	},
 	resolve: {
 		alias: {
 			$example: resolve('./src/example')
