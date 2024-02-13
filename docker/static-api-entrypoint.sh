2c2c4596f86c2e228a6b9f1129be4234dbbf2816
Jin Igarashi <jin.igarashi@undp.org>
2024-02-13 12:06:27 +0000

    fix: don't execute evfb if DISPLAY is already in use (#2873)

--- docker/static-api-entrypoint.sh
+++ docker/static-api-entrypoint.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
-# Start Xvfb
-Xvfb ${DISPLAY} -screen 0 "1024x768x24" &
+# Check if DISPLAY is already in use
+if ! ps aux | grep -q "[X]vfb.*${DISPLAY}"; then
+  echo "Starting Xvfb..."
+  # Start Xvfb
+  Xvfb ${DISPLAY} -screen 0 "1024x768x24" &
+else
+  echo "Xvfb is already running on ${DISPLAY}"
+fi
 
 # Start your node application
 node /app/index.js
