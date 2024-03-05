8741ae5255a14958751e5e9f5bcf830e937f8aa7
Jin Igarashi <igarashi.jin@water-gis.com>
2024-03-05 15:11:48 +0000

    feat: add a link of tool page at landing page

--- sites/geohub/src/routes/(app)/+page.svelte
+++ sites/geohub/src/routes/(app)/+page.svelte
@@ -174,7 +174,23 @@
 	<ExploreDatasets />
 </section>
 
-<section id="dashboards" class="hero is-link py-6 my-6">
+<section id="tools" class="hero has-background-grey-darker mt-6">
+	<div class="hero-body has-text-centered">
+		<h2 class="title is-2 mb-4 has-text-white has-text-weight-bold">Analytical tools</h2>
+
+		<p class="is-size-5 has-text-white mx-auto mb-4 max-width">
+			More and more geospatial analytical tools for decision making are being developed to GeoHub.
+		</p>
+
+		<div class="mt-4">
+			<a class="button is-primary is-uppercase has-text-weight-bold" href="/tools">
+				<span>Explore analytical tools</span>
+			</a>
+		</div>
+	</div>
+</section>
+
+<section id="dashboards" class="hero is-link py-6 mb-6">
 	<div class="hero-body has-text-centered">
 		<h2 class="title is-2 mb-4 has-text-white has-text-weight-bold">Explore dashboards</h2>
 
