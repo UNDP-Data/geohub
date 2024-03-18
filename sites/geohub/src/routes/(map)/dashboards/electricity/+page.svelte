9cb035198debd8413c07b0909742dc14e9fc37a8
John Kennith Mirano <john.mirano@ibm.com>
2024-03-18 21:11:26 +0800

    refactor: electricity dashboard analyse electricity

--- sites/geohub/src/routes/(map)/dashboards/electricity/+page.svelte
+++ sites/geohub/src/routes/(map)/dashboards/electricity/+page.svelte
@@ -158,6 +158,7 @@
 	let showExplore = false;
 	let showCompare = false;
 	let showAnalyse = false;
+	let showLegend = true;
 	let POVERTY_ID = 'poverty';
 
 	let layers = ['ADM0', 'ADM1', 'ADM2', 'ADM3', 'ADM4'];
@@ -262,7 +263,7 @@
 
 					{#if showCompare}
 						<div>
-							<p>asd12312</p>
+							<p>Content Later.</p>
 						</div>
 					{/if}
 				</div>
@@ -289,8 +290,63 @@
 					</button>
 
 					{#if showAnalyse}
-						<div>
-							<p>124234</p>
+						<div class="a-fixed a-legend__wrapper has-background-white p-4">
+							<button
+								class="a-reset a-legend__button is-flex is-justify-content-space-between {showLegend
+									? 'mb-4 clicked'
+									: ''}"
+								type="button"
+								on:click={(e) => (showLegend = !showLegend)}>Legend</button
+							>
+
+							{#if showLegend}
+								<div>
+									<div class="is-flex is-flex-wrap-wrap is-justify-content-space-between mb-2">
+										<div class="is-size-7"><strong>Wealth</strong> <br /> 100%</div>
+										<div class="a-legend__container is-flex is-flex-wrap-wrap">
+											<div class="a-legend__item" style="background-color: #F3618C;"></div>
+											<div class="a-legend__item" style="background-color: #CE5495;"></div>
+											<div class="a-legend__item" style="background-color: #A9469E;"></div>
+											<div class="a-legend__item" style="background-color: #8339A6;"></div>
+											<div class="a-legend__item" style="background-color: #5E2BAF;"></div>
+											<div class="a-legend__item" style="background-color: #F689A9;"></div>
+											<div class="a-legend__item" style="background-color: #B679A9;"></div>
+											<div class="a-legend__item" style="background-color: #916CB2;"></div>
+											<div class="a-legend__item" style="background-color: #6C5EBB;"></div>
+											<div class="a-legend__item" style="background-color: #4750C3;"></div>
+											<div class="a-legend__item" style="background-color: #F9B0C6;"></div>
+											<div class="a-legend__item" style="background-color: #B9A1C6;"></div>
+											<div class="a-legend__item" style="background-color: #7A91C6;"></div>
+											<div class="a-legend__item" style="background-color: #5583CE;"></div>
+											<div class="a-legend__item" style="background-color: #2F76D7;"></div>
+											<div class="a-legend__item" style="background-color: #FCD8E2;"></div>
+											<div class="a-legend__item" style="background-color: #BDC8E3;"></div>
+											<div class="a-legend__item" style="background-color: #7DB8E2;"></div>
+											<div class="a-legend__item" style="background-color: #3DA8E2;"></div>
+											<div class="a-legend__item" style="background-color: #189BEB;"></div>
+
+											<div
+												class="a-legend__item"
+												style="background-color: #FFFFFF; border: 1px solid #d4d6d8"
+											></div>
+											<div class="a-legend__item" style="background-color: #BFEFFF;"></div>
+											<div class="a-legend__item" style="background-color: #80E0FF;"></div>
+											<div class="a-legend__item" style="background-color: #40D0FF;"></div>
+											<div class="a-legend__item" style="background-color: #00C0FF;"></div>
+										</div>
+									</div>
+
+									<div class="is-flex is-flex-wrap-wrap is-justify-content-space-between">
+										<div></div>
+										<div
+											class="a-legend__container is-justify-content-space-between is-flex is-flex-wrap-wrap"
+										>
+											<p class="is-size-7">0% <br /> <strong>Energy access</strong></p>
+											<p class="is-size-7">100%</p>
+										</div>
+									</div>
+								</div>
+							{/if}
 						</div>
 					{/if}
 				</div>
@@ -299,23 +355,6 @@
 					<CtaLink label="Download" isArrow />
 				</button>
 			</div>
-
-			<!-- <div class="box mx-0 my-1">
-				<p class="title is-5 p-0 m-0 has-text-centered pb-2">Raw Data - Electricity Access</p>
-				<ElectricityControl bind:electricitySelected bind:loadRasterLayer />
-			</div>
-			<div class="box mx-0 my-1">
-				<p class="title is-5 p-0 m-0 has-text-centered pb-2">Overlays</p>
-				<OverlayControl />
-			</div>
-			<div class="box mx-0 my-1">
-				<p class="title is-5 p-0 m-0 has-text-centered pb-2">Statistics - Electricity Access</p>
-				<Charts />
-			</div>
-			<div class="box mx-0 my-1">
-				<p class="title is-5 p-0 m-0 has-text-centered pb-2">Statistics - Download</p>
-				<DownloadData />
-			</div> -->
 		{/if}
 	</div>
 	<div slot="main">
@@ -398,6 +437,44 @@
 
 		&-full-w {
 			width: 100%;
+		}
+
+		&-legend {
+			&__wrapper {
+				width: 300px;
+				top: 165px;
+				left: 367px;
+				border-radius: 4px;
+				box-shadow: 2px 2px 2px 0 #7d7d7d;
+			}
+
+			&__container {
+				width: calc(100% - 65px);
+			}
+
+			&__item {
+				width: 39px;
+				height: 39px;
+				margin: 0.5px;
+				background-color: #f9f9f9;
+			}
+
+			&__button {
+				width: 100%;
+				cursor: pointer;
+
+				&:after {
+					content: '\f077';
+					font-family: 'Font Awesome 5 Free';
+					font-weight: 900;
+				}
+
+				&.clicked:after {
+					content: '\f078';
+					font-family: 'Font Awesome 5 Free';
+					font-weight: 900;
+				}
+			}
 		}
 	}
 </style>
