9ce18fe4c4c1d01d880f1da06118efa6e5160836
Jin Igarashi <igarashi.jin@water-gis.com>
2024-03-20 16:13:49 +0000

    fix: set href as null instead of empty string if tab id is not starting with hash

--- packages/svelte-undp-components/src/lib/components/ColorMapPicker.svelte
+++ packages/svelte-undp-components/src/lib/components/ColorMapPicker.svelte
@@ -105,13 +105,7 @@
 </div>
 
 <div bind:this={tooltipContent} data-testid="color-map-picker" class="tooltip p-2">
-	<Tabs
-		bind:tabs
-		bind:activeTab={activeColorMapType}
-		on:tabChange={(e) => (activeColorMapType = e.detail)}
-		size="is-small"
-		fontWeight="semibold"
-	/>
+	<Tabs bind:tabs bind:activeTab={activeColorMapType} size="is-small" fontWeight="semibold" />
 
 	<button class="delete close is-radiusless"></button>
 
