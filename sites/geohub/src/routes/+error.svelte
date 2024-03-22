e6fbbbc820e97571af4875ef0ca21bd7b6f3d83a
Jin Igarashi <jin.igarashi@undp.org>
2024-03-22 12:33:03 +0000

    fix: removed hardcoded links from header and footer (#3175)
    * refactor: moved HeaderItems and FooterItems to /server/config and return items at +layout.server.ts
    
    * added GEOHUB_DOCS_ENDPOINT variable in .env
    
    * fix: update static api URL in footer from GEOHUB_STATIC_IMAGE_API
    
    * fix: added SVELTE_UNDP_DESIGN_ENDPOINT and SVELTE_UNDP_COMPONENTS_ENDPOINT variables to .env
    
    * changeset

--- sites/geohub/src/routes/+error.svelte
+++ sites/geohub/src/routes/+error.svelte
@@ -1,7 +1,6 @@
 <script lang="ts">
 	import { page } from '$app/stores';
 	import Header from '$components/header/Header.svelte';
-	import { FooterItems } from '$lib/config/AppConfig';
 	import { HEADER_HEIGHT_CONTEXT_KEY, createHeaderHeightStore } from '$stores';
 	import { Footer } from '@undp-data/svelte-undp-design';
 	import { setContext } from 'svelte';
@@ -9,12 +8,7 @@
 	const headerHeightStore = createHeaderHeightStore();
 	setContext(HEADER_HEIGHT_CONTEXT_KEY, headerHeightStore);
 
-	let footerItems = FooterItems;
-	if (!($page.data.session?.user?.is_superuser === true)) {
-		if (footerItems['Management']) {
-			delete footerItems['Management'];
-		}
-	}
+	let footerLinks = $page.data.footerLinks;
 </script>
 
 <div class="header">
@@ -37,4 +31,4 @@
 	</div>
 </section>
 
-<Footer logoUrl="/assets/undp-images/undp-logo-white.svg" bind:footerItems />
+<Footer logoUrl="/assets/undp-images/undp-logo-white.svg" bind:footerItems={footerLinks} />
