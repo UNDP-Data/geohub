b870a1dd06c7b93d42287775c8f81317380c37f2
Jin Igarashi <jin.igarashi@undp.org>
2024-03-25 11:08:04 +0000

    fix: fixed bug not showing management link in footer correctly (#3185)

--- sites/geohub/src/routes/+layout.server.ts
+++ sites/geohub/src/routes/+layout.server.ts
@@ -3,8 +3,8 @@ import { env } from '$env/dynamic/private';
 import { MapStyleId, SiteInfo } from '$lib/config/AppConfig';
 import { upsertUser, isSuperuser } from '$lib/server/helpers';
 import type { HeaderLink } from '@undp-data/svelte-undp-design';
-import { HeaderItems, type LineName } from '$lib/server/config/HeaderItems';
-import { FooterItems } from '$lib/server/config/FooterItems';
+import { HeaderItems } from '$lib/server/config/HeaderItems';
+import { getFooterItems, type FooterItemType } from '$lib/server/config/FooterItems';
 
 export const load: LayoutServerLoad = async ({ locals, url }) => {
 	const session = await locals.getSession();
@@ -19,10 +19,12 @@ export const load: LayoutServerLoad = async ({ locals, url }) => {
 		await upsertUser(session.user.email);
 	}
 
+	let is_superuser = false;
 	if (session?.user?.email) {
+		is_superuser = await isSuperuser(session.user.email);
 		// eslint-disable-next-line @typescript-eslint/ban-ts-comment
 		// @ts-ignore
-		session.user.is_superuser = await isSuperuser(session.user.email);
+		session.user.is_superuser = is_superuser;
 	}
 
 	const title = 'GeoHub';
@@ -34,45 +36,11 @@ export const load: LayoutServerLoad = async ({ locals, url }) => {
 	const socialImage = `${env.GEOHUB_STATIC_IMAGE_API}/og?url=${ogStyle}`;
 	const ogUrl = `${url.origin}${url.pathname}`;
 
-	const footerLinks = FooterItems;
+	const footerTypes: FooterItemType[] = ['geohub', 'dashboard', 'dev'];
+	if (is_superuser) footerTypes.push('management');
+	const footerLinks = getFooterItems(footerTypes);
 
-	const items: LineName[] = ['home', 'data', 'map', 'tools'];
-	if (env.GEOHUB_DOCS_ENDPOINT) {
-		items.push('support');
-
-		// update support link
-		const link = footerLinks['GeoHub'].find((l) => l.title.toLocaleLowerCase() === 'support');
-		link.url = env.GEOHUB_DOCS_ENDPOINT;
-	} else {
-		footerLinks['GeoHub'] = [
-			...footerLinks['GeoHub'].filter((l) => l.title.toLocaleLowerCase() !== 'support')
-		];
-	}
-	const headerLinks: HeaderLink[] = HeaderItems(items, env.GEOHUB_DOCS_ENDPOINT);
-
-	if (!(session?.user?.is_superuser === true)) {
-		if (footerLinks['Management']) {
-			delete footerLinks['Management'];
-		}
-	}
-
-	const staticApiUrl = env.GEOHUB_STATIC_IMAGE_API;
-	const devLinks = footerLinks['For Developers'];
-	if (staticApiUrl) {
-		const link = devLinks.find((l) => l.title === 'Static Image API');
-		link.url = staticApiUrl.replace('/api', '');
-	}
-
-	if (env.SVELTE_UNDP_DESIGN_ENDPOINT) {
-		const link = devLinks.find((l) => l.title === 'Svelte UNDP design sytem');
-		link.url = env.SVELTE_UNDP_DESIGN_ENDPOINT;
-	}
-
-	if (env.SVELTE_UNDP_COMPONENTS_ENDPOINT) {
-		const link = devLinks.find((l) => l.title === 'Svelte UNDP Components');
-		link.url = env.SVELTE_UNDP_COMPONENTS_ENDPOINT;
-	}
-	footerLinks['For Developers'] = [...devLinks.filter((l) => l.url !== '')];
+	const headerLinks: HeaderLink[] = HeaderItems(['home', 'data', 'map', 'tools', 'support']);
 
 	return {
 		session,
@@ -82,7 +50,7 @@ export const load: LayoutServerLoad = async ({ locals, url }) => {
 		site_description,
 		socialImage,
 		ogUrl,
-		staticApiUrl: staticApiUrl,
+		staticApiUrl: env.GEOHUB_STATIC_IMAGE_API,
 		headerLinks,
 		footerLinks
 	};
