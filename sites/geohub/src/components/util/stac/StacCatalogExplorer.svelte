1d848c20607b80eb9772ddb8aaf4663fe0b0b91b
Jin Igarashi <igarashi.jin@water-gis.com>
2024-04-03 11:24:51 +0100

    refactor: use Breadcrumbs component for StacCatalogExplorer

--- sites/geohub/src/components/util/stac/StacCatalogExplorer.svelte
+++ sites/geohub/src/components/util/stac/StacCatalogExplorer.svelte
@@ -3,7 +3,7 @@
 	import StacCatalogItem from '$components/util/stac/StacCatalogItem.svelte';
 	import StacCatalogMap from '$components/util/stac/StacCatalogMap.svelte';
 	import type { DatasetFeature, Stac, StacCatalogBreadcrumb } from '$lib/types';
-	import { clean, handleEnterKey } from '@undp-data/svelte-undp-components';
+	import { Breadcrumbs, clean } from '@undp-data/svelte-undp-components';
 	import { Loader } from '@undp-data/svelte-undp-design';
 	import { createEventDispatcher, onMount } from 'svelte';
 
@@ -27,14 +27,14 @@
 		if (collectionId) {
 			const page: StacCatalogBreadcrumb = {
 				title: clean(dataset.properties.name),
-				url: dataset.properties.url,
+				dataUrl: dataset.properties.url,
 				type: 'Collection'
 			};
 			StacBreadcrumbs = [page];
 		} else {
 			const page: StacCatalogBreadcrumb = {
 				title: clean(stac.id),
-				url: stac.url,
+				dataUrl: stac.url,
 				type: 'Catalog'
 			};
 			StacBreadcrumbs = [page];
@@ -55,7 +55,8 @@
 		dispatch('breadcrumbSelected', data);
 	};
 
-	const handleBreadcrumbClicked = (page: StacCatalogBreadcrumb) => {
+	const handleBreadcrumbClicked = (e) => {
+		const page: StacCatalogBreadcrumb = e.detail;
 		if (StacBreadcrumbs?.length > 0) {
 			const pageIndex = StacBreadcrumbs.findIndex((p) => p.title === page.title);
 			StacBreadcrumbs = [...StacBreadcrumbs.slice(0, pageIndex + 1)];
@@ -70,68 +71,37 @@
 
 <section class=" p-4">
 	{#if StacBreadcrumbs && StacBreadcrumbs.length > 0}
-		<nav class="breadcrumb has-text-weight-bold is-medium" aria-label="breadcrumbs">
-			<ul>
-				{#each StacBreadcrumbs as page, index}
-					{#if index === StacBreadcrumbs.length - 1}
-						<li class="is-active">
-							<!-- svelte-ignore a11y-missing-attribute -->
-							<a
-								aria-current="page"
-								data-sveltekit-preload-data="off"
-								data-sveltekit-preload-code="off">{page.title}</a
-							>
-						</li>
-					{:else}
-						<li>
-							<!-- svelte-ignore a11y-missing-attribute -->
-							<a
-								role="button"
-								tabindex="0"
-								on:click={() => {
-									handleBreadcrumbClicked(page);
-								}}
-								on:keydown={handleEnterKey}
-								data-sveltekit-preload-data="off"
-								data-sveltekit-preload-code="off"
-							>
-								{page.title}
-							</a>
-						</li>
-					{/if}
-				{/each}
-			</ul>
-		</nav>
+		<Breadcrumbs bind:pages={StacBreadcrumbs} size="small" on:click={handleBreadcrumbClicked} />
 
 		{#each StacBreadcrumbs as page, index}
 			{@const isLastPage = index === StacBreadcrumbs.length - 1}
 			<div hidden={!isLastPage}>
 				{#if page.type === 'Catalog'}
 					{@const collectionUrls = StacBreadcrumbs.filter((x) => x.type === 'Collection')}
-					{@const fistColleciton = collectionUrls.length > 0 ? collectionUrls[0]?.url : ''}
+					{@const fistColleciton = collectionUrls.length > 0 ? collectionUrls[0]?.dataUrl : ''}
 					<StacCatalogMap
 						bind:stacId={stac.id}
-						bind:url={page.url}
+						bind:url={page.dataUrl}
 						collectionUrl={fistColleciton}
 						on:selected={handleSelectCollection}
 						on:dataAdded={dataAddedToMap}
 					/>
 				{:else if page.type === 'Collection'}
 					{@const collectionUrls = StacBreadcrumbs.filter((x) => x.type === 'Collection')}
-					{@const fistColleciton = collectionUrls[0].url}
+					{@const fistColleciton = collectionUrls[0].dataUrl}
 					<StacCatalogCollections
 						bind:stacId={stac.id}
 						collectionUrl={fistColleciton}
-						bind:url={page.url}
+						bind:url={page.dataUrl}
 						on:selected={handleSelectChild}
 						on:dataAdded={dataAddedToMap}
 					/>
 				{:else if page.type === 'Item'}
 					{@const collectionUrls = StacBreadcrumbs.filter((x) => x.type === 'Collection')}
-					{@const fistColleciton = collectionUrls[0].url}
+					{@const fistColleciton = collectionUrls[0].dataUrl}
 					<StacCatalogItem
 						bind:stacId={stac.id}
-						bind:url={page.url}
+						bind:url={page.dataUrl}
 						collectionUrl={fistColleciton}
 						on:dataAdded={dataAddedToMap}
 					/>
