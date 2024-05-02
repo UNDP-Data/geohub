bbdcc5aa840efd2b5ff45105e08105930cc5b883
Jin Igarashi <jin.igarashi@undp.org>
2024-05-02 21:14:24 +0900

    Fix/3424 sidebar open button (#3426)
    * fix: removed unnecessary import of fontawesome css
    
    * fix: fixed sidebar button color and style

--- sites/geohub/src/routes/(app)/+page.ts
+++ sites/geohub/src/routes/(app)/+page.ts
@@ -1,14 +1,13 @@
 import type { PageLoad } from './$types';
 
 export const load: PageLoad = async ({ data }) => {
-	const { stats, styles } = data;
 	const title = 'GeoHub';
 	const content = 'Welcome to GeoHub!';
 
 	return {
 		title,
 		content,
-		stats,
-		styles
+		stats: data.stats,
+		styles: data.styles
 	};
 };
