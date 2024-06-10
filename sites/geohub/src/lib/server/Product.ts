0d512fb710942648a7b9a58c66407cf6047407a3
Jin Igarashi <jin.igarashi@undp.org>
2024-06-10 14:59:50 +0100

    fix: returns 404 error from /products/[id] endpoint. Refactored products endpoint code. (#3641)

--- sites/geohub/src/lib/server/Product.ts
+++ sites/geohub/src/lib/server/Product.ts
@@ -2,41 +2,60 @@
  * To register products in GeoHub using the /api/products endpoint
  * ALLOWED METHODS: GET, POST, PUT, DELETE
  */
-import type { PoolClient, QueryResult } from 'pg';
-import { error } from '@sveltejs/kit';
+import type { Product } from '$lib/types';
+import type { PoolClient } from 'pg';
 
-class Product {
-	private id: string;
-	private description: string;
-	private expression: string;
-	private label: string;
+export class ProductManager {
+	private product: Product;
 
-	constructor(id: string, description: string, expression: string, label: string) {
-		this.id = id;
-		this.description = description;
-		this.expression = expression;
-		this.label = label;
+	constructor(id: string, description?: string, expression?: string, label?: string) {
+		this.product = {
+			id,
+			description,
+			expression,
+			label
+		};
 	}
 
-	public async registerProduct(client: PoolClient): Promise<QueryResult> {
+	public async get(client: PoolClient): Promise<Product> {
+		const query = {
+			text: `SELECT id, label, expression, description FROM geohub.product WHERE id = $1`,
+			values: [this.product.id]
+		};
+		const res = await client.query(query);
+		if (res.rowCount === 0) {
+			return undefined;
+		}
+		return res.rows[0] as Product;
+	}
+
+	public async insert(client: PoolClient): Promise<Product> {
 		const query = `INSERT INTO geohub.product (id, description, expression, label) VALUES ($1, $2, $3, $4)`;
-		const values = [this.id, this.description, this.expression, this.label];
-		try {
-			return await client.query(query, values);
-		} catch (er) {
-			error(500, er);
-		}
+		const values = [
+			this.product.id,
+			this.product.description,
+			this.product.expression,
+			this.product.label
+		];
+		await client.query(query, values);
+		return this.product;
 	}
 
-	public async upsertProduct(client: PoolClient): Promise<QueryResult> {
+	public async update(client: PoolClient): Promise<Product> {
 		const query = `UPDATE geohub.product SET description=$2, expression=$3, label=$4 WHERE id=$1`;
-		const values = [this.id, this.description, this.expression, this.label];
-		try {
-			return await client.query(query, values);
-		} catch (er) {
-			error(500, er);
-		}
+		const values = [
+			this.product.id,
+			this.product.description,
+			this.product.expression,
+			this.product.label
+		];
+		await client.query(query, values);
+		return this.product;
+	}
+
+	public async delete(client: PoolClient): Promise<void> {
+		const query = `DELETE FROM geohub.product WHERE id=$1`;
+		const values = [this.product.id];
+		await client.query(query, values);
 	}
 }
-
-export default Product;
