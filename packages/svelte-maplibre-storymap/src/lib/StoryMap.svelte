ccb7fb7502e4dfd28c2ceeb3b043d3f4bc94129e
Jin Igarashi <igarashi.jin@water-gis.com>
2024-08-30 16:36:33 +0100

    fix: make attribution compact

--- packages/svelte-maplibre-storymap/src/lib/StoryMap.svelte
+++ packages/svelte-maplibre-storymap/src/lib/StoryMap.svelte
@@ -33,6 +33,7 @@
 	export let config: StoryMapConfig;
 	export let template: StoryMapTemplate = 'light';
 	export let marginTop = 0;
+	export let compactAttribution = true;
 
 	const tippyTooltip = initTooltipTippy({
 		placement: 'left',
@@ -91,7 +92,7 @@
 			attributionControl: false
 		});
 
-		map.addControl(new AttributionControl({ compact: false }), 'bottom-right');
+		map.addControl(new AttributionControl({ compact: compactAttribution }), 'bottom-right');
 
 		if (!navigationControl) {
 			navigationControl = new NavigationControl();
