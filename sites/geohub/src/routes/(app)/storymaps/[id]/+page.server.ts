3b406f3323960730a68b7ad174a5c779496f0ef9
Jin Igarashi <igarashi.jin@water-gis.com>
2024-08-30 11:06:26 +0100

    refacor: created a common function for +page.server.ts

--- sites/geohub/src/routes/(app)/storymaps/[id]/+page.server.ts
+++ sites/geohub/src/routes/(app)/storymaps/[id]/+page.server.ts
@@ -1,55 +1,12 @@
 import type { PageServerLoad } from './$types';
-import { error } from '@sveltejs/kit';
-import { getDomainFromEmail } from '$lib/helper';
-import { AccessLevel, Permission } from '$lib/config/AppConfig';
-import type { StoryMapConfig } from '$lib/types';
+import { loadStorymap } from './loadStorymap';
 
 export const load: PageServerLoad = async (event) => {
 	const { params, parent, fetch } = event;
 	const { session, socialImage } = await parent();
 	const user = session?.user;
 	const id = params.id;
-
-	const res = await fetch(`/api/storymaps/${id}`);
-	if (!res.ok) {
-		if (res.status === 403) {
-			error(res.status, { message: 'No permission to access' });
-		} else if (res.status === 404) {
-			error(res.status, { message: 'No storymap found' });
-		} else {
-			error(res.status, { message: res.statusText });
-		}
-	}
-
-	const storymap: StoryMapConfig = await res.json();
-
-	const accessLevel = storymap.access_level;
-	if (accessLevel === AccessLevel.PRIVATE) {
-		if (!(storymap.permission && storymap.permission >= Permission.READ)) {
-			error(403, { message: 'Permission error' });
-		}
-	} else if (accessLevel === AccessLevel.ORGANIZATION) {
-		let domain = '';
-		if (user?.email) {
-			domain = getDomainFromEmail(user?.email);
-		}
-		if (!(domain && storymap.created_user && storymap.created_user.indexOf(domain) > -1)) {
-			if (!(storymap.permission && storymap.permission >= Permission.READ)) {
-				error(403, { message: 'Permission error' });
-			}
-		}
-	}
-
-	const staticUrl = storymap.links?.find((l) => l.rel === 'ogimage')?.href;
-	const socialImageUrl = staticUrl ?? socialImage;
-
-	const title = `${storymap.title} | GeoHub`;
-	const site_description = `GeoHub storymap for ${storymap.title}`;
-
-	return {
-		title,
-		site_description,
-		storymap,
-		socialImage: socialImageUrl
-	};
+	const user_email = user?.email as string;
+	const res = await loadStorymap(id, user_email, socialImage, fetch);
+	return res;
 };
---
+++ sites/geohub/src/routes/(app)/storymaps/[id]/loadStorymap.ts
@@ -0,0 +1,55 @@
+import { AccessLevel, Permission } from '$lib/config/AppConfig';
+import { getDomainFromEmail } from '$lib/helper';
+import type { StoryMapConfig } from '$lib/types';
+import { error } from '@sveltejs/kit';
+
+export const loadStorymap = async (
+	id: string,
+	user_email: string,
+	socialImage: string,
+	fetch: (input: RequestInfo | URL, init?: RequestInit) => Promise<Response>
+) => {
+	const res = await fetch(`/api/storymaps/${id}`);
+
+	if (!res.ok) {
+		if (res.status === 403) {
+			error(res.status, { message: 'No permission to access' });
+		} else if (res.status === 404) {
+			error(res.status, { message: 'No storymap found' });
+		} else {
+			error(res.status, { message: res.statusText });
+		}
+	}
+
+	const storymap: StoryMapConfig = await res.json();
+
+	const accessLevel = storymap.access_level;
+	if (accessLevel === AccessLevel.PRIVATE) {
+		if (!(storymap.permission && storymap.permission >= Permission.READ)) {
+			error(403, { message: 'Permission error' });
+		}
+	} else if (accessLevel === AccessLevel.ORGANIZATION) {
+		let domain = '';
+		if (user_email) {
+			domain = getDomainFromEmail(user_email);
+		}
+		if (!(domain && storymap.created_user && storymap.created_user.indexOf(domain) > -1)) {
+			if (!(storymap.permission && storymap.permission >= Permission.READ)) {
+				error(403, { message: 'Permission error' });
+			}
+		}
+	}
+
+	const staticUrl = storymap.links?.find((l) => l.rel === 'ogimage')?.href;
+	const socialImageUrl = staticUrl ?? socialImage;
+
+	const title = `${storymap.title} | GeoHub`;
+	const site_description = `GeoHub storymap for ${storymap.title}`;
+
+	return {
+		title,
+		site_description,
+		storymap,
+		socialImage: socialImageUrl
+	};
+};
