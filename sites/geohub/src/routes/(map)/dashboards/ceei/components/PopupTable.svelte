1d8585e25f4e0b76cb982ba96559512df9594377
Lance Ricco Teng <lancericco.teng@gmail.com>
2024-09-03 21:19:44 +0800

    fix: Add sources to CEEI popup

--- sites/geohub/src/routes/(map)/dashboards/ceei/components/PopupTable.svelte
+++ sites/geohub/src/routes/(map)/dashboards/ceei/components/PopupTable.svelte
@@ -42,23 +42,150 @@
 	);
 
 	let featureDetails = {
-		'Solar Power Potential': { unit: 'kWh/kWp', refYear: 2023 },
-		'Wind Speed': { unit: 'm/s', refYear: 2023 },
-		'Geothermal Power Potential': { unit: 'unitless (index, 0 to 1)', refYear: 2019 },
-		'Hydro Power Potential': { unit: 'MWH/year', refYear: 2010 },
-		'GHG Emissions': { unit: 'm/km²', refYear: 2021 },
-		'Net Electricity Imports': { unit: 'MwH, per capita', refYear: 2022 },
-		'Fossil Fuel Share on Energy Capacity and Generation': { unit: '%', refYear: 2022 },
-		'Relative Wealth Index': { unit: 'unitless (index, -1 to 1)', refYear: 2021 },
-		'Grid Density': { unit: 'm/km²', refYear: 2021 },
-		'Access to electricity': { unit: '% of population', refYear: 2020 },
-		'Households with access to loans from commercial banks': { unit: '% of GDP', refYear: 2022 },
-		'Jobs in Renewable Energy Sector': { unit: 'jobs, per 1000hab', refYear: 2021 },
+		'Solar Power Potential': {
+			unit: 'kWh/kWp',
+			refYear: 2023,
+			source: [
+				{
+					name: 'World Bank',
+					url: 'https://globalsolaratlas.info/'
+				}
+			]
+		},
+		'Wind Speed': {
+			unit: 'm/s',
+			refYear: 2023,
+			source: [
+				{
+					name: 'World Bank',
+					url: 'https://globalwindatlas.info/em'
+				}
+			]
+		},
+		'Geothermal Power Potential': {
+			unit: 'unitless (index, 0 to 1)',
+			refYear: 2019,
+			source: [
+				{
+					name: 'Italian National Research Council (CNR)',
+					url: 'https://www.sciencedirect.com/science/article/pii/S0959652620319211?via%3Dihub'
+				}
+			]
+		},
+		'Hydro Power Potential': {
+			unit: 'MWH/year',
+			refYear: 2010,
+			source: [
+				{
+					name: 'Delft University of Technology',
+					url: 'https://data.4tu.nl/articles/dataset/Global_potential_hydropower_locations/12708413/1'
+				}
+			]
+		},
+		'GHG Emissions': {
+			unit: 'm/km²',
+			refYear: 2021,
+			source: [
+				{
+					name: 'EC/JRC',
+					url: 'https://edgar.jrc.ec.europa.eu/'
+				}
+			]
+		},
+		'Net Electricity Imports': {
+			unit: 'MwH, per capita',
+			refYear: 2022,
+			source: [
+				{
+					name: 'Ember/European Electricity Review',
+					url: 'https://ourworldindata.org/grapher/net-electricity-imports?time=2015#sources-and-processing'
+				}
+			]
+		},
+		'Fossil Fuel Share on Energy Capacity and Generation': {
+			unit: '%',
+			refYear: 2022,
+			source: [
+				{
+					name: 'IRENA',
+					url: 'https://pxweb.irena.org/pxweb/en/IRENASTAT?_gl=1*upqkmt*_ga*MTIyNzEwMDQxNy4xNzA5MTIyNzIx*_ga_7W6ZEF19K4*MTcxMTU2Njk3Mi4zLjEuMTcxMTU2NzQ4MC41MC4wLjA.'
+				}
+			]
+		},
+		'Relative Wealth Index': {
+			unit: 'unitless (index, -1 to 1)',
+			refYear: 2021,
+			source: [
+				{
+					name: 'Meta',
+					url: 'https://dataforgood.facebook.com/dfg/tools/relative-wealth-index'
+				}
+			]
+		},
+		'Grid Density': {
+			unit: 'm/km²',
+			refYear: 2021,
+			source: [
+				{
+					name: 'GridFinder',
+					url: 'https://gridfinder.rdrn.me/'
+				}
+			]
+		},
+		'Access to electricity': {
+			unit: '% of population',
+			refYear: 2020,
+			source: [
+				{
+					name: 'UNDP/Univ.Michigan',
+					url: 'https://geohub.data.undp.org/data/319d1c2288a1d91e696cdada20a03e54'
+				}
+			]
+		},
+		'Households with access to loans from commercial banks': {
+			unit: '% of GDP',
+			refYear: 2022,
+			source: [
+				{
+					name: 'IMF',
+					url: 'https://data.imf.org/?sk=E5DCAB7E-A5CA-4892-A6EA-598B5463A34C'
+				}
+			]
+		},
+		'Jobs in Renewable Energy Sector': {
+			unit: 'jobs, per 1000hab',
+			refYear: 2021,
+			source: [
+				{
+					name: 'IRENA',
+					url: 'https://pxweb.irena.org/pxweb/en/IRENASTAT?_gl=1*upqkmt*_ga*MTIyNzEwMDQxNy4xNzA5MTIyNzIx*_ga_7W6ZEF19K4*MTcxMTU2Njk3Mi4zLjEuMTcxMTU2NzQ4MC41MC4wLjA.'
+				}
+			]
+		},
 		'Public and foreign (aid) investments on renewable energy': {
 			unit: 'USD, per capita',
-			refYear: 2021
+			refYear: 2021,
+			source: [
+				{
+					name: 'IRENA',
+					url: 'https://www.irena.org/Data/View-data-by-topic/Finance-and-Investment/Renewable-Energy-Finance-Flows'
+				},
+				{
+					name: 'OECD',
+					url: 'https://www.oecd.org/dac/financing-sustainable-development/development-finance-standards/official-development-assistance.htm'
+				}
+			]
 		},
-		'Education Index': { unit: 'unitless (index, 0-1)', refYear: 2021 }
+		'Education Index': {
+			unit: 'unitless (index, 0-1)',
+			refYear: 2021,
+			source: [
+				{
+					name: 'Global Data Lab',
+					url: 'https://globaldatalab.org/shdi/metadata/edindex/'
+				}
+			]
+		}
 	};
 
 	const handleAccordionSelect = (pillar) => {
@@ -104,6 +231,16 @@
 								</div>
 								<div class="is-italic is-size-7">
 									{featureDetails[key].unit} ({featureDetails[key].refYear})
+								</div>
+								<div class="is-italic is-size-7">
+									<span>source: </span>
+									<ul class="source-list">
+										{#each featureDetails[key].source as source}
+											<li>
+												<a href={source.url}>{source.name}</a>
+											</li>
+										{/each}
+									</ul>
 								</div>
 							</td>
 							{#each formattedFeatures as feature}
@@ -153,5 +290,24 @@
 		-o-transform: rotate(180deg);
 		transform: rotate(180deg);
 		transition: rotateZ(180deg);
+	}
+
+	.source-list {
+		display: inline;
+		list-style: none;
+	}
+
+	.source-list li {
+		display: inline;
+		list-style: none;
+		margin: 0px;
+	}
+
+	.source-list li:after {
+		content: ', ';
+	}
+
+	.source-list li:last-child:after {
+		content: '';
 	}
 </style>
