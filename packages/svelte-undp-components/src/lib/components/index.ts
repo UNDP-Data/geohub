a81b7caf3dd7f46cf7e62d6e15013f9aff6ca9fc
Jin Igarashi <igarashi.jin@water-gis.com>
2024-09-04 12:14:17 +0100

    feat: added SDGSelector component in svelte-undp-component package.

--- packages/svelte-undp-components/src/lib/components/index.ts
+++ packages/svelte-undp-components/src/lib/components/index.ts
@@ -18,6 +18,7 @@ import OpacityEditor from './OpacityEditor.svelte';
 import PanelButton from './PanelButton.svelte';
 import PropertyEditor from './PropertyEditor.svelte';
 import SegmentButtons, { type SegmentButton } from './SegmentButtons.svelte';
+import SdgSelector from './SDGSelector.svelte';
 import ShowDetails from './ShowDetails.svelte';
 import Slider from './Slider.svelte';
 import Tabs, { type Tab } from './Tabs.svelte';
@@ -42,6 +43,7 @@ export {
 	OpacityEditor,
 	PanelButton,
 	PropertyEditor,
+	SdgSelector,
 	SegmentButtons,
 	ShowDetails,
 	Slider,
