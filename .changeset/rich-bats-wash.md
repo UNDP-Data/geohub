07c672cef9136c9b1519a3e124496edccfd1ce2f
Jin Igarashi <igarashi.jin@water-gis.com>
2024-09-04 13:43:19 +0100

    fix: load undp-bulma from NPM instead of CDN

---
+++ .changeset/rich-bats-wash.md
@@ -0,0 +1,6 @@
+---
+"@undp-data/svelte-undp-components": patch
+"@undp-data/svelte-undp-design": patch
+---
+
+fix: load undp-bulma from NPM instead of CDN
--- packages/svelte-undp-components/.storybook/preview.ts
+++ packages/svelte-undp-components/.storybook/preview.ts
@@ -1,4 +1,5 @@
 import type { Preview } from '@storybook/svelte';
+import '@undp-data/undp-bulma/dist/style.css';
 
 const preview: Preview = {
 	parameters: {
