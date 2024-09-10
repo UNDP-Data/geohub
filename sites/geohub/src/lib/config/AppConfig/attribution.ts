7444bef76bb8d778fbd6d4dd3993fdd6ba1e94ff
Jin Igarashi <igarashi.jin@water-gis.com>
2024-09-10 13:45:56 +0100

    fix: fixed storymap footer style

--- sites/geohub/src/lib/config/AppConfig/attribution.ts
+++ sites/geohub/src/lib/config/AppConfig/attribution.ts
@@ -1,2 +1,7 @@
 export const attribution =
 	'<a target="_top" rel="noopener" href="http://undp.org">United Nations Development Programme (UNDP)</a>';
+
+export const getAttribution = (isLink = false) => {
+	const label = `© ${new Date().getFullYear()} United Nations Development Programme`;
+	return isLink ? `<a target="_top" rel="noopener" href="http://undp.org">${label}</a>` : label;
+};
