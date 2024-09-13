bcb890b08b576aa4c39f09036711c6bf80b821a8
Jin Igarashi <igarashi.jin@water-gis.com>
2024-09-13 16:51:30 +0100

    fix: removed comment

--- sites/geohub/src/lib/server/helpers/loadStorymapById.ts
+++ sites/geohub/src/lib/server/helpers/loadStorymapById.ts
@@ -49,20 +49,6 @@ export const loadStorymapById = async (
 		dbm.end();
 	}
 
-	// const res = await fetch(`/api/storymaps/${id}`);
-
-	// if (!res.ok) {
-	// 	if (res.status === 403) {
-	// 		error(res.status, { message: 'No permission to access' });
-	// 	} else if (res.status === 404) {
-	// 		error(res.status, { message: 'No storymap found' });
-	// 	} else {
-	// 		error(res.status, { message: res.statusText });
-	// 	}
-	// }
-
-	// const storymap: StoryMapConfig = await res.json();
-
 	if (!embed) {
 		const accessLevel = storymap.access_level;
 		if (accessLevel === AccessLevel.PRIVATE) {
