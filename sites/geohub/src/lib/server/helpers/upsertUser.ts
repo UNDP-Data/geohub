44f1088e37b719efe18bf4533127569a0a22f13d
Jin Igarashi <igarashi.jin@water-gis.com>
2024-10-02 17:12:27 +0100

    fix: fixed bug of upsertUser.

--- sites/geohub/src/lib/server/helpers/upsertUser.ts
+++ sites/geohub/src/lib/server/helpers/upsertUser.ts
@@ -1,14 +1,18 @@
+import { generateHashKey } from '$lib/helper';
 import { db } from '$lib/server/db';
 import { usersInGeohub } from '$lib/server/schema';
-import { sql } from 'drizzle-orm';
 
 export const upsertUser = async (user_email: string) => {
-	await db.execute(sql`
-		INSERT INTO ${usersInGeohub} 
-		(${usersInGeohub.id}, ${usersInGeohub.userEmail}) 
-		values(MD5(${user_email}), ${user_email})
-		ON CONFLICT (${usersInGeohub.id}) 
-		DO UPDATE 
-		SET ${usersInGeohub.lastaccessedat} = now()
-	`);
+	await db
+		.insert(usersInGeohub)
+		.values({
+			id: generateHashKey(user_email),
+			userEmail: user_email
+		})
+		.onConflictDoUpdate({
+			target: [usersInGeohub.id],
+			set: {
+				lastaccessedat: new Date().toISOString()
+			}
+		});
 };
