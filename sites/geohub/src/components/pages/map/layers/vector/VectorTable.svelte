56c56f6d30defb4d395c7fa2ff909c82cfacb20c
Jin Igarashi <igarashi.jin@water-gis.com>
2024-10-10 14:41:47 +0100

    fix: show pagination

--- sites/geohub/src/components/pages/map/layers/vector/VectorTable.svelte
+++ sites/geohub/src/components/pages/map/layers/vector/VectorTable.svelte
@@ -24,7 +24,6 @@
 
 	let panelHeaderHeight = 0;
 	let headerHeight = 0;
-	$: tableHeight = height - headerHeight - panelHeaderHeight;
 
 	let tableData:
 		| { type: 'FeatureCollection'; features: Feature[]; links: Link[]; pages: Pages }
@@ -108,156 +107,161 @@
 	showExpand={false}
 	bind:headerHeight={panelHeaderHeight}
 >
-	<div class="is-flex px-4 py-2" bind:clientHeight={headerHeight}>
-		<div class="search-control">
-			<SearchExpand
-				bind:value={query}
-				open={true}
-				placeholder="Type keyword..."
-				on:change={handleFilterInput}
-				iconSize={16}
-				fontSize={6}
-				timeout={SearchDebounceTime}
-				disabled={!tableData}
-				loading={!tableData}
-			/>
-		</div>
-
-		{#if tableData}
-			<div
-				class="ml-auto my-auto dropdown {showDownloadMenu ? 'is-active' : ''}"
-				role="menu"
-				tabindex="-1"
-				on:mouseenter={() => (showDownloadMenu = true)}
-				on:mouseleave={() => {
-					showDownloadMenu = false;
-				}}
-			>
-				<div class="dropdown-trigger">
-					<button
-						class="button"
-						aria-haspopup="true"
-						aria-controls="download-table-dropdown-menu"
-						on:click={() => {
-							showDownloadMenu = !showDownloadMenu;
-						}}
-					>
-						<span class="icon is-small">
-							<span class="material-symbols-outlined"> download </span>
-						</span>
-						<span>Download</span>
-						<span class="icon is-small has-text-primary">
-							<i
-								class="fas fa-chevron-down toggle-icon {showDownloadMenu ? 'active' : ''}"
-								aria-hidden="true"
-							></i>
-						</span>
-					</button>
-				</div>
-				<div class="dropdown-menu" id="download-table-dropdown-menu" role="menu">
-					<div class="dropdown-content">
-						{#each SupportedTableFormats as format}
-							{@const fileUrl = tableData?.links.find((l) => l.rel === format)?.href}
-							{#if fileUrl}
-								<a href={fileUrl} target="_blank" class="dropdown-item">
-									<p class="is-uppercase">{format}</p>
-								</a>
-							{/if}
-						{/each}
-					</div>
-				</div>
-			</div>
-		{/if}
-	</div>
-
-	<div class="table-contents mb-4" style="height: {tableHeight}px;">
-		{#if !tableData}
-			<div class="is-flex is-justify-content-center">
-				<Loader />
-			</div>
-		{:else if tableData.features.length > 0}
-			<div class="table-container attribute-table">
-				<table class="table is-hoverable is-fullwidth">
-					<thead>
-						<tr>
-							<th></th>
-							{#each columns as col}
-								<th>
-									<p>{clean(col)}</p>
-								</th>
-							{/each}
-						</tr>
-					</thead>
-					<tbody>
-						{#each tableData.features as feature, index}
-							{#if feature.properties}
-								<tr>
-									<th class="row-number">{index + 1}</th>
-									{#each columns as col}
-										{@const value = feature.properties[col]}
-										<td>
-											{#if value}
-												{value}
-											{/if}
-										</td>
-									{/each}
-								</tr>
-							{/if}
-						{/each}
-					</tbody>
-				</table>
-			</div>
-
-			<div class="pt-5">
-				<Pagination
-					bind:totalPages={tableData.pages.totalPages}
-					bind:currentPage={tableData.pages.currentPage}
-					hidden={tableData.pages.totalPages <= 1}
-					on:clicked={handlePaginationClicked}
+	<div class="panel-contents">
+		<div class="is-flex px-4 py-2" bind:clientHeight={headerHeight}>
+			<div class="search-control">
+				<SearchExpand
+					bind:value={query}
+					open={true}
+					placeholder="Type keyword..."
+					on:change={handleFilterInput}
+					iconSize={16}
+					fontSize={6}
+					timeout={SearchDebounceTime}
+					disabled={!tableData}
+					loading={!tableData}
 				/>
 			</div>
-		{:else}
-			<Notification type="info" showIcon={false} showCloseButton={false}>
-				No table content found in current map extent.
-			</Notification>
-		{/if}
+
+			{#if tableData}
+				<div
+					class="ml-auto my-auto dropdown {showDownloadMenu ? 'is-active' : ''}"
+					role="menu"
+					tabindex="-1"
+					on:mouseenter={() => (showDownloadMenu = true)}
+					on:mouseleave={() => {
+						showDownloadMenu = false;
+					}}
+				>
+					<div class="dropdown-trigger">
+						<button
+							class="button"
+							aria-haspopup="true"
+							aria-controls="download-table-dropdown-menu"
+							on:click={() => {
+								showDownloadMenu = !showDownloadMenu;
+							}}
+						>
+							<span class="icon is-small">
+								<span class="material-symbols-outlined"> download </span>
+							</span>
+							<span>Download</span>
+							<span class="icon is-small has-text-primary">
+								<i
+									class="fas fa-chevron-down toggle-icon {showDownloadMenu ? 'active' : ''}"
+									aria-hidden="true"
+								></i>
+							</span>
+						</button>
+					</div>
+					<div class="dropdown-menu" id="download-table-dropdown-menu" role="menu">
+						<div class="dropdown-content">
+							{#each SupportedTableFormats as format}
+								{@const fileUrl = tableData?.links.find((l) => l.rel === format)?.href}
+								{#if fileUrl}
+									<a href={fileUrl} target="_blank" class="dropdown-item">
+										<p class="is-uppercase">{format}</p>
+									</a>
+								{/if}
+							{/each}
+						</div>
+					</div>
+				</div>
+			{/if}
+		</div>
+
+		<div class="table-contents" style="height: {height - panelHeaderHeight - headerHeight}px;">
+			{#if !tableData}
+				<div class="is-flex is-justify-content-center">
+					<Loader />
+				</div>
+			{:else if tableData.features.length > 0}
+				<div class="table-container attribute-table">
+					<table class="table is-hoverable is-fullwidth">
+						<thead>
+							<tr>
+								<th></th>
+								{#each columns as col}
+									<th>
+										<p>{clean(col)}</p>
+									</th>
+								{/each}
+							</tr>
+						</thead>
+						<tbody>
+							{#each tableData.features as feature, index}
+								{#if feature.properties}
+									<tr>
+										<th class="row-number">{index + 1}</th>
+										{#each columns as col}
+											{@const value = feature.properties[col]}
+											<td>
+												{#if value}
+													{value}
+												{/if}
+											</td>
+										{/each}
+									</tr>
+								{/if}
+							{/each}
+						</tbody>
+					</table>
+				</div>
+
+				{#if tableData}
+					<div class="ml-4 mb-5">
+						<Pagination
+							bind:totalPages={tableData.pages.totalPages}
+							bind:currentPage={tableData.pages.currentPage}
+							hidden={tableData.pages.totalPages <= 1}
+							on:clicked={handlePaginationClicked}
+						/>
+					</div>
+				{/if}
+			{:else}
+				<Notification type="info" showIcon={false} showCloseButton={false}>
+					No table content found in current map extent.
+				</Notification>
+			{/if}
+		</div>
 	</div>
 </FloatingPanel>
 
 <style lang="scss">
-	.search-control {
-		max-width: 200px;
-	}
-
-	.toggle-icon {
-		-webkit-transition: all 0.3s ease;
-		-moz-transition: all 0.3s ease;
-		-ms-transition: all 0.3s ease;
-		-o-transition: all 0.3s ease;
-		transition: all 0.3s ease;
-
-		&.active {
-			transform: rotate(-180deg);
-			-webkit-transform: rotate(-180deg);
-			-moz-transform: rotate(-180deg);
-			-ms-transform: rotate(-180deg);
-			-o-transform: rotate(-180deg);
-			transition: rotateZ(-180deg);
+	.panel-contents {
+		.search-control {
+			max-width: 200px;
 		}
-	}
 
-	.table-contents {
-		.attribute-table {
-			height: 100%;
-			overflow: auto;
+		.toggle-icon {
+			-webkit-transition: all 0.3s ease;
+			-moz-transition: all 0.3s ease;
+			-ms-transition: all 0.3s ease;
+			-o-transition: all 0.3s ease;
+			transition: all 0.3s ease;
 
-			.row-number {
-				background-color: #edeff0;
-				text-align: center;
+			&.active {
+				transform: rotate(-180deg);
+				-webkit-transform: rotate(-180deg);
+				-moz-transform: rotate(-180deg);
+				-ms-transform: rotate(-180deg);
+				-o-transform: rotate(-180deg);
+				transition: rotateZ(-180deg);
 			}
+		}
 
-			th {
-				white-space: nowrap;
+		.table-contents {
+			overflow-y: auto;
+
+			.attribute-table {
+				.row-number {
+					background-color: #edeff0;
+					text-align: center;
+				}
+
+				th {
+					white-space: nowrap;
+				}
 			}
 		}
 	}
