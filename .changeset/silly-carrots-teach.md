187475601972ca8d62cfe63567b3ee7d6cd69466
Jin Igarashi <jin.igarashi@undp.org>
2024-10-29 16:07:20 +0900

    fix: show menu by clicking header column to sort directly by asc or desc instead of toggling with click (#4344)
    * fix: show menu by clicking header column to sort directly by asc or desc instead of toggling with click
    
    * fix: right click to show header context menu

---
+++ .changeset/silly-carrots-teach.md
@@ -0,0 +1,5 @@
+---
+"geohub": patch
+---
+
+fix: show menu by clicking header column to sort directly by asc or desc instead of toggling with click
--- sites/geohub/src/components/pages/map/layers/vector/VectorTableColumn.svelte
+++ sites/geohub/src/components/pages/map/layers/vector/VectorTableColumn.svelte
@@ -1,7 +1,23 @@
 <script lang="ts">
-	import { clean, initTooltipTippy } from '@undp-data/svelte-undp-components';
+	import { clean, initTippy, initTooltipTippy } from '@undp-data/svelte-undp-components';
 	import { createEventDispatcher } from 'svelte';
 
+	let tippyInstance: { show: () => void } | undefined;
+	const tippy = initTippy({
+		appendTo: document.body,
+		placement: 'bottom-start',
+		offset: [0, 7],
+		arrow: false,
+		// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+		// @ts-ignore
+		trigger: 'click',
+		onCreate: (instance) => {
+			tippyInstance = instance;
+		},
+		onDestroy: () => {
+			tippyInstance = undefined;
+		}
+	});
 	const tippyTooltip = initTooltipTippy();
 	const dispatch = createEventDispatcher();
 
@@ -11,27 +27,41 @@
 	export let isActive = false;
 	export let isFiltered = false;
 
-	const handleColumnClick = () => {
-		if (isActive) {
-			order = order === 'asc' ? 'desc' : 'asc';
-		} else {
-			order = 'asc';
-			isActive = true;
-		}
+	let tooltipContent: HTMLElement;
+
+	const handleSortClick = (value: 'asc' | 'desc') => {
+		order = value;
+		isActive = true;
 		dispatch('change', {
 			name: name,
 			order: order,
 			isActive: isActive
 		});
 	};
+
+	const handleResetSort = () => {
+		isActive = false;
+		dispatch('change', {
+			name: name,
+			order: order,
+			isActive: isActive
+		});
+	};
+
+	const handleContextMenu = () => {
+		if (tippyInstance && 'show' in tippyInstance) {
+			tippyInstance.show();
+		}
+	};
 </script>
 
 <button
 	class="button sort-button"
-	on:click={handleColumnClick}
+	use:tippy={{ content: tooltipContent }}
 	use:tippyTooltip={{
-		content: `Click to sort by ${clean(name)}`
+		content: `${clean(name)}`
 	}}
+	on:contextmenu|preventDefault={handleContextMenu}
 >
 	<span class="label is-flex" style={width ? `max-width: ${width}px;` : ''}>
 		{#if isFiltered}
@@ -53,6 +83,47 @@
 	{/if}
 </button>
 
+<div class="context-menu" bind:this={tooltipContent}>
+	<div class="navbar">
+		<ul>
+			{#if isActive}
+				<li>
+					<button class="is-flex is-align-items-center p-2" on:click={handleResetSort}>
+						<span class="icon is-small material-symbols-outlined mr-2"> clear_all </span>
+						<span> Reset sort </span>
+					</button>
+				</li>
+			{/if}
+			{#if !(isActive && order === 'asc')}
+				<li>
+					<button
+						class="is-flex is-align-items-center p-2"
+						on:click={() => {
+							handleSortClick('asc');
+						}}
+					>
+						<span class="icon is-small material-symbols-outlined mr-2"> arrow_downward </span>
+						<span> Sort ascending </span>
+					</button>
+				</li>
+			{/if}
+			{#if !(isActive && order === 'desc')}
+				<li>
+					<button
+						class="is-flex is-align-items-center p-2"
+						on:click={() => {
+							handleSortClick('desc');
+						}}
+					>
+						<span class="icon is-small material-symbols-outlined mr-2"> arrow_upward </span>
+						<span>Sort descending </span>
+					</button>
+				</li>
+			{/if}
+		</ul>
+	</div>
+</div>
+
 <style lang="scss">
 	.name {
 		white-space: nowrap;
@@ -68,6 +139,48 @@
 		.sort-icon {
 			font-size: 16px;
 			margin-bottom: auto;
+		}
+	}
+
+	:global(.tippy-content:has(.context-menu)) {
+		padding: 0;
+	}
+
+	.context-menu {
+		.navbar {
+			display: inline-flex;
+			border: 1px #d4d6d8 solid;
+			width: fit-content;
+			min-width: 150px;
+			background-color: #fff;
+			border-radius: 0;
+			overflow: hidden;
+			flex-direction: column;
+			box-shadow: 0px 3px 6px 0px rgba(0, 0, 0, 0.1);
+
+			ul {
+				margin: 6px 0;
+			}
+		}
+
+		ul li {
+			display: block;
+			list-style-type: none;
+			width: 1fr;
+
+			button {
+				font-size: 1rem;
+				color: #232e3d;
+				width: 100%;
+				height: 30px;
+				border: 0px;
+				background-color: #fff;
+
+				&:hover {
+					color: #000;
+					background-color: #edeff0;
+				}
+			}
 		}
 	}
 </style>
