23f338bcc64b0876deb00920f89769648d006242
Jin Igarashi <igarashi.jin@water-gis.com>
2024-11-06 22:36:07 +0900

    refactor: merged Hillshade to RasterLegend

--- sites/geohub/src/components/maplibre/raster/RasterLegend.svelte
+++ sites/geohub/src/components/maplibre/raster/RasterLegend.svelte
@@ -1,10 +1,14 @@
 <script lang="ts">
-	import Hillshade from '$components/maplibre/raster/Hillshade.svelte';
 	import { getValueFromRasterTileUrl } from '$lib/helper';
 	import type { Link, RasterAlgorithm, RasterTileMetadata, Tag } from '$lib/types';
 	import {
 		Accordion,
 		Help,
+		HillshadeAccentColor,
+		HillshadeExaggeration,
+		HillshadeHighlightColor,
+		HillshadeIlluminationDirection,
+		HillshadeShadowColor,
 		MAPSTORE_CONTEXT_KEY,
 		type MapStore
 	} from '@undp-data/svelte-undp-components';
@@ -19,16 +23,15 @@
 	export let metadata: RasterTileMetadata;
 	export let tags: Tag[] = [];
 	export let links: Link[] = [];
-	let algorithmId: string = undefined;
+	let algorithmId: string | undefined = undefined;
 	let algorithm: RasterAlgorithm;
+	let layerStyle: LayerSpecification | undefined;
 
 	const handleSelectAlgorithm = () => {
 		layerStyle = $map.getStyle().layers.find((l: LayerSpecification) => l.id === layerId);
 	};
 
-	let expanded: { [key: string]: boolean } = {
-		color: true
-	};
+	let expanded: { [key: string]: boolean } = {};
 	// to allow only an accordion to be expanded
 	let expandedDatasetId: string;
 	$: {
@@ -46,8 +49,6 @@
 		}
 	}
 
-	let layerStyle: LayerSpecification;
-
 	onMount(() => {
 		algorithmId = (getValueFromRasterTileUrl($map, layerId, 'algorithm') as string) ?? undefined;
 		if (algorithmId) {
@@ -56,6 +57,12 @@
 			});
 		}
 		layerStyle = $map.getStyle().layers.find((l: LayerSpecification) => l.id === layerId);
+		const isHillShade = layerStyle && layerStyle.type === 'hillshade' ? true : false;
+		if (!isHillShade) {
+			expanded = { color: true };
+		} else {
+			expanded = { 'hillshade-accent-color': true };
+		}
 	});
 
 	const getAlgorithm = async (id: string) => {
@@ -93,7 +100,57 @@
 	{/if}
 
 	{#if layerStyle && layerStyle.type === 'hillshade'}
-		<Hillshade bind:layerId />
+		<Accordion title="Hillshade accent color" bind:isExpanded={expanded['hillshade-accent-color']}>
+			<div class="pb-2" slot="content">
+				<HillshadeAccentColor {layerId} />
+			</div>
+			<div slot="buttons">
+				<Help>
+					Change the shading color used to accentuate rugged terrain like sharp cliffs and gorges.
+				</Help>
+			</div>
+		</Accordion>
+		<Accordion title="Hillshade exaggeration" bind:isExpanded={expanded['hillshade-exaggeration']}>
+			<div class="pb-2" slot="content">
+				<HillshadeExaggeration {layerId} />
+			</div>
+			<div slot="buttons">
+				<Help>Change the Intensity of the hillshade.</Help>
+			</div>
+		</Accordion>
+		<Accordion
+			title="Hillshade highlight color"
+			bind:isExpanded={expanded['hillshade-highlight-color']}
+		>
+			<div class="pb-2" slot="content">
+				<HillshadeHighlightColor {layerId} />
+			</div>
+			<div slot="buttons">
+				<Help>Change the shading color of areas that faces towards the light source.</Help>
+			</div>
+		</Accordion>
+		<Accordion
+			title="Hillshade illumination direction"
+			bind:isExpanded={expanded['hillshade-illumination-direction']}
+		>
+			<div class="pb-2" slot="content">
+				<HillshadeIlluminationDirection {layerId} />
+			</div>
+			<div slot="buttons">
+				<Help>
+					The direction of the light source used to generate the hillshading with 0 as the top of
+					the viewport
+				</Help>
+			</div>
+		</Accordion>
+		<Accordion title="Hillshade shadow color" bind:isExpanded={expanded['hillshade-shadow-color']}>
+			<div class="pb-2" slot="content">
+				<HillshadeShadowColor {layerId} />
+			</div>
+			<div slot="buttons">
+				<Help>Change the shading color of areas that face away from the light source.</Help>
+			</div>
+		</Accordion>
 	{:else if layerStyle?.type === 'raster'}
 		{#key layerStyle}
 			<RasterLegendEdit bind:layerId bind:metadata bind:tags bind:expanded bind:algorithmId />
