d11951a847ebd105123422a02a349a9f2462182e
Jin Igarashi <igarashi.jin@water-gis.com>
2024-11-18 18:47:47 +0900

    adjusted max extent

--- sites/geohub/src/routes/(app)/dashboards/zanzibar/+page.server.ts
+++ sites/geohub/src/routes/(app)/dashboards/zanzibar/+page.server.ts
@@ -17,8 +17,8 @@ export const load: PageServerLoad = async ({ fetch, url }) => {
 	const center: LngLatLike = [39.58, -5.6472];
 	const zoom = 6;
 	const maxExtent: LngLatBoundsLike = [
-		[37.657592, -7.246684],
-		[40.915039, -3.983451]
+		[38.8971, -6.893],
+		[40.0507, -4.5757]
 	];
 	const places: LocationSwitchPlaces[] = [
 		{
