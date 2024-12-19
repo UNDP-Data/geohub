9baaa4314177b3a0359f684814fe87c6979cb7c3
Jin Igarashi <jin.igarashi@undp.org>
2024-12-19 14:54:27 +0900

    fix: allow unsigned users to access stac product api to get information. (#4666)

--- sites/geohub/src/routes/api/stac/[id]/[collection]/products/+server.ts
+++ sites/geohub/src/routes/api/stac/[id]/[collection]/products/+server.ts
@@ -1,14 +1,9 @@
-import { error, type RequestHandler } from '@sveltejs/kit';
+import { type RequestHandler } from '@sveltejs/kit';
 import { db } from '$lib/server/db';
 import { sql } from 'drizzle-orm';
 import { stacCollectionProductInGeohub } from '$lib/server/schema';
 
-export const GET: RequestHandler = async ({ locals, params }) => {
-	const session = await locals.auth();
-	if (!session) {
-		error(403, { message: 'Permission error' });
-	}
-
+export const GET: RequestHandler = async ({ params }) => {
 	const products = await db
 		.select({
 			stac_id: stacCollectionProductInGeohub.stacId,
