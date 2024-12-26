4632a3013df0b9e5964b9006ac7fbf97812af171
Jin Igarashi <igarashi.jin@water-gis.com>
2024-12-26 16:34:27 +0900

    refactor: migrated api and auth pages to svelte5

--- sites/geohub/src/routes/(app)/api/+page.svelte
+++ sites/geohub/src/routes/(app)/api/+page.svelte
@@ -1,26 +1,33 @@
 <script lang="ts">
 	import { browser } from '$app/environment';
-	import { page } from '$app/stores';
+	import { page } from '$app/state';
 	import { HeroHeader, type BreadcrumbPage } from '@undp-data/svelte-undp-components';
 	import { SwaggerUIBundle } from 'swagger-ui-dist';
 	import 'swagger-ui-dist/swagger-ui.css';
+	import type { PageData } from './$types';
 
-	let spec = $page.data.spec;
-
-	let swaggerDiv: HTMLDivElement;
-
-	$: if (swaggerDiv) {
-		SwaggerUIBundle({
-			url: spec,
-			domNode: swaggerDiv,
-			presets: [SwaggerUIBundle.presets.apis, SwaggerUIBundle.SwaggerUIStandalonePreset]
-		});
+	interface Props {
+		data: PageData;
 	}
 
-	let breadcrumbs: BreadcrumbPage[] = [
+	let { data }: Props = $props();
+
+	let swaggerDiv: HTMLDivElement | undefined = $state();
+
+	$effect(() => {
+		if (swaggerDiv) {
+			SwaggerUIBundle({
+				url: data.spec,
+				domNode: swaggerDiv,
+				presets: [SwaggerUIBundle.presets.apis, SwaggerUIBundle.SwaggerUIStandalonePreset]
+			});
+		}
+	});
+
+	let breadcrumbs: BreadcrumbPage[] = $state([
 		{ title: 'home', url: '/' },
-		{ title: 'API docs', url: $page.url.href }
-	];
+		{ title: 'API docs', url: page.url.href }
+	]);
 </script>
 
 <HeroHeader title="API Documentation" bind:breadcrumbs />
