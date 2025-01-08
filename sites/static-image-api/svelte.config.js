6d68c1e9e8ccf69fccccc170c412fb294fbc66ac
Jin Igarashi <jin.igarashi@undp.org>
2025-01-08 13:46:05 +0900

    chore: use named import of sveltePreprocess (#4696)

--- sites/static-image-api/svelte.config.js
+++ sites/static-image-api/svelte.config.js
@@ -1,11 +1,11 @@
 import adapter from '@sveltejs/adapter-node';
-import preprocess from 'svelte-preprocess';
+import { sveltePreprocess } from 'svelte-preprocess';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
 	// for more information about preprocessors
-	preprocess: preprocess(),
+	preprocess: sveltePreprocess(),
 
 	kit: {
 		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
