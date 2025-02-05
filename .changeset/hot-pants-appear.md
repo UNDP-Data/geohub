93b51ce97015f81224308dab8914a7e6549af119
Joseph Thuha <josephthuha@gmail.com>
2025-02-05 10:22:11 +0300

    refactor: change electricity forecast to v3 data (#4786)
    * refactor: change electricity forecast to v3 data
    
    * add changeset
    
    * update changeset text

---
+++ .changeset/hot-pants-appear.md
@@ -0,0 +1,5 @@
+---
+"geohub": patch
+---
+
+fix: switched the electricity forecast data (2021-2030) to v3 on Electricity Dashboard
--- sites/geohub/src/routes/(map)/dashboards/electricity/+page.ts
+++ sites/geohub/src/routes/(map)/dashboards/electricity/+page.ts
@@ -6,7 +6,7 @@ export const load: PageLoad = async ({ url, parent }) => {
 	const content = 'Electricity dashboard';
 	const site_description = `The 'Affordable and clean energy' dashboard helps identify vulnerable areas in the world that have limited or no access to energy.`;
 
-	const adminUrl = `${azureUrl}/hrea/admin/v3`;
+	const adminUrl = `${azureUrl}/hrea/admin/v4`;
 
 	return {
 		adminUrl,
