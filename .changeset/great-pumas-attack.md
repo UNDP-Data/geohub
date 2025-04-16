27fa5faeb2b1f0680f910062683b8c530b3dad16
Jin Igarashi <jin.igarashi@undp.org>
2025-04-16 21:44:30 +0900

    fix: handle layer name correctly if it contains dot (.) in title (#4927)
    * fix: show accordion and floating panel titile correctly if title contains dot (.)
    
    * fix: fixed /datasets/{id}/table endpoint to handle layer name correctly if layer name contains dot (.)

---
+++ .changeset/great-pumas-attack.md
@@ -0,0 +1,7 @@
+---
+"@undp-data/svelte-maplibre-storymap": patch
+"@undp-data/svelte-undp-components": patch
+"geohub": patch
+---
+
+fix: show accordion and floating panel titile correctly if title contains dot (.)
--- packages/svelte-undp-components/src/lib/util/clean.ts
+++ packages/svelte-undp-components/src/lib/util/clean.ts
@@ -2,11 +2,16 @@
  * Remove underscore and extension and apply start/title case to a string
  * @param val String to clean
  */
-export const clean = (val: string, isUppercase = true) => {
+export const clean = (val: string, isUppercase = true, removeExtension = true) => {
 	if (!val) return '';
 	// apply start/title case
-	return val
-		.replace(/[_-]/g, ' ') // remove underscore and hyphen
-		.replace(/\.[^/.]+$/, '') // remove extension
-		.replace(/\b\w/g, (str) => (isUppercase ? str.toUpperCase() : str));
+	let result = val.replace(/[_-]/g, ' '); // remove underscore and hyphen
+
+	if (removeExtension) {
+		result = result.replace(/\.[^/.]+$/, '');
+	}
+
+	result = result.replace(/\b\w/g, (str) => (isUppercase ? str.toUpperCase() : str));
+
+	return result;
 };
