2b6b5d28e75d1637aa7524f24ce6138caf3e60fd
Jin Igarashi <igarashi.jin@water-gis.com>
2025-06-16 15:24:22 +0900

    feat: upgrade storybook from v8 to v9

--- packages/svelte-undp-components/src/lib/components/ui/DatePicker.stories.ts
+++ packages/svelte-undp-components/src/lib/components/ui/DatePicker.stories.ts
@@ -1,4 +1,4 @@
-import type { Meta, StoryObj } from '@storybook/svelte';
+import type { Meta, StoryObj } from '@storybook/sveltekit';
 import DatePicker from './DatePicker.svelte';
 import dayjs from 'dayjs';
 
--- packages/svelte-undp-components/src/lib/components/ui/HeroHeader.stories.ts
+++ packages/svelte-undp-components/src/lib/components/ui/HeroHeader.stories.ts
@@ -1,4 +1,4 @@
-import type { Meta, StoryObj } from '@storybook/svelte';
+import type { Meta, StoryObj } from '@storybook/sveltekit';
 import HeroHeader from './HeroHeader.svelte';
 import type { BreadcrumbPage } from './Breadcrumbs.svelte';
 import type { Tab } from './Tabs.svelte';
