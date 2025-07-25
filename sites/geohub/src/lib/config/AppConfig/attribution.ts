deb543e6f3767ba1b0216f8c5051ad65d0333087
Thuhaa <josephthuha@gmail.com>
2025-07-25 17:49:05 +0300

    fix: remove undp from attribution

--- sites/geohub/src/lib/config/AppConfig/attribution.ts
+++ sites/geohub/src/lib/config/AppConfig/attribution.ts
@@ -1,16 +1,17 @@
 const disclaimerText = `
 <br/><br/><b>Disclaimer</b><br/>
-<span>
-  The designations employed and the presentation of material on this map do not imply the expression of any opinion whatsoever on the part of the<br>
-  Secretariat of the United Nations or UNDP concerning the legal status of any country, territory, city or area or its authorities, or concerning<br>
-  the delimitation of its frontiers or boundaries.
+<span class="mr-2">
+  The designations employed and the presentation of material on this map do not imply the expression of any opinion whatsoever on the part<br>
+  of the Secretariat of the United Nations or UNDP concerning the legal status of any country, territory, city or area or its authorities,<br>
+  or concerning the delimitation of its frontiers or boundaries.
 </span>
+
 
 `;
 
-export const attribution = `<span><a target="_top" rel="noopener" href="http://undp.org">United Nations Development Programme (UNDP)</a>${disclaimerText}</span>`;
+export const attribution = `<span>${disclaimerText}</span>`;
 
 export const getAttribution = (isLink = false) => {
-	const label = `© ${new Date().getFullYear()} United Nations Development Programme.`;
-	return isLink ? `<a target="_top" rel="noopener" href="http://undp.org">${label}</a>` : label;
+	const label = `© ${new Date().getFullYear()}`;
+	return isLink ? ` ` : label;
 };
